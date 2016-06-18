a51c0b2ec6d0f95eb801cbda4ffa034ec80b574a
sagarnikam123 <sagarnikam123@gmail.com>
2016-06-18 08:36:51 +0530

    exceptions APIs and protocols

---
+++ pluralSight/pythonFundamentals/7_handlingExceptions/8_exceptionsAPIsAndProtocols.py
@@ -0,0 +1,21 @@
+# Exceptions, APIs and Protocols
+
+################################################################################
+# few exception types
+# IndexError # integer index is out of range
+# KeyError # look-up in a mapping fails
+# ValueError # object is of the right type, but contains an inappropriate value.
+# TypeError
+
+z = [1, 4, 2]
+# IndexError: list index out of range
+z[4]
+
+# ValueError: invalid literal for int() with base 10: 'jim'
+int("jim")
+
+codes = dict(gb=44, us=1, no=47, fr=33, es=34)
+# KeyError: 'de'
+codes['de']
+
+################################################################################
