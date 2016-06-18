45ac41144a87c58654b2d6d54ff4073d4d7c7239
sagarnikam123 <sagarnikam123@gmail.com>
2016-06-18 08:33:12 +0530

    removed few codes blocks to respective files

--- pluralSight/pythonFundamentals/7_handlingExceptions/2_exceptionsAndControlFlow.py
+++ pluralSight/pythonFundamentals/7_handlingExceptions/2_exceptionsAndControlFlow.py
@@ -8,21 +8,4 @@ convert(44)
 # ValueError: invalid literal for int() with base 10: 'hedgehog'
 convert("hedgehog")
 
-# add try-except construct
-from module.exceptional2 import convert
-convert("34")
-convert("giraffe")
-# TypeError: int() argument must be a string, a bytes-like object or a number, not 'list'
-convert( [4, 5, 6] )
-
-# add handler for TypeError
-from module.exceptional3 import convert
-convert([1, 3, 19])
-
-# collapsing both exception handler type to avoid code duplicacy
-from module.exceptional4 import convert
-convert("29")
-convert("elephant")
-convert( [4, 5, 1] )
-
 ################################################################################
