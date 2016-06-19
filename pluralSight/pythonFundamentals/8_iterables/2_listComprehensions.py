ef5356219cca041831796df8ef63bcf59bd9ab02
sagarnikam123 <sagarnikam123@gmail.com>
2016-06-19 16:40:00 +0530

    list comprehensions

---
+++ pluralSight/pythonFundamentals/8_iterables/2_listComprehensions.py
@@ -0,0 +1,20 @@
+# List Comprehensions
+
+################################################################################
+words = "Why sometimes I have believed as many as six impossible things before breakfast".split()
+words
+# list comprehension
+# [expr(item) for item in iterable]
+[len(word) for word in words]
+# declarative imperative code for above
+lenghts = []
+for word in words :
+
+lenghts
+
+from math import factorial
+f = [len(str(factorial(x))) for x in range(28) ]
+f
+type(f)
+
+################################################################################
