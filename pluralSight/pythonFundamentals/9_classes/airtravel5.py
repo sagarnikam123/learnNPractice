2a555b5dddbff9ba6ff67605ac4123de90c6f25f
sagarnikam123 <sagarnikam123@gmail.com>
2016-06-23 07:56:13 +0530

    added Aircraft class

--- pluralSight/pythonFundamentals/9_classes/airtravel5.py
+++ pluralSight/pythonFundamentals/9_classes/airtravel5.py
@@ -23,4 +23,22 @@ class Flight:
     def airline(self):
         return self._number[:2]
 
+class Aircraft:
+
+    def __init__(self, registration, model, num_rows, num_seats_per_row):
+        self._registration = registration
+        self._model = model
+        self._num_rows = num_rows
+        self._num_seats_per_row = num_seats_per_row
+
+    def registration(self):
+        return self._registration
+
+    def model(self):
+        return self._model
+
+    def seating_plan(self):
+        return ( range(1, self._num_rows + 1 ),
+                 'ABCDEFGHJK'[:self._num_seats_per_row] )
+
 ################################################################################
