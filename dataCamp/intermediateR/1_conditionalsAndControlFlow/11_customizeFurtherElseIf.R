20ff182b407e72e90c03322b80239b6e57df1769
sagarnikam123 <sagarnikam123@gmail.com>
2016-06-24 20:26:33 +0530

    intermediate R

---
+++ dataCamp/intermediateR/1_conditionalsAndControlFlow/11_customizeFurtherElseIf.R
@@ -0,0 +1,42 @@
+# Customize further: else if
+
+#####################################################################################################################
+#																													#
+#	The else if statement allows to further customize your control structure. 										#
+#	You can add as many else if statements as you like. Keep in mind that R ignores the remainder of the control	#
+#	structure once a condition has been found that is TRUE and the corresponding expressions have been executed.	#
+#	Here's an overview of the syntax to freshen your memory:														#
+#																													#
+#	if(condition1) {																								#
+#	  expr1																											#
+#	} else if(condition2) {																							#
+#	  expr2																											#
+#	} else if(condition3) {																							#
+#	  expr3																											#
+#	} else {																										#
+#	  expr4																											#
+#	}																												#
+#																													#
+#####################################################################################################################
+
+# Variables related to your last day of recordings
+medium <- "Facebook"
+num_views <- 12
+
+# Control structure for medium
+if(medium == "LinkedIn") {
+  print("Showing LinkedIn information")
+} else if (medium == "Facebook"){
+  print("Showing Facebook information");
+} else {
+  print("Unknown medium")
+}
+
+# Control structure for num_views
+if(num_views > 15) {
+  print("You're popular!")
+} else if (num_views > 10 & num_views <=15){
+  print("Your number of views is average")
+}else {
+  print("Try to be more visible!")
+}
---
+++ dataCamp/intermediateR/1_conditionalsAndControlFlow/3_compareVectors.R
@@ -0,0 +1,28 @@
+# Compare vectors
+
+#####################################################################################################################
+#																													#
+#	You are already aware that R is a master at handling vectors.													#
+#	Without having to change anything about the syntax, R's relational operators also work on vectors.				#
+#																													#
+#	Let's go back to the example that was started in the video. You want to figure out whether your marketing 		#
+#	efforts, funny tweets and facebook posts have increased your visibility on various social media platforms,		#
+#	more specifically LinkedIn and Facebook. The sample code in the editor intializes 								#
+#	the vectors linkedin and facebook. Each of the vector contains the number of profile views of your LinkedIn and	#
+#	Facebook profiles in the last seven days.																		#
+#																													#
+#####################################################################################################################
+
+# The linkedin and facebook vectors have already been created for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+facebook <- c(17, 7, 5, 16, 8, 13, 14)
+
+# Popular days
+linkedin > 15
+
+
+# Quiet days
+linkedin <= 5
+
+# LinkedIn more popular than Facebook
+linkedin > facebook
