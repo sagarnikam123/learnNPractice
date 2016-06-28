e741e5729b2b4cd36dbbe29dcac0213ede0bed1a
sagarnikam123 <sagarnikam123@gmail.com>
2016-06-28 20:29:11 +0530

    oo with function objects

---
+++ pluralSight/pythonFundamentals/9_classes/10_OOWithFunctionObjects.py
@@ -0,0 +1,8 @@
+# OO with function objects
+
+################################################################################
+from airtravel11 import console_card_printer, make_flight
+f = make_flight()
+f.make_boarding_cards(console_card_printer)
+
+################################################################################
