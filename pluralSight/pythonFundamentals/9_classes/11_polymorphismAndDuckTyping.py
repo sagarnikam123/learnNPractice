91bb596e2dbfe2e7958133d8f3911a6a58d8d2c5
sagarnikam123 <sagarnikam123@gmail.com>
2016-06-28 20:46:47 +0530

    polymorphism & duck typing

---
+++ pluralSight/pythonFundamentals/9_classes/11_polymorphismAndDuckTyping.py
@@ -0,0 +1,11 @@
+# polymorphism and Duck typing
+
+################################################################################
+from airtravel12 import *
+f, g = make_flights()
+f.aircraft_model()
+g.aircraft_model()
+f.num_available_seats()
+g.num_available_seats()
+
+################################################################################
