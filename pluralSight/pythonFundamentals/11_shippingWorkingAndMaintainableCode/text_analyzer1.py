b3468099d5b159a518a78a7da3e4ffdd292ee204
sagarnikam123 <sagarnikam123@gmail.com>
2016-07-03 08:16:31 +0530

    diff text analyzer

---
+++ pluralSight/pythonFundamentals/11_shippingWorkingAndMaintainableCode/text_analyzer1.py
@@ -0,0 +1,16 @@
+# text_analyzer1
+
+################################################################################
+import unittest
+
+class TextAnalysisTests(unittest.TestCase):
+    """ Tests for the "analyze_text()" function. """
+
+    def test_function_runs(self):
+        """ Basic smoke test : does the function run. """
+        analyze_text()
+
+if __name__ == '__main__':
+    unittest.main()
+    
+################################################################################
