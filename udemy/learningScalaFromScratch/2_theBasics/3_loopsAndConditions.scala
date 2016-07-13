2f77f4187062c79581455c52f4050d0e43cd1c91
sagarnikam123 <sagarnikam123@gmail.com>
2016-07-13 20:17:46 +0530

    loops & conditions

---
+++ udemy/learningScalaFromScratch/2_theBasics/3_loopsAndConditions.scala
@@ -0,0 +1,19 @@
+var i = 0
+
+while (i < 5) {
+  println(i)
+  i += 1
+}
+
+var i = 0;
+
+while (i < 10){
+  if (i !=0){
+    print(" ");
+  }
+  print(i);
+  i += 1;
+}
+
+(x: Int, y: Int) => x + y
+res5(1,2)
