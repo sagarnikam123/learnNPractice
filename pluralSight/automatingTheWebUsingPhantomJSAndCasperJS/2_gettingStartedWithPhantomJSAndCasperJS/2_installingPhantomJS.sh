c6ca7b8c71f62789e10487296c68ff2c9106ec84
sagarnikam123 <sagarnikam123@gmail.com>
2016-07-18 16:51:14 +0530

    installing phantomjs

---
+++ pluralSight/automatingTheWebUsingPhantomJSAndCasperJS/2_gettingStartedWithPhantomJSAndCasperJS/2_installingPhantomJS.sh
@@ -0,0 +1,12 @@
+# checking prerequisite
+npm --version
+python --version
+
+mkdir automation-tools
+cd automation-tools
+
+
+# using local phantomjs
+npm init .
+# installing phantomjs
+npm install phantomjs-prebuilt --save
