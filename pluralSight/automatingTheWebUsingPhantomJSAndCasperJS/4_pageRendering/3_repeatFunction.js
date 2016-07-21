39c2885e41d15d99f148ddd8978cbb924ac79202
sagarnikam123 <sagarnikam123@gmail.com>
2016-07-21 16:57:33 +0530

    repeat function

---
+++ pluralSight/automatingTheWebUsingPhantomJSAndCasperJS/4_pageRendering/3_repeatFunction.js
@@ -0,0 +1,26 @@
+var casper = require('casper').create()
+
+var urls = ["http://www.google.com", "http://www.bing.com"];
+var viewportSizes = [480, 720, 1200];
+
+casper.start();
+
+var counter = 0;
+
+casper.repeat(viewportSizes.length,function(){
+  var viewportSize = viewportSizes[counter];
+  
+  casper.each(urls, function(self, item ,index){
+    self.thenOpen(item, function(){
+      var title = self.getTitle();
+      console.log(title);
+      this.wait(2000, function(){
+        this.capture('./images/screenshot_' + index + '.png');
+      });
+    });
+  });
+  counter += 1;
+});
+
+
+casper.run();
