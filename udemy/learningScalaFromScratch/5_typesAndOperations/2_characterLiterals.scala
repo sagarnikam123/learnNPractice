f2817225defa99024a09dc2c06e7e846b22e2053
sagarnikam123 <sagarnikam123@gmail.com>
2016-07-22 20:09:24 +0530

    character literals

---
+++ udemy/learningScalaFromScratch/5_typesAndOperations/2_characterLiterals.scala
@@ -0,0 +1,25 @@
+val big = 1.2345
+val bigger = 12.45e1
+val biggerstill = 123E45
+
+val little = 1.2345F
+val litt = 3e5f
+
+val another = 3e5
+val again = 3e5D
+val a = 'A'
+
+val d = '\u0041'
+val d = '\u0044'
+val d = '\u0045'
+val B\u0041\u0044 = 1
+val backslash =  '\\'
+
+val hello = "hello"
+val escapes = "\\\"\'"
+
+println("""Welcome to Ultimatrix 9000.
+Type "HELP" for help.""")
+
+println("""Welcome to Ultimatrix 9000.
+|Type "HELP" for help.""".stripMargin)
