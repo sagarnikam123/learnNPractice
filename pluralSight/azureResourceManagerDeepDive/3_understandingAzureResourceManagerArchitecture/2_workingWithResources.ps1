87f940de4c3c044f59bac3593d08b7fa8e508dae
sagarnikam123 <sagarnikam123@gmail.com>
2016-07-23 00:09:40 -0700

    complete script

--- pluralSight/azureResourceManagerDeepDive/3_understandingAzureResourceManagerArchitecture/2_workingWithResources.ps1
+++ pluralSight/azureResourceManagerDeepDive/3_understandingAzureResourceManagerArchitecture/2_workingWithResources.ps1
@@ -1,3 +1,16 @@
+# Login to Azure Account
+Login-AzureRmAccount
+
+# Get azure subscription 
+Get-AzureRmSubscription
+
+# Get all azure resources 
+Get-AzureRmResource
+
+# GEt all Azure resource group
+Get-AzureRmResourceGroup
+Get-AzureRmResourceGroup | Select-Object ResourceGroupName
+#####################################################
 # Resource providers & resource namespaces with types
 Get-AzureRmResourceProvider | `
   Select-Object ProviderNamespace, ResourceTypes | `
@@ -11,5 +24,59 @@ Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Compute | `
 # Using ARM REST APIs
 https://management.azure.com/subscriptions/{subscription-id}/providers/{provider-name}?&api-version={api-version}
 
-#((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Compute) ResourceTypes | Where-Object
-{$_.ResourceTypeNmae -eq 'virtualMachines'}).ApiVersions
+# Getting ApiVersions for specific provider
+((Get-AzureRmResourceProvider -ProviderNamespace Microsoft.Compute).ResourceTypes | Where-Object `
+{$_.ResourceTypeName -eq 'virtualMachines'}).ApiVersions
+###############################################################
+
+### Get ARM Provider Locations
+((Get-AzureRmResourceProvider `
+    -ProviderNamespace "$resourceProviderNamespace").ResourceTypes | `
+    Where-Object {$_.ResourceTypeName -eq "$resourceTypeName"}).Locations | `
+    Sort-Object
+
+### Define Deployment Variables
+$resourceGroupName = 'pluralsight-arm-simple-paas'
+$resourceProviderNamespace = 'Microsoft.Web'
+$resourceTypeName = 'sites'
+$resourceGroupLocation = 'Central US'
+
+$randomString = ([char[]]([char]'a'..[char]'z') + 0..9 | Sort-Object {Get-Random})[0..8] -join ''
+$appNamePrefix = 'pluralsight'
+$appServicePlanName = $appNamePrefix + $randomString
+$webAppName = $appNamePrefix + $randomString
+
+# Check 
+$resourceGroupName
+$webAppName
+
+### Create ARM Resource Group
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $resourceGroupLocation `
+    -Verbose -Force
+# check 
+Get-AzureRmResourceGroup | Select-Object ResourceGroupName
+
+### Create App Service Plan
+$appServicePlan = New-AzureRmAppServicePlan `
+    -ResourceGroupName $resourceGroupName `
+    -Location $resourceGroupLocation `
+    -Name $appServicePlanName `
+    -Tier Standard `
+    -WorkerSize Small `
+    -Verbose
+
+#check 
+$appServicePlan
+
+### Create Web App
+New-AzureRmWebApp `
+    -ResourceGroupName $resourceGroupName `
+    -Location $resourceGroupLocation `
+    -AppServicePlan $appServicePlan.ServerFarmWithRichSkuName `
+    -Name $webAppName `
+    -Verbose
+
+# Go to Azure portal & check the URL of WebApp, found in HostNames
+# e.g. http://pluralsightpyhdl5nro.azurewebsites.net/    
