004ae5f0267debb37f55ced1e120c00e2a8ae48a
sagarnikam123 <sagarnikam123@gmail.com>
2016-07-25 09:12:04 +0530

    operators & symbol literals

---
+++ udemy/learningScalaFromScratch/5_typesAndOperations/3_operatorsAndSymbolLiterals.scala
@@ -0,0 +1,25 @@
+def updateRecordByName(r: Symbol, value: Any){
+/// any random code goes over here
+}
+
+updateRecordByName('favouriteAlbum, "Ok Computer")
+
+val s = 'aSymbol
+s.name
+
+val bool = true
+val fool = false
+
+val sum = 1 + 2
+val sumMore = (1). + (2)
+val longSum = 1 + 2L
+val s = "Hello world"
+s indexOf 'o'
+s indexOf('o', 5)
+
+-2.0
+(2.0).unary_-
+val s = "Hello, world"
+s.toLowerCase()
+
+1.2 + 2.3
