610729554981234566d529cc15a4cfe50cd90bb7
sagarnikam123 <sagarnikam123@gmail.com>
2016-07-26 08:28:28 +0530

    arithmetic operators

---
+++ udemy/learningScalaFromScratch/5_typesAndOperations/4_arithmeticOperators.scala
@@ -0,0 +1,41 @@
+1.2 + 2.3
+3  - 1
+
+'b' - 'a'
+'c' - 'a'
+
+2L * 3L
+3l * 7l
+
+11/4
+11.00/4.00
+11.00/4.0
+11.0/4.0
+
+11 % 4
+11.00 % 4.00
+11.0f / 4.00f
+
+val neg = 1 + -3
+val y = +3
+-neg
+
+1 > 2
+1 < 2
+1.0 <= 1.0
+3.5f > 3.6f
+'a' >= 'A'
+'a' == 'A'
+
+val thisIsBoring = !true
+!thisIsBoring
+thisIsBoring
+
+val toBe = true
+val question = toBe || !toBe
+val paradox = toBe && !toBe
+
+def salt() = { println("salt"); false}
+def pepper() = { println("pepper"); true}
+pepper() && salt()
+salt() && pepper()
