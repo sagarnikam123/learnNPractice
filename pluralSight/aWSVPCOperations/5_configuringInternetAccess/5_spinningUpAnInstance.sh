7c5efac1a2f248636bbec52e0f267790338e0a61
sagarnikam123 <sagarnikam123@gmail.com>
2016-07-26 18:01:11 +0530

    spinning up an instance

---
+++ pluralSight/aWSVPCOperations/5_configuringInternetAccess/5_spinningUpAnInstance.sh
@@ -0,0 +1,10 @@
+# Use the chmod command to make sure your private key file isn't publicly viewable.
+chmod 400 podcast-keys.pem
+
+#connecting
+ssh -i podcast-keys.pem ec2-user@ec2-52-66-103-66.ap-south-1.compute.amazonaws.com
+
+# chekcing for ip addresses
+ip a 
+
+ifconfig
