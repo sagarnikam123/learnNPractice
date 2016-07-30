4de33fa51f2f94bad473463389d3864d181f99fb
sagarnikam123 <sagarnikam123@gmail.com>
2016-07-30 18:11:45 +0530

    launching web server container

---
+++ pluralSight/dockerDeepDive/10_divingDeeperWithDockerfile/5_launchingTheWebServerContainer.sh
@@ -0,0 +1,16 @@
+# check webserver image
+docker images
+
+# running container
+docker run -d -p 80:80 webserver
+
+#chekcing container running state
+docker ps
+
+# open browser & type
+localhost
+
+# stop container
+docker stop small_easley
+# or
+docker stop webserver
