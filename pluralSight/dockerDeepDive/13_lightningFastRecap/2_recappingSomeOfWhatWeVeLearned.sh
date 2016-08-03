989154bc1c51e8dee3108f4116e50a417411b98e
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-03 19:40:37 +0530

    recap

---
+++ pluralSight/dockerDeepDive/13_lightningFastRecap/2_recappingSomeOfWhatWeVeLearned.sh
@@ -0,0 +1,10 @@
+docker version
+docker info
+docker images
+docker history icc-test
+docker pull radial/coreos-pxe
+docker ps
+docker ps -a
+docker start box1
+docker build -t "recap" .
+docker run -d --name "recap2" radial/coreos-pxe
