fa0e513bcab056a81a1177f450ee410122533276
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-09 16:30:57 +0530

    help function

---
+++ dataCamp/dataAnalysisAndStatisticalInference/1_introductionToR/9_usingTheHelpFunction.R
@@ -0,0 +1,13 @@
+# Using the help function
+
+#######################################################################################################################
+#	
+#	You might wonder how you are supposed to know that it was possible to add that third argument?
+#	
+#	Thankfully, R documents all of its functions extensively. To read what a function does, and learn the arguments
+#	that are available to you, just type in a question mark followed by the name of the function in the console!
+#
+#######################################################################################################################
+
+# Look for the documentation of the read.table function in the console
+?read.table
