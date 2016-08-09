f9ce7ec932b509de0090cc6fafdcbce49ba8bc7a
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-09 16:29:44 +0530

    connecting the dots

---
+++ dataCamp/dataAnalysisAndStatisticalInference/1_introductionToR/7_connectingTheDots.R
@@ -0,0 +1,14 @@
+# Connecting the dots
+
+#######################################################################################################################
+#	
+#	By default, R creates a scatterplot with each x,y pair indicated by an open circle. 
+#	Now, if we want to connect the data points with a line, we could add a third argument:
+#	type = "l" (with l being short for line).
+#
+#######################################################################################################################
+
+# The present dataset is already loaded
+
+# Adapt the code to create a line plot
+plot(x = present$year, y = present$girls, type="l")
