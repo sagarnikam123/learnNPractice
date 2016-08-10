3e5650f0c5a58e89bcbd9691914089948c8ba6ec
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-10 16:25:19 +0530

    info 2 knowledge categorial data

---
+++ dataCamp/dataAnalysisAndStatisticalInference/2_introductionToData/10_turningInfoIntoKnowledgeCategoricalData.R
@@ -0,0 +1,20 @@
+# Turning info into knowledge - Categorical data
+
+#######################################################################################################################
+#	
+#	Another type of data you can analyze is categorical data. For this type of variable 
+#	it makes more sense to look at absolute or relative frequency.
+#	
+#	The function table() counts the number of times each kind of category occurs in a variable. 
+#	For example, to see the number of people who have smoked at least 100 cigarettes in their lifetime, 
+#	try table(cdc$smoke100) in the console.
+#	
+#	You can also get the relative frequencies by dividing the table by the amount of observations in your data frame.
+#	
+#######################################################################################################################
+
+# Create the frequency table here:
+table(cdc$genhlth)
+
+# Create the relative frequency table here:
+table(cdc$genhlth)/dim(cdc)[1]
