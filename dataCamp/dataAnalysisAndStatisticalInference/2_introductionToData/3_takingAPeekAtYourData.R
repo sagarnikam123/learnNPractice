6637561ea7383fcc64babdc9d6e9b0f423a7618d
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-10 15:19:56 +0530

    taking apeek at your data

---
+++ dataCamp/dataAnalysisAndStatisticalInference/2_introductionToData/3_takingAPeekAtYourData.R
@@ -0,0 +1,16 @@
+# Taking a peek at your data
+
+#######################################################################################################################
+#	
+#	You can have a look at the first or last few entries (rows) of your data frame with the commands 
+#	head() and tail(), respectively.
+#	
+#	Note that you could also look at all of the data frame at once by typing its name into the console. 
+#	However, since cdc has 20,000 rows, this would mean flooding the console screen. It's better to take small peeks
+#	at the data with head(), tail() or the subsetting techniques that you'll learn in a moment.
+#	
+#######################################################################################################################
+
+# Print the head and tails of the data frame:
+head(cdc)
+tail(cdc)
