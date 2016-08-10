ffb9efb9c22084be365e980d96a040d02ab7b4c6
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-10 16:25:33 +0530

    barplot

---
+++ dataCamp/dataAnalysisAndStatisticalInference/2_introductionToData/11_creatingYourFirstBarplot.R
@@ -0,0 +1,14 @@
+# Creating your first barplot
+
+#######################################################################################################################
+#
+#	Now that you have calculated the relevant frequencies in the previous exercise,
+#	you can represent them graphically. A barplot is a very appropriate type of graph for this task.
+#	
+#	Making a barplot is easy: you use the function barplot(). 
+#	This function requires you to put the table inside the barplot() command.
+#
+#######################################################################################################################
+
+# Draw the barplot:
+barplot(table(cdc$smoke100))
