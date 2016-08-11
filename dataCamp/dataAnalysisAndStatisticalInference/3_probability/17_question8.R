366293a5b41b7c7ded888a61434314c5107288ae
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-11 16:09:54 +0530

    question

---
+++ dataCamp/dataAnalysisAndStatisticalInference/3_probability/17_question8.R
@@ -0,0 +1,19 @@
+# Question 8
+
+#######################################################################################################################
+#	
+#	How does Kobe Bryant's distribution of streak lengths compare to the distribution of streak lengths 
+#	for the simulated shooter?
+#	
+#	Tip: We've already loaded kobe_streak and sim_streak for you in the workspace.
+#	
+#	Pro-tip: Again, you can draw barplots of both Kobe and the simulated shooter. 
+#	Barplots are a great help for making visual comparisons between data like these.
+#	
+#######################################################################################################################
+
+1	They are the exact same distribution.
+2	They are fairly similar to each other.
+3	They are completely different distributions with no similar characteristics.
+
+Answer - 3	They are completely different distributions with no similar characteristics.
