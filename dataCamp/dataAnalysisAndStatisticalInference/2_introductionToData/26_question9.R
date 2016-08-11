3bbb0f9e791c383d975931c9404033e89141e303
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-11 12:26:48 +0530

    question

---
+++ dataCamp/dataAnalysisAndStatisticalInference/2_introductionToData/26_question9.R
@@ -0,0 +1,16 @@
+# Question 9
+
+#######################################################################################################################
+#	
+#	Which of the following is false based on the box plot of BMI vs. general health?
+#	The cdc data frame is already loaded into the workspace.
+#	
+#######################################################################################################################
+
+1	The median BMI is roughly 25 for all general health categories, and there is a slight increase in median BMI as general health status declines (from excellent to poor).
+2	The IQR increases slightly as general health status declines (from excellent to poor).
+3	Among people with excellent health, there are some with unusually low BMIs compared to the rest of the group.
+4	The distributions of BMIs within each health status group is left skewed.
+
+
+Answer - 4	The distributions of BMIs within each health status group is left skewed.
---
+++ dataCamp/dataAnalysisAndStatisticalInference/2_introductionToData/29_question10.R
@@ -0,0 +1,15 @@
+# Question 10
+
+#######################################################################################################################
+#
+#	Based on the plot you made in the previous exercise, which of the following is true about the relationship 
+#	between weight and desired weight?
+#
+#######################################################################################################################
+
+1	moderately weak negative linear association
+2	moderately weak positive linear association
+3	moderately strong positive linear association
+4	moderately weak negative linear association
+
+Answer - 3	moderately strong positive linear association
