3ae8d70db8ff318324f1a5decda306570f6f4c98
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-12 17:57:46 +0530

    question

---
+++ dataCamp/dataAnalysisAndStatisticalInference/4_foundationsForInferenceSamplingDistributions/15_question5.R
@@ -0,0 +1,15 @@
+# Question 5
+
+#######################################################################################################################
+#	
+#	It makes intuitive sense that as the sample size increases, the center of the sampling distribution becomes a
+#	more reliable estimate for the true population mean. Also as the sample size increases, 
+#	the variability of the sampling distribution _________.
+#	
+#######################################################################################################################
+
+1	decreases
+2	increases
+3	stays the same
+
+Answer - 1	decreases
---
+++ dataCamp/dataAnalysisAndStatisticalInference/4_foundationsForInferenceSamplingDistributions/19_question6.R
@@ -0,0 +1,17 @@
+# Question 6
+
+#######################################################################################################################
+#	
+#	Which of the following is false?
+#	
+#	Tip: You can toggle between the graphs with the buttons above it.
+#	
+#######################################################################################################################
+
+1	The variability of the sampling distribution with the smaller sample size (sample_means50) is smaller than the
+	variability of the sampling distribution with the larger sample size (sample_means150).
+2	The means for the two sampling distributions are roughly similar.
+3	Both sampling distributions are symmetric.
+
+Answer - 1	The variability of the sampling distribution with the smaller sample size (sample_means50) is smaller than the
+	variability of the sampling distribution with the larger sample size (sample_means150).
