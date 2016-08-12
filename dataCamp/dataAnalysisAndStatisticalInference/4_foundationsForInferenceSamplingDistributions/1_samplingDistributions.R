4781fbac781ec45a7120c7a4d697a1b6b0bb5fa8
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-12 17:59:58 +0530

    sampling & distribution

---
+++ dataCamp/dataAnalysisAndStatisticalInference/4_foundationsForInferenceSamplingDistributions/1_samplingDistributions.R
@@ -0,0 +1,26 @@
+# Sampling distributions
+
+#######################################################################################################################
+#	
+#	Welcome back dear student! In this lab, we'll lay the foundations for statistical inference. 
+#	You'll learn a lot in this two part lab, but don't be intimidated, perseverance will be rewarded!
+#	
+#	In Part A of this lab, we investigate the ways in which the statistics from a random sample of data can 
+#	serve as point estimates for population parameters. We're interested in formulating a sampling distribution of 
+#	our estimate in order to learn about the properties of the estimate, such as its distribution.
+#	
+#	We consider real estate data from the city of Ames, Iowa. The details of every real estate transaction in 
+#	Ames is recorded by the City Assessor's office. Our particular focus for this lab will be all residential home
+#	sales in Ames between 2006 and 2010. This collection represents our population of interest (which is rare 
+#	to have access to), but we will also work with smaller samples from this population.
+#	
+#######################################################################################################################
+
+# Load the ames data frame:
+load(url("http://assets.datacamp.com/course/dasi/ames.RData"))
+
+# Make some preliminary inspections:
+names(ames)
+head(ames)
+tail(ames)
+summary(ames)
---
+++ dataCamp/dataAnalysisAndStatisticalInference/4_foundationsForInferenceSamplingDistributions/4_samplingFromThePopulation.R
@@ -0,0 +1,25 @@
+# Sampling from the population
+
+#######################################################################################################################
+#	
+#	In this lab we have access to the entire population, but this is rarely the case in real life. Gathering 
+#	information on an entire population is often extremely costly or impossible. Because of this, we often take a 
+#	sample of the population and use that to understand the properties of the population.
+#	
+#	If we were interested in estimating the mean living area in Ames based on a sample, we can use the sample() 
+#	function to sample from the population: sample(area, 50).
+#	
+#	This command collects a simple random sample of size 50 from the vector area. This is like going into the 
+#	City Assessor's database and pulling up the files on 50 random home sales. If we didn't have access to the 
+#	population data, working with these 50 files would be considerably simpler than having 
+#	to go through all 2930 home sales.
+#	
+#######################################################################################################################
+
+# Create the samples:
+samp0 <- sample(area, 50)
+samp1 <- sample(area, 50)
+
+# Draw the histograms:
+hist(samp0)
+hist(samp1)
