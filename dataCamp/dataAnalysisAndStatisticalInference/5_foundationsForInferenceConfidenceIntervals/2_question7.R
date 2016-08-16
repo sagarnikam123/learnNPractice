112bfedabbb8415f39550717ed5ecc3f964625f8
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-16 17:23:42 +0530

    question

---
+++ dataCamp/dataAnalysisAndStatisticalInference/5_foundationsForInferenceConfidenceIntervals/2_question7.R
@@ -0,0 +1,13 @@
+# Question 7
+
+#######################################################################################################################
+#	
+#	Your distribution should be similar to others' distributions who also collect random samples from this population,
+#	but it is likely not exactly the same since it's a random sample.
+#	
+######################################################################################################################
+
+1	TRUE
+2	FALSE
+
+Answer - 1	TRUE
