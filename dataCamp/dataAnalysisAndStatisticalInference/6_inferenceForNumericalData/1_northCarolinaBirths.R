573c7192dc1c3a89cca3b3828bcd422dd98b43fc
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-17 14:48:06 +0530

    data intro

---
+++ dataCamp/dataAnalysisAndStatisticalInference/6_inferenceForNumericalData/1_northCarolinaBirths.R
@@ -0,0 +1,18 @@
+# North Carolina births
+
+#######################################################################################################################
+#	
+#	Welcome! In this lab, you'll use the techniques you learned in the previous lab 
+#	to analyze births in North Carolina, USA. Vamonos!
+#	
+#	In 2004, the state of North Carolina released a large data set containing information on births recorded in their
+#	state. This data set is useful to researchers studying the relation between habits and practices of expectant 
+#	mothers and the birth of their children. We will work with a random sample of observations from this data set.
+#	
+######################################################################################################################
+
+# Load your dataset:
+load(url("http://assets.datacamp.com/course/dasi/nc.Rdata"))
+
+# List the variables in the dataset:
+names(nc)
