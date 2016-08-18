4120873bd4eac84b23f57a945e9e6045eb4bc664
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-18 16:30:15 +0530

    relation between 2 variables

---
+++ dataCamp/dataAnalysisAndStatisticalInference/6_inferenceForNumericalData/15_relationshipsBetweenTwoVariables.R
@@ -0,0 +1,12 @@
+# Relationships between two variables
+
+#######################################################################################################################
+#	
+#	When the response variable is numerical and the explanatory variable is categorical, we can evaluate the 
+#	relationship between the two variables by comparing means (or medians, or other measures) of the numerical 
+#	response variable across the levels of the explanatory categorical variable.
+#	
+######################################################################################################################
+
+# Draw your plot here:
+plot(nc$habit, nc$weight)
