ba0348704ae23e2305dfce4e4da872011d7e80c1
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-19 15:11:30 +0530

    question

---
+++ dataCamp/dataAnalysisAndStatisticalInference/6_inferenceForNumericalData/25_question9.R
@@ -0,0 +1,15 @@
+# Question 9
+
+#######################################################################################################################
+#	
+#	Which of the following methods is appropriate for testing for a difference between the average vocabulary test 
+#	scores among the various classes?
+#	
+#######################################################################################################################
+
+1	Z test
+2	T test
+3	ANOVA
+4	χ2χ2  test
+
+Answer - 3	ANOVA
