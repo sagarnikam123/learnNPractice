6a4649c97892129aa85906bcc9f464e0f4178d73
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-21 15:38:51 +0530

    method overloading

---
+++ udemy/learningScalaFromScratch/6_functionalObjects/8_methodOverloading.scala
@@ -0,0 +1,51 @@
+// need to be checked
+class Rational(n: Int, d: Int ){
+  require (d!=0)
+  private val g= gcd(n.abs, d.abs)
+  val numer = n/g
+  val denom = d/g
+  def this(n:Int) = this(n,1)
+  def + (that: Rational): Rational =
+  new Rational(
+  numer * that.denom + that.numer * denom,
+  denom * that.denom
+  )
+  def * (that: Rational) : Rational =
+  new Rational(numer * that.numer, denom * that.denom)
+  override def toString = numer + "/" + denom
+  private def gcd(a: Int, b:Int): Int =
+  if (b==0) a else gcd(b, a %b)
+}
+
+
+class Rational(n :Int, d:Int){
+  require(d!=0)
+  private val g = gcd(n.abs, d.abs)
+  val numer = n/g
+  val denom = d/g
+  def this(n: Int) = this(n,1)
+  def + (that:Rational): Rational =
+  new Rational(
+  numer * that.denom + that.numer * denom,
+  denom * thast.denom
+  )
+  def + (i: Int) : Rational =
+  new Rational(numer + i * denom, denom)
+
+  def - (that: Rational) : Rational =
+  new Rational(
+  numer * that.denom - that.numer * denom,
+  denom * that.denom
+  )
+  def - (i: Int): Rational =
+  new Rational(numer - i* denom, denom)
+
+  def * (that: Rational) : Rational =
+  new Rational(numer * that.numer, denom * that.denom)
+  def * (i: Int) : Rational =
+  new Rational(numer * i, denom)
+
+  def / (that:Rational): Rational =
+  new Rational(numer / that.numer, denom / that.denom)
+  def / (i: Int) : Rational =
+  new Rational(numer / i, denom)
