d46da0a1431eb99369369a8d8d463e9003e55b8e
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-22 13:24:31 +0530

    rise of atheism in America

---
+++ dataCamp/dataAnalysisAndStatisticalInference/7_inferenceForCategoricalData/1_theRiseOfAtheismInAmerica.R
@@ -0,0 +1,17 @@
+# The rise of atheism in America
+
+#######################################################################################################################
+#	
+#	In August of 2012, news outlets ranging from the Washington Post to the Huffington Post ran a story about the 
+#	rise of atheism in America.
+#	
+#	The source for the story was a poll that asked people, "Irrespective of whether you attend a place of worship 
+#	or not, would you say you are a religious person, not a religious person or a convinced atheist?"
+#	
+#	This type of question, which asks people to classify themselves in one way or another, is common in 
+#	polling and generates categorical data.
+#	
+#	In this lab you'll take a look at the atheism survey and explore what's at play when making inference about 
+#	population proportions using categorical data.
+#		
+######################################################################################################################
