2353ccae4fd8f9c1f431e2e8c6aeb71fad1d5c86
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-22 13:24:12 +0530

    data

---
+++ dataCamp/dataAnalysisAndStatisticalInference/7_inferenceForCategoricalData/6_theData.R
@@ -0,0 +1,15 @@
+# The data
+
+#######################################################################################################################
+#	
+#	Turn your attention to Table 6 (pages 15 and 16 of the report), which reports the sample size and response 
+#	percentages for all 57 countries.
+#	
+#	While this is a useful format to summarize the data, you will base your analysis on the original data set of 
+#	individual responses to the survey.
+#	
+######################################################################################################################
+
+# Load in the atheism data set with the help of the provided function.
+load(url("http://assets.datacamp.com/course/dasi/atheism.RData"))
+names(atheism)
