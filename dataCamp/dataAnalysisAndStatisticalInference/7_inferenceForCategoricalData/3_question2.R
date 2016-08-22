a8dac63c2c741090480cac02dd5a0c902ac9f136
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-22 13:23:11 +0530

    question

---
+++ dataCamp/dataAnalysisAndStatisticalInference/7_inferenceForCategoricalData/3_question2.R
@@ -0,0 +1,14 @@
+# Question 2
+
+#######################################################################################################################
+#	
+#	Which of the following methods were used to gather information?
+#	
+######################################################################################################################
+
+1	Face to face
+2	Telephone
+3	Internet
+4	All of the above
+
+Answer - 4	All of the above
