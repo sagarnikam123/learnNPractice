a618dd4996936028b6555fb075d23927e1629d98
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-24 13:25:55 +0530

    question

---
+++ dataCamp/dataAnalysisAndStatisticalInference/8_introductionToLinearRegression/2_question1.R
@@ -0,0 +1,22 @@
+# Question 1
+
+#######################################################################################################################
+#	
+#	In addition to runs scored, there are seven traditionally used variables in the mlb11 data set: at_bats, hits, 
+#	homeruns, bat_avg (batting average), strikeouts, stolen_bases, and wins. Even though it's not necessary for this
+#	lab, you can always take a refresher on the rules of baseball and a description of the statistics.
+#	
+#	There are also three newer variables: new_onbase (on base percentage), new_slug (slugging percentage), 
+#	and new_obs (on-base plus slugging). For the first portion of the analysis you'll consider the seven traditional 
+#	variables. At the end of the lab, you'll work with the newer variables on your own.
+#	
+#	What type of plot would you use to display the relationship between runs and one of the other numerical variables?
+#	
+#######################################################################################################################
+
+1	histogram
+2	box plot
+3	scatterplot
+4	bar plot
+
+Answer - 3	scatterplot
---
+++ dataCamp/dataAnalysisAndStatisticalInference/8_introductionToLinearRegression/3_question2.R
@@ -0,0 +1,19 @@
+# Question 2
+
+#######################################################################################################################
+#	
+#	Using the correct type of plot of the previous exercises, plot the relationship between runs and at_bats, 
+#	using at_bats as the explanatory variable.
+#	
+#	The relationship is...
+#	
+#######################################################################################################################
+
+1	linear
+2	negative
+3	horseshoe-shaped ( ∩∩ )
+4	u-shaped ( ∪∪ )
+
+plot(mlb11$at_bats,mlb11$runs)
+
+Answer - 1	linear
