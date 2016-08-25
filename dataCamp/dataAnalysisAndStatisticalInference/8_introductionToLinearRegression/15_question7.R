673a7f827bc1f683f6936793d8386aa37e18e53a
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-25 16:24:56 +0530

    question

---
+++ dataCamp/dataAnalysisAndStatisticalInference/8_introductionToLinearRegression/15_question7.R
@@ -0,0 +1,26 @@
+# Question 7
+
+#######################################################################################################################
+#	
+#	Point 2, nearly normal residuals: To check this condition, a normal probability plot of the residuals 
+#	(the graph is shown).
+#	
+#	qqnorm(m1$residuals)
+#	qqline(m1$residuals)
+#
+#	The final code statement here adds a diagonal line to the normal probability plot.
+#	
+#	You can also look at a histogram via hist(m1$residuals).
+#	
+#	Which of the following is true?
+#
+#######################################################################################################################
+
+1	The residuals are extremely right skewed, hence the normal distribution of residuals condition is not met.
+2	The residuals are extremely left skewed, hence the normal distribution of residuals condition is not met.
+3	The residuals are perfectly symmetric, hence the normal distribution of residuals condition is met.
+4	The residuals are failry symmetric, with only a slightly longer tail on the right, hence it would be appropriate
+	to deem the the normal distribution of residuals condition met.
+
+Answer - 4	The residuals are failry symmetric, with only a slightly longer tail on the right, hence it would be appropriate
+	to deem the the normal distribution of residuals condition met.
