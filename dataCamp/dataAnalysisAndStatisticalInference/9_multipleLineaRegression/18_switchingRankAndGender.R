7476c87332f7299d2b42109c1bfee42d2780b774
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-26 16:09:17 +0530

    switch Rank to Gender

---
+++ dataCamp/dataAnalysisAndStatisticalInference/9_multipleLineaRegression/18_switchingRankAndGender.R
@@ -0,0 +1,13 @@
+# Switching rank and gender
+
+#######################################################################################################################
+#			
+#	Create a new model called m_bty_rank with gender removed and rank added in.
+#			
+#######################################################################################################################
+
+# Your linear model with rank and average beauty:
+m_bty_rank <- lm(score ~ bty_avg + rank, data= evals)
+
+# View the regression output:
+summary(m_bty_rank)
