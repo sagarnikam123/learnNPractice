bd3394ae6d876ceea202a09be6e1da771eeb51ca
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-26 15:27:15 +0530

    simple linear regression

---
+++ dataCamp/dataAnalysisAndStatisticalInference/9_multipleLineaRegression/7_simpleLinearRegression.R
@@ -0,0 +1,12 @@
+# Simple Linear Regression
+
+#######################################################################################################################
+#			
+#	The fundamental phenomenon suggested by the study is that better looking teachers are evaluated more favorably.
+#	
+#	Let's create a scatterplot to see if this appears to be the case.
+#			
+#######################################################################################################################
+
+# Create a scatterplot for score and bty_avg:
+plot(x=evals$bty_avg, y=evals$score)
