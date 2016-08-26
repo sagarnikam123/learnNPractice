9952c806f922c9c64ca8bc57204d900655517875
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-26 15:26:28 +0530

    grading of professor

---
+++ dataCamp/dataAnalysisAndStatisticalInference/9_multipleLineaRegression/1_gradingTheProfessor.R
@@ -0,0 +1,18 @@
+# Grading the professor
+
+#######################################################################################################################
+#	
+#	Many college courses conclude by giving students the opportunity to evaluate the course and the instructor 
+#	anonymously. However, the use of these student evaluations as an indicator of course quality and teaching 
+#	effectiveness is often criticized because these measures may reflect the influence of non-teaching related 
+#	characteristics, such as the physical appearance of the instructor.
+#	
+#	The article titled, "Beauty in the classroom: instructors' pulchritude and putative pedagogical productivity" 
+#	(Hamermesh and Parker, 2005) found that instructors who are viewed to be better looking receive higher 
+#	instructional ratings.
+#	
+#	In this lab we will analyze the data from this study in order 
+#	to learn what goes into a positive professor evaluation.
+#			
+#######################################################################################################################
+
