02096813386c04f57dc7d7dc6006f8f8c314b44c
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-27 23:03:05 +0530

    scope

---
+++ pluralSight/javaFundamentalsPart1/2_variablesAndOperators/greetings/hello/UsingScopes7.java
@@ -0,0 +1,20 @@
+package greetings.hello;
+
+public class UsingScopes7 {
+
+	@SuppressWarnings("unused")
+	private static int age = 50;
+	
+	public static void main(String[] args) {
+		
+		{
+			int age = 25;
+			System.out.println(age);
+		}
+		
+		int age = 20;
+		System.out.println(age);
+				
+	}
+
+}
