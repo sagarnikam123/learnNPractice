ab5c63710928a5f7a35e09d67e03a49ba942ae7d
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-27 23:02:23 +0530

    variables uses

---
+++ pluralSight/javaFundamentalsPart1/2_variablesAndOperators/greetings/hello/UsingVariables4.java
@@ -0,0 +1,20 @@
+package greetings.hello;
+
+public class UsingVariables4 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+		int number = 3;
+		String name = "Joe";
+		
+		name = "Jimmy";
+		number = 5 + 2;
+		
+		System.out.println(number);
+		System.out.println(name);
+
+	}
+
+}
