194d4c260abbc4cee6cd1dcefb5b970e944e72a4
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-27 23:02:35 +0530

    naming variables

---
+++ pluralSight/javaFundamentalsPart1/2_variablesAndOperators/greetings/hello/NamingVariables5.java
@@ -0,0 +1,15 @@
+package greetings.hello;
+
+public class NamingVariables5 {
+
+	public static void main(String[] args) {
+		
+		String firstName = "Joe";
+		
+		firstName = "Jimmy";
+		
+		System.out.println(firstName);
+
+	}
+
+}
