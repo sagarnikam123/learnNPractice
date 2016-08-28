65869172159bea22320adc96c9285fd1806e4b0b
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-28 20:31:17 +0530

    string

---
+++ pluralSight/javaFundamentalsPart1/2_variablesAndOperators/greetings/hello/UsingStrings13.java
@@ -0,0 +1,18 @@
+package greetings.hello;
+
+public class UsingStrings13 {
+
+	public static void main(String[] args) {
+		
+		String firstName = "John";
+		
+		String fullName = firstName + " Sonmez";
+		
+		System.out.println(firstName);
+		System.out.println(fullName);
+		System.out.println(fullName.length());
+		System.out.println(fullName.indexOf('z'));
+
+	}
+
+}
