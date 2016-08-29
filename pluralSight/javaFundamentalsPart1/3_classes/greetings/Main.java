8eb4b7e21884d27c1d1a52052fff9229dfa87faa
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-29 22:32:59 +0530

    Printer main

---
+++ pluralSight/javaFundamentalsPart1/3_classes/greetings/Main.java
@@ -0,0 +1,22 @@
+package greetings;
+
+import printing.Printer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Printer myPrinter = new Printer();
+		myPrinter.isOn = false;
+		myPrinter.modelNumber = "ABCD";
+		
+		Printer yourPrinter = new Printer();
+		yourPrinter.isOn = true;
+		yourPrinter.modelNumber = "FGH";
+		
+		System.out.println(myPrinter.modelNumber);
+		System.out.println(yourPrinter.modelNumber);
+
+	}
+
+}
