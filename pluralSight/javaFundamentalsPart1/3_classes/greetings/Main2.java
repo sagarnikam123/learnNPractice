b99706f13cbd8ec4a52983f78264703679c82411
sagarnikam123 <sagarnikam123@gmail.com>
2016-08-30 23:28:57 +0530

    methods

---
+++ pluralSight/javaFundamentalsPart1/3_classes/greetings/Main2.java
@@ -0,0 +1,22 @@
+package greetings;
+
+import printing.Methods7;
+import printing.Printer;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		
+		Methods7 myPrinter = new Methods7();
+		myPrinter.isOn = false;
+		myPrinter.modelNumber = "ABCD";
+		
+		Printer yourPrinter = new Printer();
+		yourPrinter.isOn = true;
+		yourPrinter.modelNumber = "FGH";
+		
+		myPrinter.print();
+		
+	}
+
+}
---
+++ pluralSight/javaFundamentalsPart1/3_classes/printing/Methods7.java
@@ -0,0 +1,12 @@
+package printing;
+
+public class Methods7 {
+
+	public boolean isOn;
+	public String modelNumber;
+
+	public void print(){
+		System.out.println(modelNumber);
+	}
+}
+
