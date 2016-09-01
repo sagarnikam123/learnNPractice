80da58367ba1b7d307a67e3ed46e9e0cde52ccbd
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-01 14:56:59 +0530

    switch

---
+++ pluralSight/javaFundamentalsPart1/4_controlStatements/controlStatements/Main2.java
@@ -0,0 +1,14 @@
+package controlStatements;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+
+		Printer2 myPrinter = new Printer2(true, "MY PRINTER");
+		myPrinter.print(0);
+		myPrinter.print(1);
+		myPrinter.print(2);
+		myPrinter.print(50);
+	}
+
+}
---
+++ pluralSight/javaFundamentalsPart1/4_controlStatements/controlStatements/Printer2.java
@@ -0,0 +1,45 @@
+package controlStatements;
+
+public class Printer2 {
+
+	private boolean isOn;
+	private String modelNumber;
+	
+	public Printer2(boolean isOn, String modelNumber){
+		this.isOn = isOn;
+		this.modelNumber = modelNumber;
+	}
+	
+	public void print(int copies){
+		
+		switch(copies){
+		case 0:
+			break;
+		case 1:
+			if(isOn){
+				System.out.println(modelNumber + " is On!");
+			}
+			else{
+				System.out.println(modelNumber + " is Off!");
+			}
+			break;
+		case 2:
+			if(isOn){
+				System.out.println(modelNumber + " is On!");
+				System.out.println(modelNumber + " is On!");
+			}
+			else{
+				System.out.println(modelNumber + " is Off!");
+				System.out.println(modelNumber + " is Off!");
+			}
+			break;
+		default:
+			System.out.println("Number of copies is not supported!");
+		}
+		
+	}
+	
+	public String getModelNumber(){
+		return modelNumber;
+	}
+}
