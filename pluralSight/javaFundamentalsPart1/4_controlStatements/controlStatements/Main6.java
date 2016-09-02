ea56f1d41255871f9554b66aee61f4f8a9061f08
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-02 20:29:17 +0530

    Break-continue

---
+++ pluralSight/javaFundamentalsPart1/4_controlStatements/controlStatements/Main6.java
@@ -0,0 +1,12 @@
+package controlStatements;
+
+public class Main6 {
+
+	public static void main(String[] args) {
+
+		BreakContinue8 myPrinter = new BreakContinue8(true, "MY PRINTER");
+		myPrinter.printColors();
+		System.out.println("----");
+		myPrinter.printColors2();
+	}
+}
