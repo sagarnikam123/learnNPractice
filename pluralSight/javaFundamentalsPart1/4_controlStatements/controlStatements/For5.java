3b5aa77583f5d2860a6d7306645167a752227855
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-02 09:34:07 +0530

    for loop

---
+++ pluralSight/javaFundamentalsPart1/4_controlStatements/controlStatements/For5.java
@@ -0,0 +1,33 @@
+package controlStatements;
+
+public class For5 {
+
+	private boolean isOn;
+	private String modelNumber;
+	
+	public For5(boolean isOn, String modelNumber){
+		this.isOn = isOn;
+		this.modelNumber = modelNumber;
+	}
+	
+	public void print(int copies){
+		
+		String onStatus = "";
+		
+		if(isOn)
+			onStatus = "is On!";
+		else
+			onStatus = "is Off!";
+		
+		String textToPrint = modelNumber + onStatus;
+		
+		for(int i = 0; i < copies; i++){
+			System.out.println(textToPrint);
+		}
+		
+	}
+	
+	public String getModelNumber(){
+		return modelNumber;
+	}
+}
---
+++ pluralSight/javaFundamentalsPart1/4_controlStatements/controlStatements/Main3.java
@@ -0,0 +1,14 @@
+package controlStatements;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+
+		For5 myPrinter = new For5(true, "MY PRINTER");
+		myPrinter.print(0);
+		myPrinter.print(1);
+		myPrinter.print(5);
+		myPrinter.print(10);
+	}
+
+}
