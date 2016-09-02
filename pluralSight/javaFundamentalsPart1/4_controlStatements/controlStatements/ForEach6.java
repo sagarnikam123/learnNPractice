56523bac086b9c2ba67c84dd6bf2a537dfe61432
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-02 20:28:17 +0530

    For each

---
+++ pluralSight/javaFundamentalsPart1/4_controlStatements/controlStatements/ForEach6.java
@@ -0,0 +1,26 @@
+package controlStatements;
+
+public class ForEach6 {
+
+	@SuppressWarnings("unused")
+	private boolean isOn;
+	private String modelNumber;
+	
+	public ForEach6(boolean isOn, String modelNumber){
+		this.isOn = isOn;
+		this.modelNumber = modelNumber;
+	}
+	
+	public void printColors(){
+		
+		String[] colors = new String[]{"Red", "Green", "Blue", "Yellow", "Orange"};
+		
+		for(String currentColor : colors){
+			System.out.println(currentColor);
+		} 
+	}
+	
+	public String getModelNumber(){
+		return modelNumber;
+	}
+}
---
+++ pluralSight/javaFundamentalsPart1/4_controlStatements/controlStatements/Main4.java
@@ -0,0 +1,11 @@
+package controlStatements;
+
+public class Main4 {
+
+	public static void main(String[] args) {
+
+		ForEach6 myPrinter = new ForEach6(true, "MY PRINTER");
+		myPrinter.printColors();
+	}
+
+}
