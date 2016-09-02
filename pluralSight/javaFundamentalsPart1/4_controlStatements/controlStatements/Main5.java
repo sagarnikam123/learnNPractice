008620b2d9660984f401640392710c90a3e0bd10
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-02 20:28:43 +0530

    While

---
+++ pluralSight/javaFundamentalsPart1/4_controlStatements/controlStatements/Main5.java
@@ -0,0 +1,11 @@
+package controlStatements;
+
+public class Main5 {
+
+	public static void main(String[] args) {
+
+		While7 myPrinter = new While7(true, "MY PRINTER");
+		myPrinter.print(2);
+	}
+
+}
---
+++ pluralSight/javaFundamentalsPart1/4_controlStatements/controlStatements/While7.java
@@ -0,0 +1,34 @@
+package controlStatements;
+
+public class While7 {
+
+	private boolean isOn;
+	private String modelNumber;
+	
+	public While7(boolean isOn, String modelNumber){
+		this.isOn = isOn;
+		this.modelNumber = modelNumber;
+	}
+	
+	public void print(int copies){
+		
+		String onStatus = "";
+		
+		if(isOn)
+			onStatus = "is On!";
+		else
+			onStatus = "is Off!";
+		
+		String textToPrint = modelNumber + onStatus;
+		
+		while(copies > 0){
+			System.out.println(textToPrint);
+			copies--;
+		}
+		
+	}
+	
+	public String getModelNumber(){
+		return modelNumber;
+	}
+}
