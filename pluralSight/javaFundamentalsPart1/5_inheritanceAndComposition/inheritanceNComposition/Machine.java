a60ec651b95e8ceaff97ed8fbdf232959226c052
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-03 20:42:01 +0530

    inheritance

---
+++ pluralSight/javaFundamentalsPart1/5_inheritanceAndComposition/inheritanceNComposition/Machine.java
@@ -0,0 +1,18 @@
+package inheritanceNComposition;
+
+public class Machine {
+
+	protected boolean isOn;
+	
+	public Machine(boolean isOn){
+		this.isOn = isOn;
+	}
+	
+	public void TurnOn(){
+		isOn = true;
+	}
+	
+	public void TurnOff(){
+		isOn = false;
+	}
+}
---
+++ pluralSight/javaFundamentalsPart1/5_inheritanceAndComposition/runner/Main.java
@@ -0,0 +1,16 @@
+package runner;
+
+import inheritanceNComposition.Printer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Printer myPrinter = new Printer(true, "MY PRINTER");
+		myPrinter.print(1);
+		myPrinter.TurnOff();
+		myPrinter.print(1);
+
+	}
+
+}
