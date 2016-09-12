d894690592b11c1eef6486c1ccc65a69d1976859
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-12 22:27:18 +0530

    checked exception

---
+++ pluralSight/javaFundamentalsPart2/1_exceptions/runner/CheckedDemoMain.java
@@ -0,0 +1,15 @@
+package runner;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CheckedDemoMain {
+
+	public static void main(String[] args) throws IOException {
+		
+		File file = new File("testFile");
+		file.createNewFile();
+
+	}
+
+}
---
+++ pluralSight/javaFundamentalsPart2/1_exceptions/runner/CheckedDemoMain2.java
@@ -0,0 +1,19 @@
+package runner;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CheckedDemoMain2 {
+
+	public static void main(String[] args) {
+		
+		File file = new File("testFile");
+		try {
+			file.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
