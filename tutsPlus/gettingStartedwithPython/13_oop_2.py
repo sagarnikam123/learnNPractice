541edf6bc41dfd5a891cdb8a47788a477b03ad7c
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-13 22:17:13 +0530

    inheritance & polymorphism

---
+++ tutsPlus/gettingStartedwithPython/13_oop_2.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+
+class house:
+    def __init__(self, doors = 3):
+        self.doors = doors
+
+    def slamDoors(self):
+        for door in range(self.doors):
+            print("SLAM!")
+        print("that's a lot of slamming for a house that costs ", self.__cost)
+
+myHouse = house()
+print(myHouse.doors)
+whoseHouse = house(doors= 53)
+print(whoseHouse.doors)
+
+#inheritance
+#subclass
+class castle(house):
+    def fireCannons(self, number):
+        for cannon in range(number):
+            print("firing cannon number ", cannon, "boom !")
+
+    def payMortage(self):
+        print("reading the cannons!")
+
+#polymorphism
+class apartment(house):
+    def payMortage(self):
+        print("here's your money!")
+
+inheritedHouse = castle(doors = 5)
+print(inheritedHouse.doors)
+print(inheritedHouse.fireCannons(8))
+
+# p
+landlordsProperties = [castle(), apartment()]
+for house in landlordsProperties:
+    house.payMortage()
