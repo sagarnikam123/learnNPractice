5cc8fa43d576674b66388afc0cb9bf20752248b3
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-17 23:35:35 +0530

    tried not working

---
+++ hackerRank/python/1_introduction/8_printFunction.py
@@ -0,0 +1,27 @@
+# Print Function
+
+#######################################################################################################################
+#
+#   Read an integer N.
+#
+#   Without using any string methods, try to print the following:
+#   123...N
+#
+#   Note that "..." represents the values in between.
+#
+#   Input Format
+#   The first line contains an integer N.
+#
+#   Output Format
+#   Output the answer as explained in the task.
+#
+#   Sample Input
+#   3
+#
+#   Sample Output
+#   123
+#
+#   Tip
+#   You can use the print function inside a map(). Can you write a 1 line code to solve the problem above?
+#
+#######################################################################################################################
