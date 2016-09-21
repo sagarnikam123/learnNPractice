08177decc2c386f0977db426d90d3a7596fa9bdf
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-21 11:32:36 +0530

    solved

--- hackerRank/python/2_basicDataTypes/6_findingThePercentage.py
+++ hackerRank/python/2_basicDataTypes/6_findingThePercentage.py
@@ -33,15 +33,45 @@
 #   Marks for Malika are {51, 56, 60} whose average is (51 + 56 + 60)/3 = 56
 #
 #######################################################################################################################
+#
+#   #otherTestCases
+#   input 1
+#   2
+#   Harsh 25 26.5 28
+#   Anurag 26 28 30
+#   Harsh
+#
+#   ouput 1
+#   26.50
+#
+#   input 2
+#   2
+#   Anurag 50 60 70
+#   Prerna 100 99.5 99
+#   Prerna
+#
+#   output 2
+#   99.50
+#
+#   input 3
+#   3
+#   Gaurav 72 79 86
+#   Bhejaria 59 60 61
+#   Salman 59 59 60
+#   Salman
+#
+#   output 3
+#   59.33
+#
+#######################################################################################################################
 
 noOfStudents = int(input().strip())
-
 hashMap = {}
 
 for zz in range(noOfStudents):
     bappa = input().strip().split(' ')
     personNav = bappa[0]
-    marks = [ int(bappa[1]), int(bappa[2]), int(bappa[3]) ]
+    marks = [ float(bappa[1]), float(bappa[2]), float(bappa[3]) ]
     hashMap[personNav] = marks
 
 lastPersonName = input().strip()
@@ -50,4 +80,4 @@ total = 0
 for mark in hashMap[lastPersonName]:
     total += mark
 
-print(total/3)
+print("%.2f" %(total/3.00) )
