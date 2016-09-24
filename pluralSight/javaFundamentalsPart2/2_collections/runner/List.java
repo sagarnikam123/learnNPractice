7e5a4f4a7287ffd6f804fd16f574a08022f3eb3c
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-24 20:00:20 +0530

    modifed for PrinterList

--- pluralSight/javaFundamentalsPart2/2_collections/runner/List.java
+++ pluralSight/javaFundamentalsPart2/2_collections/runner/List.java
@@ -1,12 +1,12 @@
 package runner;
 
 import printing.BWCartridge;
-import printing.Printer;
+import printing.PrinterList;
 
 public class List {
 
 	public static void main(String[] args) {
-		Printer<BWCartridge> printer = new Printer<BWCartridge>(true, "My PRINTER", new BWCartridge());
+		PrinterList<BWCartridge> printer = new PrinterList<BWCartridge>(true, "My PRINTER", new BWCartridge());
 		printer.loadPaper(10);
 		printer.print(5);
 		printer.outputPages();
