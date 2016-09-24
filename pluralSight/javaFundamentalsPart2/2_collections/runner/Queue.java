8b6ceb11e20e2c52ee3ab84161a815c95b9e79d5
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-24 20:16:06 +0530

    Queue-LinkedList

---
+++ pluralSight/javaFundamentalsPart2/2_collections/runner/Queue.java
@@ -0,0 +1,21 @@
+package runner;
+
+import java.util.LinkedList;
+
+public class Queue {
+
+	public static void main(String[] args) {
+
+		LinkedList<String> myQueue = new LinkedList<String>();
+		
+		myQueue.offer("a");
+		myQueue.offer("b");
+		myQueue.offer("c");
+		
+		while(myQueue.peek() != null){
+			System.out.println(myQueue.poll());
+		}
+		
+	}
+
+}
