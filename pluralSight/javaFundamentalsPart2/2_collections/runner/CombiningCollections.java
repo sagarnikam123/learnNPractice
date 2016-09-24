df3a30357dc950e3d0081e15d73f9d1a234b3dd6
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-24 21:59:20 +0530

    combining collections

---
+++ pluralSight/javaFundamentalsPart2/2_collections/runner/CombiningCollections.java
@@ -0,0 +1,47 @@
+package runner;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CombiningCollections {
+
+	public static void main(String[] args) {
+		
+		Map<String,List<Integer>> testScores = new HashMap<String, List<Integer>>(); 
+		
+		List<Integer> joeScores = new ArrayList<Integer>();
+		joeScores.add(80);
+		joeScores.add(60);
+		joeScores.add(97);
+		testScores.put("Joe",joeScores);
+				
+		List<Integer> amyScores = new ArrayList<Integer>();
+		amyScores.add(82);
+		amyScores.add(98);
+		amyScores.add(90);
+		amyScores.add(99);
+		testScores.put("Amy", amyScores);
+		
+		List<Integer> fredScores = new ArrayList<Integer>();
+		fredScores.add(50);
+		fredScores.add(60);
+		testScores.put("Fred", fredScores);
+		
+		//check
+		printScores("Fred", testScores);
+		printScores("Amy", testScores);
+		printScores("Joe", testScores);
+	}
+	
+	public static void printScores(String studentName, Map<String, List<Integer>> scoresMap ){
+		
+		List<Integer> scores = scoresMap.get(studentName);
+		
+		for(int score : scores){
+			System.out.println(score);
+		}
+	}
+
+}
