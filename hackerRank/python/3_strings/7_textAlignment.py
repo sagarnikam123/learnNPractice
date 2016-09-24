d7e8e3ac65f45a0cbad1fa9c009daa2bde4973b7
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-24 10:46:04 +0530

    text alignment

--- hackerRank/python/3_strings/7_textAlignment.py
+++ hackerRank/python/3_strings/7_textAlignment.py
@@ -75,20 +75,20 @@ c = 'H'
 
 #Top Cone
 for i in range(thickness):
-    print((c*i).______(thickness-1)+c+(c*i).______(thickness-1))
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
 
 #Top Pillars
 for i in range(thickness+1):
-    print((c*thickness).______(thickness*2)+(c*thickness).______(thickness*6))
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
 
 #Middle Belt
 for i in range((thickness+1)//2):
-    print((c*thickness*5).______(thickness*6))
+    print((c*thickness*5).center(thickness*6))
 
 #Bottom Pillars
 for i in range(thickness+1):
-    print((c*thickness).______(thickness*2)+(c*thickness).______(thickness*6))
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
 
 #Bottom Cone
 for i in range(thickness):
-    print(((c*(thickness-i-1)).______(thickness)+c+(c*(thickness-i-1)).______(thickness)).______(thickness*6))  
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
