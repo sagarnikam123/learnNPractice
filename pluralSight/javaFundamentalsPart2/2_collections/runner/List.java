ab2b316a12ef04b8af022f9eaec0419320cc98c8
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-24 19:31:49 +0530

    List

---
+++ pluralSight/javaFundamentalsPart2/2_collections/runner/List.java
@@ -0,0 +1,16 @@
+package runner;
+
+import printing.BWCartridge;
+import printing.Printer;
+
+public class List {
+
+	public static void main(String[] args) {
+		Printer<BWCartridge> printer = new Printer<BWCartridge>(true, "My PRINTER", new BWCartridge());
+		printer.loadPaper(10);
+		printer.print(5);
+		printer.outputPages();
+		
+	}
+
+}
