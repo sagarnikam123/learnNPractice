a3899e07d3e7d91b83031d1b5a664cb2599c9b39
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-25 19:23:38 +0530

    Enum class

---
+++ pluralSight/javaFundamentalsPart2/3_enumerations/runner/ClassExample.java
@@ -0,0 +1,16 @@
+package runner;
+
+import printing.ColorCartridge2;
+import printing.Printer2;
+
+public class ClassExample {
+
+	public static void main(String[] args)
+	{
+		Printer2<ColorCartridge2> printer  = new Printer2<ColorCartridge2>(true, "MY PRINTER", ColorCartridge2.RED);
+		
+		printer.loadPaper(5);
+		printer.print(3);
+		printer.outputPage(2);
+	}
+}
