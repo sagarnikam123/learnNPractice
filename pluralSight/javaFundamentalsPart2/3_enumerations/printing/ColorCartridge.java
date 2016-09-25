12e06d65a6acd42b10d2a5cb0a3590384d462039
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-25 18:48:18 +0530

    added enum color types

---
+++ pluralSight/javaFundamentalsPart2/3_enumerations/printing/ColorCartridge.java
@@ -0,0 +1,20 @@
+package printing;
+
+public enum ColorCartridge implements ICartridge
+{
+	RED,
+	BLUE,
+	GREEN,
+	YELLOW;
+	
+	@Override
+	public String toString()
+	{
+		return "Color!";
+	}
+
+	@Override
+	public String getFillPercentage() {
+		return "97%";
+	}
+}
