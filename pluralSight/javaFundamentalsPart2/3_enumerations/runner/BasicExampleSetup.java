d6f14f5fc4a1c00821fb92bc83afe57a4ed123e6
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-25 18:47:39 +0530

    Enumeration basic example

---
+++ pluralSight/javaFundamentalsPart2/3_enumerations/runner/BasicExampleSetup.java
@@ -0,0 +1,28 @@
+package runner;
+
+import printing.ColorCartridge;
+import printing.Printer;
+
+public class BasicExampleSetup {
+
+	public static void main(String[] args)
+	{
+		Printer<ColorCartridge> printer  = new Printer<ColorCartridge>(true, "MY PRINTER", ColorCartridge.RED);
+		
+		switch(printer.getCartridge()){
+		
+			case BLUE :
+				System.out.println("Blue !");
+				break;
+			case RED :
+				System.out.println("Red !");
+				break;
+			case GREEN :
+				System.out.println("Green !");
+				break;
+			case YELLOW :
+				System.out.println("Yellow !");
+				break;
+		}
+	}
+}
