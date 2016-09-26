e342b37688e0130068e8dab675aa11a84f3e111c
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-26 20:18:50 +0530

    Writing to File

---
+++ pluralSight/javaFundamentalsPart2/4_io/runner/WritingToFile.java
@@ -0,0 +1,19 @@
+package runner;
+
+import printing.ColorCartridge;
+import printing.Printer2WritingToFile;
+
+public class WritingToFile {
+
+	public static void main(String[] args)
+	{
+		Printer2WritingToFile<ColorCartridge> printer  = new Printer2WritingToFile<ColorCartridge>(true, "MY PRINTER", ColorCartridge.RED);
+		
+		printer.loadPaper(5);
+		printer.print(3);
+		printer.outputPage(1);
+		
+	}
+	
+	
+}
