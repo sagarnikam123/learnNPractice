009ca5c4dd7e26701d8b8e08f158dcea3b945be5
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-26 20:19:36 +0530

    implementing a stream

---
+++ pluralSight/javaFundamentalsPart2/4_io/printing/CapitalizationReader.java
@@ -0,0 +1,23 @@
+package printing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class CapitalizationReader extends BufferedReader {
+
+	@Override
+	public String readLine() throws IOException {
+		String line = super.readLine();
+		if(line != null){
+			return line.toUpperCase();
+		}
+		return null;
+	}
+
+	public CapitalizationReader(Reader in) {
+		super(in);
+	}
+
+
+}
---
+++ pluralSight/javaFundamentalsPart2/4_io/runner/ImplementingAStream.java
@@ -0,0 +1,19 @@
+package runner;
+
+import printing.ColorCartridge;
+import printing.Printer3ImplementStream;
+
+public class ImplementingAStream {
+
+	public static void main(String[] args)
+	{
+		Printer3ImplementStream<ColorCartridge> printer  = new Printer3ImplementStream<ColorCartridge>(true, "MY PRINTER", ColorCartridge.RED);
+		
+		printer.loadPaper(5);
+		printer.print(3);
+		printer.outputPage(1);
+		
+	}
+	
+	
+}
