4b603608795bc550df6f9c4eb21483f1130f999c
sagarnikam123 <sagarnikam123@gmail.com>
2016-09-30 18:33:29 +0530

    exceptions

---
+++ tutsPlus/gettingStartedwithPython/14_exceptions.py
@@ -0,0 +1,32 @@
+# 14_exceptions
+
+# try-except-else
+try:
+    open("files/fakefile")
+except IOError:
+    print("Unable to open file")
+else:
+    print("No problem in opening the file")
+
+print("this is important")
+
+# finally
+try:
+    open("files/fakefile")
+except IOError:
+    print("Unable to open file")
+finally:
+    print("cleaning up the code")
+
+# raising exception
+def makeDinner():
+    userInput = raw_input("Please choose a dinner item: ")
+    if userInput == "ice cream" :
+        raise Exception("Not nutritious enough !")
+    if userInput == "computer":
+        raise Exception("Not a dinner item")
+
+try:
+    makeDinner()
+except Exception, explanation:
+    print "Error: ",explanation
---
+++ tutsPlus/gettingStartedwithPython/14_exceptions2.py
@@ -0,0 +1,18 @@
+# 14_exceptions
+
+# exception with class
+
+class DinnerError(Exception):
+    pass
+
+def makeDinner():
+    userInput = raw_input("Please choose a dinner item: ")
+    if userInput == "ice cream" :
+        raise DinnerError("Not nutritious enough !")
+    if userInput == "computer":
+        raise DinnerError("Not a dinner item")
+
+try:
+    makeDinner()
+except DinnerError, explanation:
+    print "Error: ",explanation
