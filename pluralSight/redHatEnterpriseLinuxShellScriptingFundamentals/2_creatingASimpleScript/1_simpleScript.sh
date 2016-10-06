b42ad9d8bf4b8e1d9582e0719b033ff4a7c9d203
sagarnikam123 <sagarnikam123@gmail.com>
2016-10-06 22:11:49 +0530

    hello world

---
+++ pluralSight/redHatEnterpriseLinuxShellScriptingFundamentals/2_creatingASimpleScript/1_simpleScript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# hashBang/Shebang
+# RHEL/Centos don't have a /bin folder.... it has /usr/bin/ folder
+
+# Sample Hello World script for PS scripting course
+
+echo "Hey there..." # Prints short text string to screen
---
+++ pluralSight/redHatEnterpriseLinuxShellScriptingFundamentals/2_creatingASimpleScript/5_executingTheScript.sh
@@ -0,0 +1,6 @@
+#changing permission
+chmod u+x 1_simpleScript.sh
+chmod u-x 1_simpleScript.sh  # to remove executable permission
+
+# Running
+/home/quanta/git/learnNPractice/pluralSight/redHatEnterpriseLinuxShellScriptingFundamentals/2_creatingASimpleScript/1_simpleScript.sh
