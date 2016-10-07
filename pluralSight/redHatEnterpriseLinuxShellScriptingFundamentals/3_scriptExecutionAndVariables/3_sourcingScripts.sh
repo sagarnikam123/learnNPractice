deebb51fa3d047248bd29d196098cbf612128b11
sagarnikam123 <sagarnikam123@gmail.com>
2016-10-07 19:52:39 +0530

    sourcing a script

---
+++ pluralSight/redHatEnterpriseLinuxShellScriptingFundamentals/3_scriptExecutionAndVariables/3_sourcingScripts.sh
@@ -0,0 +1,10 @@
+bash ../2_creatingASimpleScript/1_simpleScript.sh
+
+# sourcing a script using dot (.)
+. ../2_creatingASimpleScript/1_simpleScript.sh
+
+psight="www.pluralsight.com"
+
+
+source simpleScript2.sh
+. simpleScript2.sh
