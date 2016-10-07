be09f9b8244c13fdc59fc4519bdabcd4d1e7d8a8
sagarnikam123 <sagarnikam123@gmail.com>
2016-10-07 20:13:48 +0530

    referencing a variable

---
+++ pluralSight/redHatEnterpriseLinuxShellScriptingFundamentals/3_scriptExecutionAndVariables/4_referencingVariables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Sample Hello World script for PS scripting course
+
+echo
+echo -e "Hi $USER\n" # Prints short text string to screen
+echo  'Hi $USER with single quotes'
+
+# values entered in single are treated as literally & they are not expanded by the shell
