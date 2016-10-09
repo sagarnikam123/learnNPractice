cb5d38acd7792b8d80bfb8210b7952e7273eed3f
sagarnikam123 <sagarnikam123@gmail.com>
2016-10-09 20:31:07 +0530

    package

---
+++ pluralSight/pythonBeyondTheBasics/2_organizingLargerPrograms/1_packages.py
@@ -0,0 +1,12 @@
+# 02_01-Packages
+
+import urllib
+import urllib.request
+
+type(urllib)
+type(urllib.request)
+urllib.__path__
+urllib.request/__path__ # gives error
+
+# packages are directories
+# modules are files
