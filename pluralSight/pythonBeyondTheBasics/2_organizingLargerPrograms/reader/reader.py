335f1c83bfe85f5c79446e2dfacc31dcba5ea35a
sagarnikam123 <sagarnikam123@gmail.com>
2016-10-11 22:08:17 +0530

    changes accordingly

--- pluralSight/pythonBeyondTheBasics/2_organizingLargerPrograms/reader/reader.py
+++ pluralSight/pythonBeyondTheBasics/2_organizingLargerPrograms/reader/reader.py
@@ -1,7 +1,16 @@
+import os
+from reader.compressed import bzipped, gzipped
+
+extension_map = {
+    '.bz2' : bzipped.opener,
+    '.gz' : gzipped.opener,
+}
+
 class Reader:
     def __init__(self, filename):
-        self.filename = filename
-        self.f = open(self.filename, 'rt')
+        extension = os.path.splitext(filename)[1]
+        opener = extension_map.get(extension, open)
+        self.f = opener(filename, 'rt')
 
     def close(self):
         self.f.close()
