413772af7aa5cd490b6a4eb862b2a8653d247c08
sagarnikam123 <sagarnikam123@gmail.com>
2016-10-12 22:36:36 +0530

    __all__ variable

---
+++ pluralSight/pythonBeyondTheBasics/2_organizingLargerPrograms/7_controllingImportsWith__all__.py
@@ -0,0 +1,16 @@
+# 02_07-Controlling Imports With __all__
+
+locals()
+from reader.compressed import *
+locals()
+
+bzipped
+gzipped
+
+# restarts REPL after changes into reader.compressed.__init__.py
+locals()
+from reader.compressed import *
+locals()
+
+bz2_opener
+gzip_opener
