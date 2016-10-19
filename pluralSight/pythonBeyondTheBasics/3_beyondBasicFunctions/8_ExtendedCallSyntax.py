a2a674c2b9a2660d487e5381429dcbc85cc74876
sagarnikam123 <sagarnikam123@gmail.com>
2016-10-19 22:34:53 +0530

    extended call syntax

---
+++ pluralSight/pythonBeyondTheBasics/3_beyondBasicFunctions/8_ExtendedCallSyntax.py
@@ -0,0 +1,21 @@
+# 03_08-Extended Call Syntax
+
+def print_args(arg1, arg2, *args):
+    print(arg1)
+    print(arg2)
+    print(args)
+
+t = (11, 12, 13, 14)
+print_args(*t)
+
+def color(red, green, blue, **kwargs):
+    print("r =", red)
+    print("g =", green)
+    print("b =", blue)
+    print(kwargs)
+
+k = {'red': 21, 'green': 68, 'blue': 120, 'alpha': 52 }
+color(**k)
+
+k =dict(red=21, green=68, blue=120, alpha=52)
+color(**k)
