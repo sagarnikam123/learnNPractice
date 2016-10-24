2f0632b6fa39e9e342e8d9092a400640b45c6491
sagarnikam123 <sagarnikam123@gmail.com>
2016-10-24 22:27:48 +0530

    nonlocal

---
+++ pluralSight/pythonBeyondTheBasics/4_closuresAndDecorators/5_theNonlocalKeyword.py
@@ -0,0 +1,19 @@
+# 04_05-The Nonlocal Keyword
+
+from make_timer import make_timer
+t = make_timer()
+t()
+t
+t()
+t()
+t()
+t()
+
+t1 = make_timer()
+t2 = make_timer()
+t1()
+t1()
+t2()
+t2()
+t2()
+t()
