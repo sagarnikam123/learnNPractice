7fff385d4b56aeb27e9af744aee04fdcfe806aae
sagarnikam123 <sagarnikam123@gmail.com>
2016-10-25 23:00:42 +0530

    unicode-ascii

---
+++ pluralSight/pythonBeyondTheBasics/4_closuresAndDecorators/escape_unicode.py
@@ -0,0 +1,11 @@
+#escape_unicode.py
+
+def escape_unicode(f):
+    def wrap(*args, **kwargs):
+        x = f(*args, **kwargs)
+        return ascii(x)
+    return wrap
+
+@escape_unicode
+def northern_city():
+    return 'Tromsø'
