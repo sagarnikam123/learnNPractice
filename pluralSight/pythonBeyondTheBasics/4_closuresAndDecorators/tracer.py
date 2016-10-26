94796d72d5a93ccb425e19b99d42e3427bedbc8b
sagarnikam123 <sagarnikam123@gmail.com>
2016-10-26 23:26:13 +0530

    tracer

---
+++ pluralSight/pythonBeyondTheBasics/4_closuresAndDecorators/tracer.py
@@ -0,0 +1,17 @@
+# tracer.py
+class Trace:
+    def __init__(self):
+        self.enabled = True
+
+    def __call__(self, f):
+        def wrap(*args, **kwargs):
+            if self.enabled:
+                print('Calling {}'.format(f))
+            return f(*args, **kwargs)
+        return wrap
+
+tracer = Trace()
+
+@tracer
+def rotate_list(l):
+    return l[1:] + [l[0]]
