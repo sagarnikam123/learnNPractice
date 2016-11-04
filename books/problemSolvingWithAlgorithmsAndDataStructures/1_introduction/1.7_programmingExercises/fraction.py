53558be649e0e6f92cd129a29483f160b34c32d2
sagarnikam123 <sagarnikam123@gmail.com>
2016-11-04 21:44:11 +0530

    fraction

---
+++ books/problemSolvingWithAlgorithmsAndDataStructures/1_introduction/1.7_programmingExercises/fraction.py
@@ -0,0 +1,11 @@
+class fraction:
+
+    def __init__(self, num, den):
+        self.top = num
+        self.bottom = den
+
+    def get_num(self):
+        return self.top
+
+    def get_den(self):
+        return self.bottom
