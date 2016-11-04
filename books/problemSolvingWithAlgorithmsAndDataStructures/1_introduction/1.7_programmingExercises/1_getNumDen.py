ec0342a530753ed345430f5e07c272f8a98de29f
sagarnikam123 <sagarnikam123@gmail.com>
2016-11-04 21:44:01 +0530

    get NumDen

---
+++ books/problemSolvingWithAlgorithmsAndDataStructures/1_introduction/1.7_programmingExercises/1_getNumDen.py
@@ -0,0 +1,12 @@
+# 1_getNumDen.py
+
+#######################################################################################################################
+#
+#   1. Implement the simple methods get_num and get_den that will return the numerator and denominator of a fraction.
+#
+#######################################################################################################################
+from fraction import fraction
+x = fraction(3,5)
+
+x.get_num()
+x.get_den()
