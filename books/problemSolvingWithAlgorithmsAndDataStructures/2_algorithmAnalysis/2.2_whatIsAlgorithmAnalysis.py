96e361c0f7c6b3cf588c50948ceb5ee5ee3225d6
sagarnikam123 <sagarnikam123@gmail.com>
2016-11-05 18:17:02 +0530

    completed

--- books/problemSolvingWithAlgorithmsAndDataStructures/2_algorithmAnalysis/2.2_whatIsAlgorithmAnalysis.py
+++ books/problemSolvingWithAlgorithmsAndDataStructures/2_algorithmAnalysis/2.2_whatIsAlgorithmAnalysis.py
@@ -16,3 +16,31 @@ def foo(tom):
     return fred
 
 print(foo(10))
+
+import time
+
+def sum_of_n_2(n):
+    start = time.time()
+    the_sum = 0
+    for i in range(1, n+1):
+        the_sum = the_sum + i
+    end = time.time()
+    return the_sum, end - start
+
+for i in range(5):
+    print('Sum is %d requied %10.7f seconds' %sum_of_n_2(10000))
+
+for i in range(5):
+    print('Sum is %d requied %10.7f seconds' %sum_of_n_2(1000000))
+
+def sum_of_n_3(n):
+    start = time.time()
+    theSum = (n * (n + 1)) / 2
+    end = time.time()
+    return theSum, end - start
+
+print(sum_of_n_3(10))
+
+noToRun = [10000, 100000, 1000000, 10000000, 1000000000 ]
+for i in noToRun:
+    print('Sum is %d requied %10.7f seconds' %sum_of_n_3(i))
