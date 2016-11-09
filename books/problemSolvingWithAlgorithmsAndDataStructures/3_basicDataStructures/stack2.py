a4c3ff9cd5f6325bb8c2cfe7f9a61bf85ce784fd
sagarnikam123 <sagarnikam123@gmail.com>
2016-11-09 20:26:30 +0530

    stack

---
+++ books/problemSolvingWithAlgorithmsAndDataStructures/3_basicDataStructures/stack2.py
@@ -0,0 +1,20 @@
+#stack2.py
+class Stack2:
+
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.insert(0, item)
+
+    def pop(self):
+        return self.items.pop(0)
+
+    def peek(self):
+        return self.items[0]
+
+    def size(self):
+        return len(self.items)
