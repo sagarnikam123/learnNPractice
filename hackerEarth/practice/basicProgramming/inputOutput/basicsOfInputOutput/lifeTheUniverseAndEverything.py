4b3b2e1ac251b9b0a81e3c158172752d69596ec1
sagarnikam123 <sagarnikam123@gmail.com>
2016-11-17 08:54:25 +0530

    solved

---
+++ hackerEarth/practice/basicProgramming/inputOutput/basicsOfInputOutput/lifeTheUniverseAndEverything.py
@@ -0,0 +1,27 @@
+# Life, the Universe, and Everything
+
+#######################################################################################################################
+#
+#   Your program is to use the brute-force approach in order to find the Answer to Life, the Universe, and Everything.
+#   More precisely... rewrite small numbers from input to output. Stop processing input after reading
+#   in the number 42. All numbers at input are integers of one or two digits.
+#
+#   SAMPLE INPUT
+#   1
+#   2
+#   88
+#   42
+#   99
+#
+#   SAMPLE OUTPUT
+#   1
+#   2
+#   88
+#
+#######################################################################################################################
+
+n = int(input().strip())
+
+while n != 42 :
+    print(n)
+    n = int(input().strip())
