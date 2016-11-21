859ca33a52a5c163149a7678448e377d74c267c0
sagarnikam123 <sagarnikam123@gmail.com>
2016-11-21 16:07:20 +0530

    palindrome checker

---
+++ books/problemSolvingWithAlgorithmsAndDataStructures/3_basicDataStructures/palindromeChecker.py
@@ -0,0 +1,18 @@
+# palindrome Checker
+
+from deque import Deque
+
+def pal_checker(a_string):
+    char_deque = Deque()
+    for ch in a_string:
+        char_deque.add_rear(ch)
+    still_equal = True
+    while char_deque.size() > 1 and still_equal:
+        first = char_deque.remove_front()
+        last = char_deque.remove_rear()
+        if first != last:
+            still_equal = False
+    return still_equal
+
+print(pal_checker('lsdkjfskf'))
+print(pal_checker('radar'))
