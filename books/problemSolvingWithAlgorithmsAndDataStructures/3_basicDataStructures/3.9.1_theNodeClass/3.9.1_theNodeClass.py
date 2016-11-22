3fdde031644f830a388a4991927214bb5eb2335d
sagarnikam123 <sagarnikam123@gmail.com>
2016-11-22 15:44:11 +0530

    node try

---
+++ books/problemSolvingWithAlgorithmsAndDataStructures/3_basicDataStructures/3.9.1_theNodeClass/3.9.1_theNodeClass.py
@@ -0,0 +1,18 @@
+# 3.9.1 The Node Class
+
+from node import Node
+temp = Node(93)
+temp.get_data()
+
+from unorderedList import UnorderedList
+my_list = UnorderedList()
+my_list.add(31)
+my_list.add(77)
+my_list.add(17)
+my_list.add(93)
+my_list.add(26)
+my_list.add(54)
+
+my_list.search(17)
+my_list.remove(17)
+my_list.search(17)
