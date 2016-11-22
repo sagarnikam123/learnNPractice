09049ee5bc630cd6ee8327ab1258016169e9a5e3
sagarnikam123 <sagarnikam123@gmail.com>
2016-11-22 15:44:23 +0530

    un-ordered list

---
+++ books/problemSolvingWithAlgorithmsAndDataStructures/3_basicDataStructures/3.9.1_theNodeClass/unorderedList.py
@@ -0,0 +1,49 @@
+# 3.9.2 The Unordered List Class
+
+from node import Node
+
+class UnorderedList:
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def add(self, item):
+        temp = Node(item)
+        temp.set_next(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.get_next()
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                current = current.get_next()
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found :
+            if current.get_data() == item :
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+        if previous == None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
