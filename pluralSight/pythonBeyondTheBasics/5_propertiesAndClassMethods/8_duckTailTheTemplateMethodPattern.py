bbfd0a783d725aaa9ab2750b93d67fc248c97d6c
sagarnikam123 <sagarnikam123@gmail.com>
2016-11-28 22:03:14 +0530

    template method

---
+++ pluralSight/pythonBeyondTheBasics/5_propertiesAndClassMethods/8_duckTailTheTemplateMethodPattern.py
@@ -0,0 +1,9 @@
+# 05_08-Duck Tail The Template Method Pattern
+
+from shipping8 import *
+z1 = HeatedRefrigeratedShippingContainer.create_empty('YML', length_ft=40, celsius=-18.0)
+z1.volume_ft3
+z1.celsius = 10
+z1.celsius = -26
+z1.fahrenheit = -14.0
+z1.fahrenheit
