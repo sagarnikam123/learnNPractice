83a4e52e735f382b53625b8e36f50daca721d70a
sagarnikam123 <sagarnikam123@gmail.com>
2016-12-02 22:49:40 +0530

    duck tail

---
+++ pluralSight/pythonBeyondTheBasics/6_stringsAndRepresentations/8_duckTailBiggerIsntAlwaysBetter.py
@@ -0,0 +1,8 @@
+# 06_08-Duck Tail Bigger Isn't Always Better
+
+from table import Table
+t = Table(['First name', 'Last name'],
+['Fred', 'George', 'Scooby'],
+['Flintstone','Jetson','Doo'])
+print(str(t))
+print(repr(t))
