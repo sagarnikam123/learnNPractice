fb78947d3345a42aa91b447f97be938c27d39a29
sagarnikam123 <sagarnikam123@gmail.com>
2016-12-03 23:54:05 +0530

    runnig junit from command line

---
+++ pluralSight/unitTestingInJavaWithJUnit/4_integratingJUnit/6_runningFromTheCommandLine.sh
@@ -0,0 +1,3 @@
+# 6_Running From The Command Line
+
+java -cp /home/quanta/Desktop/ProteinTracker.jar:<junitpath>:<hamcrestpath>  org.junit.runner.JUnitCore com.simpleprogrammer.proteintracker.tests.TrackingServiceTests
