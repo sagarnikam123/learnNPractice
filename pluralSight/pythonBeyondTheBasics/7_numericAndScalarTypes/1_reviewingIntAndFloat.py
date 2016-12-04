c0a8f7b0f3e7ac39911602ad47d777f6a39c0fc0
sagarnikam123 <sagarnikam123@gmail.com>
2016-12-04 18:44:45 +0530

    review int & float

---
+++ pluralSight/pythonBeyondTheBasics/7_numericAndScalarTypes/1_reviewingIntAndFloat.py
@@ -0,0 +1,23 @@
+# 07_01-Reviewing int and float
+
+from math import factorial as fac
+fac(1000)
+
+import sys
+sys.float_info
+most_negative_float = - sys.float_info.max
+most_negative_float
+greatest_negative_float = -sys.float_info.min
+greatest_negative_float
+
+float(10)
+
+2**53
+float(2**53)
+float(2**53 + 1)
+float(2**53 + 2)
+float(2**53 + 3)
+float(2**53 + 4)
+
+0.8 - 0.7
+2/3
