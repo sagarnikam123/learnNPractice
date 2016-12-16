c081acac7f193d29671cbf6713ea454dcbca82c4
Sagar Nikam <sagarnikam123@gmail.com>
2016-12-16 12:45:04 +0530

    number base conversion

---
+++ pluralSight/pythonBeyondTheBasics/7_numericAndScalarTypes/6_numberBaseConversions.py
@@ -0,0 +1,14 @@
+# 07_06-Number Base Conversions
+
+0b101010
+0o52
+0x2a
+bin(42)
+oct(42)
+hex(42)
+hex(42)[2:]
+
+int('2a', base=16)
+int('acghd', base=18)
+int('0b111000', base=2)
+int('0o664', base=0)
