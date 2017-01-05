d01cb83287af8524ec4c480f1068df51da550b73
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-05 22:18:18 +0530

    inheritance

---
+++ pluralSight/pythonBeyondTheBasics/9_inheritanceAndSubtypePolymorphism/1_inheritanceOverview.py
@@ -0,0 +1,19 @@
+# 09_01-Inheritance Overview
+
+from base import Base
+b = Base()
+b.f()
+
+from base import Sub
+s = Sub()
+s.f()
+
+from base import Sub
+s = Sub()
+s.f()
+
+from base import Sub
+s = Sub()
+
+from base import Sub
+s = Sub()
