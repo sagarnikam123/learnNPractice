4261b0c57c07e052d36630529153ab566fe936d8
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-06 14:29:36 +0530

    install & config chef manage console

---
+++ pluralSight/planningAndInstallingChef/2_installingChef/5_installAndConfigureChefManagementConsole.sh
@@ -0,0 +1,8 @@
+# 5_Install and Configure Chef Management Console
+
+# installing chef management console
+sudo chef-server-ctl install chef-manage
+
+sudo chef-server-ctl reconfigure
+
+sudo chef-manage-ctl reconfigure
