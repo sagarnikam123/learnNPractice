c3b377e53fd94ce8dd9e8f22fdb3e6d451ee8844
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-09 16:46:18 +0530

    configure chef workstation

---
+++ pluralSight/planningAndInstallingChef/3_configuringTheChefWorkstation/1_configuringTheChefWorkstation.sh
@@ -0,0 +1,5 @@
+# 1_Configuring the Chef Workstation
+
+# download chefDK on Workstation
+wget https://packages.chef.io/files/stable/chef/12.17.44/ubuntu/16.04/chef_12.17.44-1_amd64.deb
+sudo dpkg -i chef_12.17.44-1_amd64.deb
