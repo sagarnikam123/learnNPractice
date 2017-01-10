bb2226378789a9316e2837ccb5871fff0ed531ce
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-10 14:24:18 +0530

    deploy clinet

---
+++ pluralSight/planningAndInstallingChef/4_bootstrappingChefNodes/1_deployClientServersInChefLabAzureResourceGroup.sh
@@ -0,0 +1,10 @@
+# 1_Deploy Client Servers in Chef-lab Azure Resource Group
+
+# SSH into Ubuntu/CentOS server
+# password - 123456
+ssh user@sagarnikam1232b.mylabserver.com
+
+# ping to chef server
+ping sagarnikam1231b.mylabserver.com
+
+# RDP to windows box & check connection
