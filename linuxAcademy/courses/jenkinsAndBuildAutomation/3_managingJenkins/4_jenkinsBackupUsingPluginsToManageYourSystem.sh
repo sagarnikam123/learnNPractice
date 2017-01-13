71cacc053b8d1cd5eb9916cc09675541e5bef95e
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-13 22:13:59 +0530

    backup

---
+++ linuxAcademy/courses/jenkinsAndBuildAutomation/3_managingJenkins/4_jenkinsBackupUsingPluginsToManageYourSystem.sh
@@ -0,0 +1,11 @@
+# Jenkins Backup - Using Plugins to Manage Your System
+
+su root
+mkdir /jenkins_backup
+chown jenkins:jenkins /jenkins_backup
+cd /
+ll
+
+# backup hudson configuration
+cd jenkins_backup/
+ls
