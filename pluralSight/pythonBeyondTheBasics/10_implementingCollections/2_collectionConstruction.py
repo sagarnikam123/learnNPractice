8e1f2192b2027bd5baa948e6600e8d0948881e2e
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-14 23:03:37 +0530

    collection construction

---
+++ pluralSight/pythonBeyondTheBasics/10_implementingCollections/2_collectionConstruction.py
@@ -0,0 +1,11 @@
+# 10_02-Collection Construction
+
+# ImportError: No module named 'sorted_set'
+python3 test_sorted_set.py
+python3 test_sorted_set.py
+
+# TypeError: __init__() missing 1 required positional argument: 'items'
+python3 test_sorted_set.py
+
+# make "items=None" & ADD "if items is not None else []"
+python3 test_sorted_set.py
