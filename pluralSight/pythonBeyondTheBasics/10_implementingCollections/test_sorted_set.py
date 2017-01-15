aee0666cc730eb0f260eb2b7a483d2fdb2149c7c
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-15 12:12:45 +0530

    sequence protocol

--- pluralSight/pythonBeyondTheBasics/10_implementingCollections/test_sorted_set.py
+++ pluralSight/pythonBeyondTheBasics/10_implementingCollections/test_sorted_set.py
@@ -84,5 +84,31 @@ class TestIterableProtocol(unittest.TestCase):
             index += 1
 
 
+class TestSequenceProtocol(unittest.TestCase):
+
+    def setUp(self):
+        self.s = SortedSet([1, 4, 9, 13, 15])
+
+    def test_index_zero(self):
+        self.assertEqual(self.s[0], 1)
+
+    def test_index_four(self):
+        self.assertEqual(self.s[4], 15)
+
+    def test_index_one_beyond_the_end(self):
+        with self.assertRaises(IndexError):
+            self.s[5]
+
+    def test_index_minus_one(self):
+        self.assertEqual(self.s[-1], 15)
+
+    def test_index_minus_five(self):
+        self.assertEqual(self.s[-5], 1)
+
+    def test_index_one_before_the_beginning(self):
+        with self.assertRaises(IndexError):
+            self.s[-6]
+
+
 if __name__ == '__main__':
     unittest.main()
