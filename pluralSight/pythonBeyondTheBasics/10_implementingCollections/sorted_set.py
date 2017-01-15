fe9185a2717cf7866f74741cd3502896c90c5cad
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-15 11:37:59 +0530

    len func & added set implementation

--- pluralSight/pythonBeyondTheBasics/10_implementingCollections/sorted_set.py
+++ pluralSight/pythonBeyondTheBasics/10_implementingCollections/sorted_set.py
@@ -2,7 +2,10 @@
 
 class SortedSet:
     def __init__(self, items=None):
-        self._items = sorted(items) if items is not None else []
+        self._items = sorted(set(items)) if items is not None else []
 
     def __contains__(self, item):
         return item in self._items
+
+    def __len__(self):
+        return len(self._items)
