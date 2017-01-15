0dfcfe9590b40b99c7e613052e517a47ed78d79d
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-15 11:27:50 +0530

    contains func

--- pluralSight/pythonBeyondTheBasics/10_implementingCollections/sorted_set.py
+++ pluralSight/pythonBeyondTheBasics/10_implementingCollections/sorted_set.py
@@ -3,3 +3,6 @@
 class SortedSet:
     def __init__(self, items=None):
         self._items = sorted(items) if items is not None else []
+
+    def __contains__(self, item):
+        return item in self._items
