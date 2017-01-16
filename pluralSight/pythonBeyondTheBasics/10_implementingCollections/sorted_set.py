a991b2d4446060c9ad1aa5076da4734ae9cd04ae
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-16 22:34:05 +0530

    eq & ne func

--- pluralSight/pythonBeyondTheBasics/10_implementingCollections/sorted_set.py
+++ pluralSight/pythonBeyondTheBasics/10_implementingCollections/sorted_set.py
@@ -22,3 +22,13 @@ class SortedSet:
         return "SortedSet({})".format(
             repr(self._items) if self._items else ''
         )
+
+    def __eq__(self, rhs):
+        if not isinstance(rhs, SortedSet):
+            return NotImplemented
+        return self._items == rhs._items
+
+    def __ne__(self, rhs):
+        if not isinstance(rhs, SortedSet):
+            return NotImplemented
+        return self._items != rhs._items
