090e93e49f7a880df91eaf55478df1eccb115938
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-16 22:35:43 +0530

    implementing of equality

---
+++ pluralSight/pythonBeyondTheBasics/10_implementingCollections/9_implementingEqualityAndInequality.py
@@ -0,0 +1,7 @@
+# 10_09-Implementing Equality and Inequality
+
+from sorted_set import SortedSet
+
+SortedSet([1, 2, 3]) == SortedSet([1, 2, 3])
+SortedSet([1, 2, 3]) is SortedSet([1, 2, 3])
+[1, 2, 3] == [1, 2, 3]
