9a56803c44e8634ddb9289ab687af2bc47b1cbe4
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-17 22:00:44 +0530

    sequence protocol indexing

--- pluralSight/pythonBeyondTheBasics/10_implementingCollections/test_sorted_set.py
+++ pluralSight/pythonBeyondTheBasics/10_implementingCollections/test_sorted_set.py
@@ -134,6 +134,14 @@ class TestSequenceProtocol(unittest.TestCase):
         with self.assertRaises(StopIteration):
             next(r)
 
+    def test_index_positive(self):
+        s = SortedSet([1, 5, 8, 9])
+        self.assertEqual(s.index(8), 2)
+
+    def test_index_negative(self):
+        s = SortedSet([1, 5, 8, 9])
+        with self.assertRaises(ValueError):
+            s.index(15)
 
 class TestReprProtocol(unittest.TestCase):
 
