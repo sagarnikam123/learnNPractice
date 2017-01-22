552549b002cb26c1d0555b9c04aa9c4727c2fb26
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-22 22:19:19 +0530

    handling exception with class

---
+++ pluralSight/pythonBeyondTheBasics/11_exceptionsAndErrors/handler.py
@@ -0,0 +1,18 @@
+# handler.py
+
+from random import randrange
+
+def main():
+    number = randrange(100)
+    while True:
+        try:
+            guess = int(input("? "))
+            #guess = foo(input("? "))
+        except ValueError:
+            continue
+        if guess == number:
+            print("You win!")
+            break
+
+if __name__ == '__main__':
+    main()
