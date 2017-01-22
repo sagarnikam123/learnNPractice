dd542d7a13b17ccb324f87f1935306273a8fdc2c
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-22 22:18:59 +0530

    specifying exception

---
+++ pluralSight/pythonBeyondTheBasics/11_exceptionsAndErrors/1_alwaysSpecifyAnExceptionType.py
@@ -0,0 +1,37 @@
+# 11_01-Always Specify an Exception Type
+
+python3 handler.py
+? 10
+? 37
+? 53
+? 22
+
+# ValueError: invalid literal for int() with base 10: 'seven'
+python3 handler.py
+? 45
+? 21
+? 99
+? seven
+
+python3 handler.py
+? 10
+? 17
+? seven
+? 9
+? ^C? ^C? ^C? ^C? ^C? ^C? ^C? ^C? ^C? ^C? ^C? ^C? ^Z
+
+python3 handler.py
+sadfas
+dsf
+sdf
+dsd32r23f
+
+# NameError: name 'foo' is not defined
+python3 handler.py
+
+python3 handler.py
+? 10
+? 15
+? seven
+? 3
+? ^C
