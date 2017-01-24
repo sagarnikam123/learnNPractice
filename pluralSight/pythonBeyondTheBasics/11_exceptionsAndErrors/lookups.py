5192db091ccbb7d63ac8f7479d5567d30ceb6f68
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-24 22:45:28 +0530

    Lookup Error

---
+++ pluralSight/pythonBeyondTheBasics/11_exceptionsAndErrors/lookups.py
@@ -0,0 +1,17 @@
+def lookups():
+    s = [1, 4, 6]
+    try:
+        item = s[5]
+    #except IndexError:
+    except LookupError:
+        print("Handled IndexError")
+
+    d = dict(a=65, b=66, c=67)
+    try:
+        value = d['x']
+    #except KeyError:
+    except LookupError:
+        print("Handled KeyError")
+
+if __name__ == '__main__':
+    lookups()
