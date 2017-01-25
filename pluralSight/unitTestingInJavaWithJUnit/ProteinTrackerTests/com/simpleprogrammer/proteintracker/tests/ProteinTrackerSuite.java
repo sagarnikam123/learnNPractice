44bfd0c27efc03206f2f178934502345934a685f
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-25 13:41:42 +0530

    protein tracker suite

---
+++ pluralSight/unitTestingInJavaWithJUnit/ProteinTrackerTests/com/simpleprogrammer/proteintracker/tests/ProteinTrackerSuite.java
@@ -0,0 +1,13 @@
+package com.simpleprogrammer.proteintracker.tests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	HelloJUnitTest.class,
+	TrackingServiceTest.class
+})
+
+public class ProteinTrackerSuite {
+
+}
