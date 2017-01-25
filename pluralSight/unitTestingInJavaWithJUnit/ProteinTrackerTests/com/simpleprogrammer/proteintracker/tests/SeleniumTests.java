2da34b0b21578eb6d0bd855f9ead93501fd73ad8
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-25 13:41:24 +0530

    selenium

---
+++ pluralSight/unitTestingInJavaWithJUnit/ProteinTrackerTests/com/simpleprogrammer/proteintracker/tests/SeleniumTests.java
@@ -0,0 +1,20 @@
+package com.simpleprogrammer.proteintracker.tests;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumTests {
+
+	@Test
+	public void CanOpenGoogle(){
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://google.com");
+		WebElement searchBox = driver.findElement(By.name("q"));
+		searchBox.sendKeys("Pluralsight");
+		searchBox.submit();
+	}
+	
+}
