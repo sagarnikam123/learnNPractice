e68c46c18700fb192a995af047c0873d8a8a6dcc
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-30 22:17:00 +0530

    added traceback functionality

--- pluralSight/pythonBeyondTheBasics/11_exceptionsAndErrors/chaining.py
+++ pluralSight/pythonBeyondTheBasics/11_exceptionsAndErrors/chaining.py
@@ -1,6 +1,7 @@
 # chaining.py
 
 import math
+import traceback
 
 class InclinationError(Exception):
     pass
@@ -10,3 +11,16 @@ def inclination(dx, dy):
         return math.degrees(math.atan(dy / dx))
     except ZeroDivisionError as e:
         raise InclinationError("Slope cannot be vertical") from e
+
+def main():
+    try:
+        inclination(0, 5)
+    except InclinationError as e:
+        print(e.__traceback__)
+        traceback.print_tb(e.__traceback__)
+        s = traceback.format_tb(e.__traceback__)
+        print(s)
+
+if __name__ == '__main__':
+    main()
+    print("Finished")
