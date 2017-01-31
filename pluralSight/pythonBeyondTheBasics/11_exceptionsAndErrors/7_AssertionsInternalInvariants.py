3e95c73d51e649eb96c56e1ed0b67d22be7696f9
Sagar Nikam <sagarnikam123@gmail.com>
2017-01-31 15:54:23 +0530

    assertino internal invariant

---
+++ pluralSight/pythonBeyondTheBasics/11_exceptionsAndErrors/7_AssertionsInternalInvariants.py
@@ -0,0 +1,19 @@
+# 11_07-Assertions Internal Invariants
+
+# AssertionError: The condition was false
+assert False, "The condition was false"
+
+assert True, "The condition was false"
+assert 5 > 2, "You are in a defective universe!"
+
+from internal import *
+
+modulus_four(4)
+# AssertionError: Remainder is not 2
+modulus_four(3)
+modulus_three(3)
+
+from internal import *
+modulus_four(3)
+modulus_four(4)
+modulus_three(4)
