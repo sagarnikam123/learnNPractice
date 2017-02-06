d69cc73970a9bf7490ce6c702e107f0a193e7464
Sagar Nikam <sagarnikam123@gmail.com>
2017-02-06 15:55:36 +0530

    test for Calculator

---
+++ pluralSight/javaFundamentalsCollections/com/monotonic/collections/_5_queues/c_stacks/CalculatorTest.java
@@ -0,0 +1,33 @@
+package com.monotonic.collections._5_queues.c_stacks;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+	private Calculator calculator = new Calculator();
+	
+	@Test
+	public void shouldEvaluateConstants(){
+		int result = calculator.evaluate("1");
+		assertEquals(1, result);
+	}
+	
+	@Test
+	public void shouldSupportAdding(){
+		int result = calculator.evaluate("1 + 2");
+		assertEquals(3, result);
+	}
+	
+	@Test
+	public void shouldSupportSubtraction(){
+		int result = calculator.evaluate("1 - 2");
+		assertEquals(-1, result);
+	}
+	
+	@Test
+	public void shouldComplexStatements(){
+		int result = calculator.evaluate("1 - 3 + 2 + 4");
+		assertEquals(4, result);
+	}
+}
