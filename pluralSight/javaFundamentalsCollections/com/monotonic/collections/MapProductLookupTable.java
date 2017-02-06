a54914f86f1f885147d3387f3afb0b2c6d7ab63b
Sagar Nikam <sagarnikam123@gmail.com>
2017-02-06 16:06:18 +0530

    map product lookup table

---
+++ pluralSight/javaFundamentalsCollections/com/monotonic/collections/MapProductLookupTable.java
@@ -0,0 +1,30 @@
+package com.monotonic.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapProductLookupTable implements ProductLookupTable{
+
+	private final Map<Integer, Product2> idToProduct = new HashMap<Integer, Product2>();
+	
+	@Override
+	public void addProduct(Product2 productToAdd) {
+		final int id = productToAdd.getId();
+		
+		if(idToProduct.containsKey(id)){
+			throw new IllegalArgumentException("Unable to add product, duplicate id for " + productToAdd);
+		}
+		idToProduct.put(id, productToAdd);
+	}
+
+	@Override
+	public Product2 lookupById(int id) {
+		return idToProduct.get(id);
+	}
+	
+	@Override
+	public void clear() {
+		idToProduct.clear();
+	}
+
+}
