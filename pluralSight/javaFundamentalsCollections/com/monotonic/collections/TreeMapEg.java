577018e1c41e5ffffd67380faa77530e8aeeb92a
Sagar Nikam <sagarnikam123@gmail.com>
2017-02-06 16:02:50 +0530

    treeMap example

---
+++ pluralSight/javaFundamentalsCollections/com/monotonic/collections/TreeMapEg.java
@@ -0,0 +1,89 @@
+package com.monotonic.collections;
+
+import java.util.TreeMap;
+
+import com.monotonic.map_visualiser.Console;
+import com.monotonic.map_visualiser.TreeMapVisualiser;
+
+public class TreeMapEg {
+
+	public static void main(String[] args) {
+
+		final Console console = new Console();
+		final TreeMapVisualiser visualiser = new TreeMapVisualiser(console);
+		final TreeMap<String, String> treeified = new TreeMap<>()
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				;
+		
+		for(int i = 0; i < 26; i++){
+			final String value = String.valueOf(i);
+			treeified.put(value, value);
+			console.clear();
+			visualiser.visualise(treeified);
+			console.await();
+		}
+	}
+
+}
