74977f7d37e705ea89b75d5c0c409400ec09bdfd
Sagar Nikam <sagarnikam123@gmail.com>
2017-02-09 17:29:31 +0530

    assertion performance

---
+++ pluralSight/pythonBeyondTheBasics/11_exceptionsAndErrors/9_assertionsPerformance.py
@@ -0,0 +1,7 @@
+# 11_09-Assertions Performance
+
+# without -O option
+python3 -m timeit -n 1 -s "from random import randrange; from sorted_set import SortedSet; s = SortedSet(randrange(1000) for _ in range(2000))" "[s.count(i) for i in range(1000)]"
+
+# with -O option
+python3 -O -m timeit -n 1 -s "from random import randrange; from sorted_set import SortedSet; s = SortedSet(randrange(1000) for _ in range(2000))" "[s.count(i) for i in range(1000)]"
