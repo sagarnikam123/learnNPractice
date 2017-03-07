0846ece479202f97d413285917948559a188c359
Sagar Nikam <sagarnikam123@gmail.com>
2017-03-07 13:35:56 +0530

    adding ubuntu box to vagrant

---
+++ pluralSight/puppetFundamentalsForSystemAdministrators/3_installingAndConfiguringThePuppetAgent/3_addingTheUbuntuBoxToVagrant.sh
@@ -0,0 +1,9 @@
+# 3_Adding the Ubuntu Box to Vagrant
+
+# download ubuntu box 4m here
+# https://github.com/benpiper/puppet-fundamentals-lab
+
+cd puppet-fundamentals-lab/
+# below file must be present here
+# /home/quanta/git/puppet-fundamentals-lab/trusty-server-cloudimg-amd64-vagrant-disk1.box
+vagrant box add trusty64 trusty-server-cloudimg-amd64-vagrant-disk1.box
