2492aef71fea17d0edb1462b6a70b0a6d1f70dfb
Sagar Nikam <sagarnikam123@gmail.com>
2017-03-07 11:53:54 +0530

    downloading linux boxex

---
+++ pluralSight/puppetFundamentalsForSystemAdministrators/2_installingAndConfiguringThePuppetMaster/4_downloadingTheCentOSAndUbuntuBoxes.sh
@@ -0,0 +1,14 @@
+# 4_Downloading the CentOS and Ubuntu Boxes
+
+git clone https://github.com/benpiper/puppet-fundamentals-lab
+
+# download CentOS
+http://benpiper.github.io/course-files/puppet-system-administrators-fundamentals/centos65.box.torrent
+https://www.dropbox.com/s/206lcenz3o45c79/centos65.box?dl=1
+# Ubuntu 14.04
+http://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box
+
+
+# on RHEL/CentOS Linux users: need to run the following commands after installing VirtualBox:
+sudo yum -y install kernel-devel
+sudo /etc/init.d/vboxadd setup
