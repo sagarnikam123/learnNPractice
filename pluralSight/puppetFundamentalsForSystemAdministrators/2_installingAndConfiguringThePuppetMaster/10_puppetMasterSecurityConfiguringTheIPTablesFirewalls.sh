e5071f230ba565ae1b7a8f50cd722d5f0b8aaa42
Sagar Nikam <sagarnikam123@gmail.com>
2017-03-16 16:17:42 +0530

    updated ated with firewalld

--- pluralSight/puppetFundamentalsForSystemAdministrators/2_installingAndConfiguringThePuppetMaster/10_puppetMasterSecurityConfiguringTheIPTablesFirewalls.sh
+++ pluralSight/puppetFundamentalsForSystemAdministrators/2_installingAndConfiguringThePuppetMaster/10_puppetMasterSecurityConfiguringTheIPTablesFirewalls.sh
@@ -1,10 +1,20 @@
 # 10_Puppet Master Security: Configuring the IPTables Firewalls
 
+# installing & starting firewalld for centos-7
+yum install firewalld -y
+systemctl enable firewalld
+systemctl start firewalld
+systemctl status firewalld
+
 # adding/appending port's info into iptables
 sudo nano /etc/sysconfig/iptables
 
-# add below line
+# add below line - centos-6
 -A INPUT -m state --state NEW -m tcp -p tcp --dport 8140 -j ACCEPT
+
+# for centos-7
+firewall-cmd --zone=public --add-port=8140/tcp --permanent
 
 # reload IPTables configuration
 sudo service iptables restart
+firewall-cmd --reload
