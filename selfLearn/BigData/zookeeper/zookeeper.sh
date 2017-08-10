38ab47a78a253e6f9b8363383a6913f5ec0dc75f
Sagar Nikam <sagarnikam123@gmail.com>
2017-08-10 17:15:26 +0530

    zooKeeper commands

--- selfLearn/BigData/zookeeper/zookeeper.sh
+++ selfLearn/BigData/zookeeper/zookeeper.sh
@@ -1,5 +1,7 @@
 #Zookeeper
 
+################################################################################
+
 #download
 https://zookeeper.apache.org/
 
@@ -28,3 +30,51 @@ bin/zkServer.sh  stop
 
 #startingZookeeper cli
 bin/zkCli.sh
+
+################################################################################
+
+# reading zookeeper log files
+java -cp zookeeper-3.4.10.jar:lib/log4j-1.2.16.jar:lib/slf4j-log4j12-1.6.1.jar:lib/slf4j-api-1.6.1.jar org.apache.zookeeper.server.LogFormatter data/version-2/log.1
+
+# creating (persistent) ZNode
+# create /path /data
+create /firstZNode "tomatino"
+
+# creating sequential ZNode
+create -s /SeqZNode2 "seqData2"
+
+# creating ephemeral ZNode
+create -e /ephemeralZNode "ePhiMera"
+
+# getting data
+# get /path
+get /firstZNode
+get /SeqZNode20000000002
+
+# setting watcher
+# get /path [watch] 1
+get /firstZNode 1
+
+# setting data
+# set /path /data
+set /firstZNode "Nava Data aahe"
+
+# creating children/sub-znode
+# create /parent/path/subnode/path /data
+create /firstZNode/firstChotaZNode "aatach taiyar kela"
+create /firstZNode/secondChintusaZNode "navjat"
+
+# listing children
+# ls /path
+ls /firstZNode
+
+# checking status
+# stat /path
+stat /firstZNode
+stat /ephemeralZNode
+
+# removing a zNode
+# rmr /path
+rmr /firstZNode
+
+################################################################################
