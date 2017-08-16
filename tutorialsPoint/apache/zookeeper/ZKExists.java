2835211198a93ef79ff6bc0099b20a75899ea32d
Sagar Nikam <sagarnikam123@gmail.com>
2017-08-16 17:35:04 +0530

    checking existance of ZNode

---
+++ tutorialsPoint/apache/zookeeper/ZKExists.java
@@ -0,0 +1,35 @@
+package apache.zookeeper;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+
+public class ZKExists {
+
+	private static ZooKeeper zk;
+	private static ZooKeeperConnection conn;
+	
+	// Method to check existence of znode and its status, if znode is available.
+	public static Stat znode_exists(String path) throws KeeperException, InterruptedException {
+		return zk.exists(path, true);
+	}
+	
+	public static void main(String[] args) {
+		String path = "/MeraPahlaZNode"; // Assign znode to the specified path
+		
+		try {
+			conn = new ZooKeeperConnection();
+			zk = conn.connect("localhost");
+			Stat stat = znode_exists(path); // Stat checks the path of the znode
+			
+			if(stat !=null) {
+				System.out.println("Node exists and the node version is " + stat.getVersion());
+			}else {
+				System.out.println("Node does not exists");
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage()); // Catches error messages
+		}
+		
+	}
+}
