218e7ef2a4209ad17fe2fd9bdc392561f5618b1f
Sagar Nikam <sagarnikam123@gmail.com>
2017-10-27 12:42:12 +0530

    exercise

---
+++ youTube/codeBasics/pythonTutorials/13_readWriteFileExercise.py
@@ -0,0 +1,24 @@
+f2 = open('/home/quanta/git/learnNPractice/youTube/codeBasics/pythonTutorials/data/13_readWriteFileInput.txt', 'r')
+g2 = open('/home/quanta/git/learnNPractice/youTube/codeBasics/pythonTutorials/data/13_readWriteFileInputSum.txt', 'w')
+
+numberList = []
+
+for m2 in f2:
+    black_white = m2.rstrip('\n').split(',')
+    few_sum = int(black_white[0]) + int(black_white[1])
+    g2.write("sum: " + str(few_sum) + ' | ' + m2.rstrip('\n') + '\n')
+    numberList += black_white
+f2.close()
+g2.close()
+
+
+def count_num(num):
+    total_count = 0
+    for x in numberList:
+        if int(x) == num:
+            total_count += 1
+    return total_count
+
+
+print(count_num(100))
+print(count_num(9))
