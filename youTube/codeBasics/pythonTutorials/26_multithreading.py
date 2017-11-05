4074e5615a05ad6e67c0f2058171af9d62d32721
Sagar Nikam <sagarnikam123@gmail.com>
2017-11-05 11:30:48 +0530

    multithreading

---
+++ youTube/codeBasics/pythonTutorials/26_multithreading.py
@@ -0,0 +1,62 @@
+# 26_Multithreading - Introduction
+
+import time
+
+
+def calc_square(numbers):
+    print('calculate square numbers')
+    for n in numbers:
+        time.sleep(0.2)
+        print('sqaure: ', n * n)
+
+
+def calc_cube(numbers):
+    print('calculate cube of numbers')
+    for n in numbers:
+        time.sleep(0.2)
+        print('cube:', n * n * n)
+
+
+arr = [2, 3, 8, 9]
+
+t = time.time()
+calc_square(arr)
+calc_cube(arr)
+
+print('done in : ', time.time() - t)
+print('Hah... I am done wih all my work now!')
+################################################################################
+
+import time
+import threading
+
+
+def calc_square(numbers):
+    print('calculate square numbers')
+    for n in numbers:
+        time.sleep(0.2)
+        print('sqaure: ', n * n)
+
+
+def calc_cube(numbers):
+    print('calculate cube of numbers')
+    for n in numbers:
+        time.sleep(0.2)
+        print('cube:', n * n * n)
+
+
+arr = [2, 3, 8, 9]
+
+t = time.time()
+t1 = threading.Thread(target=calc_square, args=(arr, ))
+t2 = threading.Thread(target=calc_cube, args=(arr, ))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
+print('done in : ', time.time() - t)
+print('Hah... I am done wih all my work now!')
+################################################################################
