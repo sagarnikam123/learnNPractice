73a974fab1b871fcd41f5fd920afe0e90b672081
Sagar Nikam <sagarnikam123@gmail.com>
2017-11-17 17:04:42 +0530

    solved

--- hackerRank/tracks/languages/python/1_introduction/7_writeAFunction.py
+++ hackerRank/tracks/languages/python/1_introduction/7_writeAFunction.py
@@ -48,11 +48,7 @@
 #######################################################################################################################
 
 def is_leap(year):
-    leap = False
-    # Write your logic here
-    if (year % 4 == 0) and (year % 100 == 0) and (year % 400 == 0):
-        leap = True
-    return leap
+    return year % 4 == 0 and ( year % 400 == 0 or year % 100 != 0)
 
 year = int(input().strip())
 print(is_leap(year))
