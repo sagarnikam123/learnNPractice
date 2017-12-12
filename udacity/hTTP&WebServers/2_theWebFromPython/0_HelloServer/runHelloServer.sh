f9c6477d8ceab34cb0bf925bf8fa4329bfc3032a
Sagar Nikam <sagarnikam123@gmail.com>
2017-12-12 18:08:11 +0530

    running server

---
+++ udacity/hTTP&WebServers/2_theWebFromPython/0_HelloServer/runHelloServer.sh
@@ -0,0 +1,14 @@
+# running HelloServer.py
+
+python3 HelloServer.py
+
+# open
+# http://localhost:8000/
+# http://localhost:8000/goodbye
+
+
+ncat localhost 8000
+GET / HTTP/1.1
+
+ncat localhost 8000
+GET /helloHi HTTP/1.1
