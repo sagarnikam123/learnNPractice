87ac815650894da8fe940ff1b3959466d8c6930c
Sagar Nikam <sagarnikam123@gmail.com>
2017-12-18 15:25:21 +0530

    running echo server

---
+++ udacity/hTTP&WebServers/2_theWebFromPython/1_EchoServer/runEchoServer.sh
@@ -0,0 +1,9 @@
+# running HelloServer.py
+
+python3 EchoServer.py
+
+# open
+# http://localhost:8000/
+# http://localhost:8000/bears
+# http://localhost:8000/spiders_from_mars#stardust
+# http://localhost:8000/giant-squid?color=green
