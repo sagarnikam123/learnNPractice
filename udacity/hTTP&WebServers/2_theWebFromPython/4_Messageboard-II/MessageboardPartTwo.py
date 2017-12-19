6d6900ffeb7e2fdab03cf5e85134312a6c5dc737
Sagar Nikam <sagarnikam123@gmail.com>
2017-12-19 18:07:40 +0530

    messageBoard-II

---
+++ udacity/hTTP&WebServers/2_theWebFromPython/4_Messageboard-II/MessageboardPartTwo.py
@@ -0,0 +1,44 @@
+# Messageboard Part One
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import parse_qs
+
+form = """
+<!DOCTYPE html>
+
+<title>Message Board</title>
+<form method="POST" action="http://localhost:8000">
+  <textarea name="message"></textarea>
+  <br>
+  <button type="submit">Post it!</button>
+</form>
+
+</html>
+"""
+class MessageHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        # 1. How logn was the message ?
+        length = int(self.headers.get('Content-length', 0))
+
+        # 2. Read the correct amount of data from the request
+        data = self.rfile.read(length).decode()
+
+        # 3. Extract the Message field from the request data
+        message  = parse_qs(data)['message'][0]
+
+        # send the Message field back as the response
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(message.encode())
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(form.encode())
+
+if __name__ == '__main__':
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, MessageHandler)
+    httpd.serve_forever()
---
+++ udacity/hTTP&WebServers/2_theWebFromPython/4_Messageboard-II/running.sh
@@ -0,0 +1,6 @@
+python3 MessageboardPartTwo.py
+python3 test.py
+
+# http://localhost:8000/
+# http://localhost:8000/bears
+# http://localhost:8000/udacity-rocks/my-foxes
