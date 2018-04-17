02f3a73a0b9faa1efccfda7390ca203ebc90562a
Sagar Nikam <sagarnikam123@gmail.com>
2018-04-17 17:26:05 +0530

    module installation

---
+++ others/tech/downloadingTry/python/youTube/install.sh
@@ -0,0 +1,11 @@
+# This covers various module installtion
+
+
+# https://github.com/nficano/pytube
+sudo -H pip3 install --upgrade pytube
+
+# https://github.com/rg3/youtube-dl
+sudo -H pip3 install --upgrade youtube-dl
+
+# https://github.com/mps-youtube/mps-youtube
+sudo apt install mps-youtube
