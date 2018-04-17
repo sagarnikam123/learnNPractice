0b1c4ae41e438ace2d05df519ffa9427862359cb
Sagar Nikam <sagarnikam123@gmail.com>
2018-04-17 16:26:01 +0530

    tried

---
+++ tutsPlus/gettingStartedwithPython/15_urllib.py
@@ -0,0 +1,11 @@
+# 15_urllib
+
+import urllib
+import re
+
+html = urllib.urlopen("https://jesse.sh/")
+#print html.read()
+
+#urllib.urlretrieve("https://jesse.sh/img/caniuse-fetch.png", "files/caniuseFectch.png")
+
+print re.findall( "[\W]+@[\W.]+", str(html))
