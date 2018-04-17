604292c56a81be333fb87cf7ccc88c20471311da
Sagar Nikam <sagarnikam123@gmail.com>
2018-04-17 17:37:00 +0530

    compatibility

---
+++ pluralSight/javaFundamentalsGenerics/com/monotonic/generics/_6_compatibility/c_implications_of_erasure/InstanceOf.java
@@ -0,0 +1,13 @@
+package com.monotonic.generics._6_compatibility.c_implications_of_erasure;
+
+public class InstanceOf {
+
+	public boolean equals(Object o){
+		
+		// Banned
+		if (o instanceof InstanceOf /* <T> */ ){
+			return true;
+		}
+		return false;
+	}
+}
