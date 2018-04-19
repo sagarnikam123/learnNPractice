9a390e72e9dc12935032fb7f7d4709a97c908044
Sagar Nikam <sagarnikam123@gmail.com>
2018-04-19 17:07:03 +0530

    map literals continued

---
+++ others/language/go/gotour/basics/moreTypesStructsSlicesMaps/map-literals-continued.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type vertex struct {
+	Lag, Long float64
+}
+
+var m = map[string]vertex{
+	"Bell Labs": {40.68433, -74.39967},
+	"Google":    {37.42202, -122.08408},
+}
+
+func main() {
+	fmt.Println(m)
+}
