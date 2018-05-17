4e44cd4c21d65483059e049ef743113dbad71965
Sagar Nikam <sagarnikam123@gmail.com>
2018-05-17 16:41:43 +0530

    running terraform

---
+++ pluralSight/terraformGettingStarted/other/howToRun.sh
@@ -0,0 +1,28 @@
+################################################################################
+#
+# Terraform
+#
+################################################################################
+
+# version
+terraform version
+
+# plugin initiatenalization
+terraform init
+
+# plan
+terraform plan -var-file='../secure/terraform.tfvars'
+
+# apply
+terraform apply -var-file='../secure/terraform.tfvars'
+
+# destroy
+terraform destroy -var-file='../secure/terraform.tfvars'
+
+################################################################################
+#
+# SSH
+#
+################################################################################
+
+ssh -i ap-south-1Mumbai.pem ec2-user@ec2-13-232-79-51.ap-south-1.compute.amazonaws.com
