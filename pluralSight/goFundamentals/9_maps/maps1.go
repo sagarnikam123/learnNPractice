7fcb90c42aaecccc41ac9bee11e21051978be311
Sagar Nikam <sagarnikam123@gmail.com>
2018-05-24 12:53:21 +0530

    declaring, init, print - maps

---
+++ pluralSight/goFundamentals/9_maps/maps1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	leagueTitles := make(map[string]int)
+
+	leagueTitles["Sunderland"] = 6
+	leagueTitles["Newcastle"] = 4
+
+	recentHead2Head := map[string]int{
+		"Sunderland": 5,
+		"Newcastle":  0,
+	}
+
+	fmt.Printf("\nLeague titles: %v\nRecent head to heads: %v\n", leagueTitles, recentHead2Head)
+}
