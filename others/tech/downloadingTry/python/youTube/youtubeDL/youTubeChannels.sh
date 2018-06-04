5e75dcd1ce5e3116d92deaa93f1636044bce1ff8
Sagar Nikam <sagarnikam123@gmail.com>
2018-06-04 18:47:37 +0530

    added channels

--- others/tech/downloadingTry/python/youTube/youtubeDL/youTubeChannels.sh
+++ others/tech/downloadingTry/python/youTube/youtubeDL/youTubeChannels.sh
@@ -1,16 +1,24 @@
 
 youtube-dl --format mp4 --yes-playlist --download-archive 'FILE' 'URL'
-
 youtube-dl --format mp4 --yes-playlist --ignore-errors --download-archive 'FILE' 'URL'
+
 ################################################################################
-# Go in 5 Minutes
+# Go in 5 Minutes-
 youtube-dl --format mp4 --yes-playlist --download-archive '/home/quanta/Dropbox/downloadArchive/uTubeDownloadArchive/goIn5Minutes' \
 https://www.youtube.com/channel/UC2GHqYE3fVJMncbrRd8AqcA/videos
 
-# Byte by Byte
+# Byte by Byte-
 youtube-dl --format mp4 --yes-playlist --download-archive '/home/quanta/Dropbox/downloadArchive/uTubeDownloadArchive/byteByByte' \
 https://www.youtube.com/channel/UCWSYAntBbdd2SLYUqPIxo0w/videos
 
-# Team Naach
-youtube-dl --format mp4 --yes-playlist --download-archive '/home/quanta/Dropbox/downloadArchive/uTubeDownloadArchive/teamNaach' \
+# Team Naach-
+youtube-dl --format mp4 --yes-playlist --ignore-errors --download-archive '/home/quanta/Dropbox/downloadArchive/uTubeDownloadArchive/teamNaach' \
 https://www.youtube.com/channel/UCwd-Oa3SpLAwZWqZJBQdEpw/videos
+
+# Success In Tech-
+youtube-dl --format mp4 --yes-playlist --download-archive '/home/quanta/Dropbox/downloadArchive/uTubeDownloadArchive/successInTech' \
+https://www.youtube.com/channel/UC-vYrOAmtrx9sBzJAf3x_xw/videos
+
+# Geeks for Geeks
+youtube-dl --format mp4 --yes-playlist --download-archive '/home/quanta/Dropbox/downloadArchive/uTubeDownloadArchive/geeksforGeeks' \
+https://www.youtube.com/channel/UC0RhatS1pyxInC00YKjjBqQ/videos
