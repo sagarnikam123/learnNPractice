92365321fc1bfcde816c7466e40a5ceb18e43557
Sagar Nikam <sagarnikam123@gmail.com>
2018-06-04 17:54:04 +0530

    channels downloading examples

---
+++ others/tech/downloadingTry/python/youTube/youtubeDL/youTubeChannels.sh
@@ -0,0 +1,15 @@
+
+youtube-dl --format mp4 --yes-playlist --download-archive 'FILE' 'URL'
+
+################################################################################
+# Go in 5 Minutes
+youtube-dl --format mp4 --yes-playlist --download-archive '/home/quanta/Dropbox/downloadArchive/uTubeDownloadArchive/goIn5Minutes' \
+https://www.youtube.com/channel/UC2GHqYE3fVJMncbrRd8AqcA/videos
+
+# Byte by Byte
+youtube-dl --format mp4 --yes-playlist --download-archive '/home/quanta/Dropbox/downloadArchive/uTubeDownloadArchive/byteByByte' \
+https://www.youtube.com/channel/UCWSYAntBbdd2SLYUqPIxo0w/videos
+
+# Team Naach
+youtube-dl --format mp4 --yes-playlist --download-archive '/home/quanta/Dropbox/downloadArchive/uTubeDownloadArchive/teamNaach' \
+https://www.youtube.com/channel/UCwd-Oa3SpLAwZWqZJBQdEpw/videos
