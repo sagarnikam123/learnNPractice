5b14c69aadf41e8c63ceeaf8bf116626cd90e2f9
Sagar Nikam <sagarnikam123@gmail.com>
2018-06-14 11:19:33 +0530

    ticker

---
+++ others/language/go/goByExample/tickers.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	ticker := time.NewTicker(500 * time.Millisecond)
+	go func() {
+		for t := range ticker.C {
+			fmt.Println("Tick at", t)
+		}
+	}()
+
+	time.Sleep(1600 * time.Millisecond)
+	ticker.Stop()
+	fmt.Println("Ticker stopped")
+}
