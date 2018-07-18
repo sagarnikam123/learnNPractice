57b4ad8aa707bb9f6093d979740c728481fa961d
Sagar Nikam <sagarnikam123@gmail.com>
2018-07-18 17:47:42 +0530

    EN-stop words

---
+++ youTube/sentdex/nltk/2_stopWords.py
@@ -0,0 +1,19 @@
+# words which you don't need
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+example_sentence = "This is an example showing off stop word filteration."
+stop_words = set(stopwords.words("english"))
+#print(stop_words)
+
+words = word_tokenize(example_sentence)
+print(words)
+
+#filtered_sentence = []
+#for w in words:
+#    if w not in stop_words:
+#        filtered_sentence.append(w)
+filtered_sentence = [w for w in words if not w in stop_words]
+
+print(filtered_sentence)
