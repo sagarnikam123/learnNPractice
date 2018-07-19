1f289c073230eaa905a5cf5844190d68d53269ed
Sagar Nikam <sagarnikam123@gmail.com>
2018-07-19 21:49:58 +0530

    class & instance attribute access

---
+++ udemy/theFourPillarsOfOOPInPython3ForBeginners/4_attributesAndMethods/employee.py
@@ -0,0 +1,33 @@
+# Class attribute - common to instance of a class
+#                   i.e. for every object u create for a particular class
+#                   the value remains same
+
+class Employee:
+    numberOfWorkingHours = 40
+
+employeeOne = Employee()
+employeeTwo = Employee()
+
+print(employeeOne.numberOfWorkingHours)
+print(employeeTwo.numberOfWorkingHours)
+
+# changing the class attribute value
+Employee.numberOfWorkingHours = 45
+print(employeeOne.numberOfWorkingHours)
+print(employeeTwo.numberOfWorkingHours)
+
+# Give instance attributes for a class
+employeeOne.name = "John"
+print(employeeOne.name)
+# AttributeError: 'Employee' object has no attribute 'name'
+#print(employeeTwo.name)
+employeeTwo.name = "Mary"
+print(employeeTwo.name)
+
+# change class attribute using object
+employeeOne.numberOfWorkingHours = 40
+print(employeeOne.numberOfWorkingHours)
+print(employeeTwo.numberOfWorkingHours)
+
+# Python first checks instance attributes is present on object,
+# if not then picks up class attribute
