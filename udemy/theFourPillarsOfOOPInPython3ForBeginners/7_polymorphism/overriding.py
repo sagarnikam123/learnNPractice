50d5a10cb207f5d9b391f468a23940ac2d0c69b1
Sagar Nikam <sagarnikam123@gmail.com>
2018-07-21 19:41:59 +0530

    overriding

---
+++ udemy/theFourPillarsOfOOPInPython3ForBeginners/7_polymorphism/overriding.py
@@ -0,0 +1,27 @@
+class Employee:
+    def setNumberOfWorkingHours(self):
+        self.numberOfWorkingHours = 40
+
+    def displayNumberOfWorkingHours(self):
+        print(self.numberOfWorkingHours)
+
+class Trainee(Employee):
+    def setNumberOfWorkingHours(self):
+        self.numberOfWorkingHours = 45
+
+    def resetNumberOfWorkingHours(self):
+        super().setNumberOfWorkingHours()
+
+employee = Employee()
+employee.setNumberOfWorkingHours()
+print("Number of working hours of employee:", end = ' ')
+employee.displayNumberOfWorkingHours()
+
+trainee = Trainee()
+trainee.setNumberOfWorkingHours()
+print("Number of working hours of trainee:", end = ' ')
+trainee.displayNumberOfWorkingHours()
+
+trainee.resetNumberOfWorkingHours()
+print("Number of working hours of trainee after reset:", end = ' ')
+trainee.displayNumberOfWorkingHours()
