763cf869d2df6bb69f5e95e888aa429a9a16dffc
Sagar Nikam <sagarnikam123@gmail.com>
2018-07-23 08:58:32 +0530

    abstract base class

---
+++ udemy/theFourPillarsOfOOPInPython3ForBeginners/7_polymorphism/abc.py
@@ -0,0 +1,25 @@
+# Abstract Base Class
+from abc import ABCMeta, abstractmethod
+
+class Shape(metaclass = ABCMeta):
+    @abstractmethod
+    def area(self):
+        return 0
+
+class Square(Shape):
+    side = 4
+    def area(self):
+        print("Area of Square:", self.side * self.side)
+
+class Rectangle(Shape):
+    width = 5
+    length = 10
+    def area(self):
+        print("Area of Rectangle:", self.width * self.length)
+
+square = Square()
+rectangle = Rectangle()
+square.area()
+rectangle.area()
+# TypeError: Can't instantiate abstract class Shape with abstract methods area
+#shape = Shape()
