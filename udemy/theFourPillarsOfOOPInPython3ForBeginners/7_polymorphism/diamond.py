b6927d5ca9b69a9889889b3f15fc656d80b42054
Sagar Nikam <sagarnikam123@gmail.com>
2018-07-23 08:25:54 +0530

    diamond shapre problem in overriding

---
+++ udemy/theFourPillarsOfOOPInPython3ForBeginners/7_polymorphism/diamond.py
@@ -0,0 +1,17 @@
+class A:
+    def method(self):
+        print("This method belongs to class A")
+
+class B(A):
+    def method(self):
+        print("This method belongs to class B")
+
+class C(A):
+    def method(self):
+        print("This method belongs to class C")
+
+class D(C, B):
+    pass
+
+d = D()
+d.method()
