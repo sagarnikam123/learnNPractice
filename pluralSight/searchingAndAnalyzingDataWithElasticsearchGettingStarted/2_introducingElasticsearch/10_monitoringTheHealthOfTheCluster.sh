68bca7177bb962cc6f3d5f47d77d054dabe2c41e
Sagar Nikam <sagarnikam123@gmail.com>
2018-07-25 09:06:00 +0530

    check health of cluster

---
+++ pluralSight/searchingAndAnalyzingDataWithElasticsearchGettingStarted/2_introducingElasticsearch/10_monitoringTheHealthOfTheCluster.sh
@@ -0,0 +1,13 @@
+# 10_Monitoring the Health of the Cluster
+
+# open browser- to check health of cluster
+# localhost:9200
+
+# view avaialable parameters to check
+# http://localhost:9200/_cat/
+
+# another parameters for clusters health
+# http://localhost:9200/_cat/health?v&pretty
+
+# nodes avaialable
+# localhost:9200/_cat/nodes?v&pretty
