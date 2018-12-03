164894a980f1946a7b8e009dca8a70113ae8cd18
Sagar Nikam <sagarnikam123@gmail.com>
2018-12-03 17:13:08 +0530

    single node installation

---
+++ others/tech/BigData/cassandra/install.sh
@@ -0,0 +1,10 @@
+# installing Cassandra (single node)
+
+# http://cassandra.apache.org
+
+# untar
+tar -xvzf apache-cassandra-3.11.3-bin.tar.gz
+
+cd apache-cassandra-3.11.3
+# Start Cassandra in the foreground
+bin/cassandra -f
