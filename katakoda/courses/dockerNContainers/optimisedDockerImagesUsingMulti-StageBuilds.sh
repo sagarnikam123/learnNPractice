36cb46719eed78927f8e51beb5250df8ce1b8e16
Sagar Nikam <sagarnikam123@gmail.com>
2018-12-24 15:44:35 +0530

    completed

---
+++ katakoda/courses/dockerNContainers/optimisedDockerImagesUsingMulti-StageBuilds.sh
@@ -0,0 +1,6 @@
+# Optimised Docker Images using Multi-Stage Builds
+
+################################################################################
+# Create Dockerfile
+git clone https://github.com/katacoda/golang-http-server.git
+################################################################################
