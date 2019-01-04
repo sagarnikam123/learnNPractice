5699e9c78a0e25fdd5cf7f1bae410f49ca8e553e
sagarnikam123 <sagarnikam123@gmail.com>
2019-01-04 14:19:15 +0530

    changed folder name to docker

--- katakoda/courses/dockerNContainers/optimisedDockerImagesUsingMulti-StageBuilds.sh
+++
@@ -1,6 +0,0 @@
-# Optimised Docker Images using Multi-Stage Builds
-
-################################################################################
-# Create Dockerfile
-git clone https://github.com/katacoda/golang-http-server.git
-################################################################################
