4cb3179eb50e3e79eca30d2e4413a7b5592dc504
sagarnikam123 <sagarnikam123@gmail.com>
2019-06-07 14:07:48 +0530

    completed

---
+++ qwiklabs/gcp/creatingAVirtualMachine.sh
@@ -0,0 +1,17 @@
+# Creating a Virtual Machine-GSP001
+
+gcloud auth list
+gcloud config list project
+
+# Install a NGINX web server
+sudo su -
+apt-get update
+
+apt-get install nginx -y
+ps auwx | grep nginx
+
+# Create a new instance with gcloud
+gcloud compute instances create gcelab2 --machine-type n1-standard-2 --zone asia-south1-c
+
+# SSH to VM
+gcloud compute ssh gcelab2 --zone asia-south1-c
