fdc3e6872fb508042af6e16a13d5f06f03ad70db
sagarnikam123 <sagarnikam123@gmail.com>
2019-07-01 13:14:59 +0530

    pizzaOrder with status - homework

---
+++ others/language/python/officeTraining/assignments/dominosPizzaOrder.py
@@ -0,0 +1,38 @@
+class dominosPizzaOrder(self):
+
+    """usage for creating an object of this class"""
+
+    pizzaNames = ["margherita", "cornNonion", "onionCapsicumTomato"]
+    pizzaCrusts = ["HandTossed" , "ThinCrust", "CheezeBurst", "NewHandTossed"]
+    pizzaToppings = ["Onion", "Tomato", "Capsicum" ]
+    OrderStatus = ["recieved", "preperation", "baking", "outForDelivery", "Delivered"]
+    
+    def __init__(self, name, mobNumber, orderNumber, pizzaName, pizzaCrust, pizzaTopping):
+        self.name = name
+        self.mobNumber = mobNumber
+        self.orderNumber = orderNumber
+        self.pizzaName = pizzaName
+        self.pizzaCrust = pizzaCrust
+        self.pizzaTopping = pizzaTopping
+
+    def listPizzas(self):
+        pass
+
+    def listCrusts(self):
+        pass
+
+    def listToppings(self,pizzaName):
+        """takes a Pizza Name and list its toppings."""
+        pass
+
+    def orderDetails(self):
+        """prints details of the order, including names and details"""
+        pass
+
+    def incrementStatus(self):
+        """increment the status of order to next level"""
+        pass
+
+    def orderStatus(self):
+        """returns current status"""
+        pass
