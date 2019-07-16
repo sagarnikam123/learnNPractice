bf65d70845ed9e669fa19f327d67185d0c678a7e
sagarnikam123 <sagarnikam123@gmail.com>
2019-07-16 17:16:15 +0530

    done

--- others/language/python/pythonDocsOfficial/4_moreControlFlowTools/4.8_intermezzoCodingStyle.py
+++ others/language/python/pythonDocsOfficial/4_moreControlFlowTools/4.8_intermezzoCodingStyle.py
@@ -1,1 +1,9 @@
 # 4.8. Intermezzo: Coding Style
+
+# PEP 8 -- Style Guide for Python Code
+
+# 4-space indentation, and no tabs
+# Wrap lines so that they don’t exceed 79 characters
+# When possible, put comments on a line of their own
+# UpperCamelCase for classes
+# lowercase_with_underscores for functions and methods
