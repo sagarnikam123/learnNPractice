c6d92f6bf252ab5417d99343671cf7be8c45d2bb
sagarnikam123 <sagarnikam123@gmail.com>
2019-07-29 14:54:41 +0530

    fibonnaci series programme

---
+++ others/language/python/pythonDocsOfficial/6_modules/fibo.py
@@ -0,0 +1,27 @@
+# Fibonacci numbers module
+
+def fib(n):
+	a, b = 0, 1
+	while a < n:
+		print(a, end=" ")
+		a, b = b, a+b
+	print()
+
+
+def fib2(n):    # return Fibonacci series up to n
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+
+
+print("I m from Fibo newly changed-Part-2")
+
+def abc_newlyMade():
+	pass
+
+if __name__ == "__main__":
+	import sys
+	fib(int(sys.argv[1]))
