a6eb69dca70a947781b2cf3c2f0e5b4d327de527
sagarnikam123 <sagarnikam123@gmail.com>
2019-08-07 14:58:10 +0530

    done

--- others/language/python/pythonDocsOfficial/8_errorsAndExceptions/errorsAndExceptions.py
+++ others/language/python/pythonDocsOfficial/8_errorsAndExceptions/errorsAndExceptions.py
@@ -68,3 +68,97 @@ except ValueError:
 except:
     print('Unexcepted error:', sys.exc_info()[0])
     raise
+
+import sys
+for arg in sys.argv[1:]:
+    try:
+        f = open(arg, 'r')
+    except OSError:
+        print('cannot open', arg)
+    else:
+        print(arg, 'has', len(f.readlines()), 'lines')
+
+try:
+    raise Exception('spam', 'eggs')
+except Exception as inst:
+    print(type(inst))   # the exception instance
+    print(inst.args)    # arguments stored in .args
+    print(inst) # __str__ allows args to be printed directly,
+                # but may be overridden in exception subclasses
+    x, y = inst.args    # unpack args
+    print('x =', x)
+    print('y =', y)
+
+def this_failes():
+    x = 1/0
+
+try:
+    this_failes()
+except ZeroDivisionError as err:
+    print('Handling run-time error:', err)
+
+# 8.4. Raising Exceptions
+raise NameError('HiThere')
+raise ValueError
+
+try:
+    raise NameError('HiThere')
+except NameError:
+    print('An exception flew by!')
+    raise
+
+# 8.5. User-defined Exceptions
+class Error(Exception):
+    """ Base class for exceptions in this module."""
+    pass
+
+class InputError(Error):
+    """ Exception raised for errors in the input.
+
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of error
+    """
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
+
+class TrasitionError(Error):
+    """Raised when an operation attempts a state transition that's not allowed
+
+    Attributes:
+        previous -- state at beginning of transition
+        next -- attempted new state
+        message -- explanation of why the specific transition is not allowed
+    """
+    def __init__(self, previous, next, message):
+        self.previous = previous
+        self.next = next
+        self.message = message
+
+# 8.6. Defining Clean-up Actions
+try:
+    raise KeyboardInterrupt
+finally:
+    print('Goodbye, world!')
+
+def divide(x, y):
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        print('division by zero!')
+    else:
+        print('result is', result)
+    finally:
+        print('executing finally clause')
+
+divide(2, 1)
+divide(2, 0)
+divide("2", "1")
+
+for line in open('myfile.txt'):
+    print(line, end='')
+
+with open('myfile.txt') as f:
+    for line in f:
+        print(line, end='')
