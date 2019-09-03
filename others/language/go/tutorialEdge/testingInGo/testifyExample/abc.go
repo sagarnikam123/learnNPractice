eb1c775e4d6e35cebb7b109b46b91f9aecb2d9ec
sagarnikam123 <sagarnikam123@gmail.com>
2019-09-03 12:00:03 +0530

    in-progress

---
+++ others/language/go/tutorialEdge/testingInGo/testifyExample/abc.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "fmt"
+)
+
+// Calculate return x + 2
+func Calculate(x int) (result int){
+  result = x + 2
+  return result
+}
+
+
+func main(){
+  fmt.Println("Hello World!")
+}
---
+++ others/language/go/tutorialEdge/testingInGo/testifyExample/abc_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "testing"
+  "github.com/stretchr/testify/assert"
+)
+
+/*
+func TestCalculate(t *testing.T){
+  // if Calculate(2) != 4 {
+  //   t.Error("Expected 2 + 2 equal to 4")
+  // }
+
+  assert.Equal(t, Calculate(2), 4)
+}
+
+*/
+
+func TestCalculate(t *testing.T){
+  
+}
