aa0e61f0f6d48a9410cb6867451b19450ef6de75
sagarnikam123 <sagarnikam123@gmail.com>
2020-04-07 11:55:16 +0530

    done string

---
+++ geeksForGeeks/languages/go/2_fundamentals/strings.go
@@ -0,0 +1,18 @@
+// Go program to illustrate the use of strings
+package main
+
+import "fmt"
+
+func main(){
+  // str variable which stores strings
+  str := "GeeksforGeeks"
+
+  // Display the length of the string
+  fmt.Printf("Length of the string is: %d", len(str))
+
+  // Display the string
+  fmt.Printf("\nString is: %s", str)
+
+  // Display the type of str variable
+  fmt.Printf("\nType of str is: %T \n", str)
+}
