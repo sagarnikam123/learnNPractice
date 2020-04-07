b538e48f7f069171349f4e6eb06f0d271ebe7578
sagarnikam123 <sagarnikam123@gmail.com>
2020-04-07 11:54:41 +0530

    done integer

---
+++ geeksForGeeks/languages/go/2_fundamentals/intergers.go
@@ -0,0 +1,14 @@
+// Go program to illustrate the use of integers
+package main
+
+import "fmt"
+
+func main() {
+  // Using 8-bit unsigned int
+  var X uint8 = 225
+  fmt.Println(X+1, X)
+
+  // Using 16-bit singed int
+  var Y int16 = 32767
+  fmt.Println(Y+2, Y-2)
+}
