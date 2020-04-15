3d8e7b52ebafc5a0183cf81929037e0bb4bacc28
sagarnikam123 <sagarnikam123@gmail.com>
2020-04-15 19:28:09 +0530

    done-R Basics

---
+++ edx/dataScienceRBasics/1_rBasicsFunctionsDataTypes/1.2_rBasics/1_objects.R
@@ -0,0 +1,14 @@
+# assigning values to variables
+a <- 1
+b <- 1
+c <- -1
+
+# view values
+a
+print(a)
+ls() # name of objects save in your workspace
+
+# solving the equation  x2+x−1=0
+# solving the quadratic equation
+(-b + sqrt(b^2 - 4*a*c)) / (2*a)
+(-b - sqrt(b^2 - 4*a*c)) / (2*a)
---
+++ edx/dataScienceRBasics/1_rBasicsFunctionsDataTypes/1.2_rBasics/2_functions.R
@@ -0,0 +1,40 @@
+ls # to view function definition
+ls()
+log(8)
+log(a)
+exp(1)
+log(2.718282)
+
+# nested function
+log(exp(1))
+
+# help manual
+help(log)
+?log
+
+# check arguments of a function
+args(log)
+log(8, base=2)
+log(x=8, base=2)
+log(8, 2)
+
+# arithmetic operator
+help("+")
+help("^")
+?"-"
+2^3
+
+# check all availble data
+data()
+CO2
+BOD
+
+# other mathematic objects in-built
+pi
+Inf
+
+# variable names
+solution_1 <- (-b + sqrt(b^2 - 4*a*c)) / (2*a)
+solution_2 <- (-b - sqrt(b^2 - 4*a*c)) / (2*a)
+
+# comments
