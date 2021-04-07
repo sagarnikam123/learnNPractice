74111efaf3eba3c06650bafce5dfd7b794715bbf
sagarnikam123 <sagarnikam123@gmail.com>
2021-04-07 21:06:46 +0530

    typeof

---
+++ pluralSight/javaScriptFundamentals/3_languageFeatures/13_typeof.js
@@ -0,0 +1,8 @@
+console.log(typeof(1));
+console.log(typeof(true));
+console.log(typeof('Hello'));
+console.log(typeof(function(){}));
+console.log(typeof({}));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(NaN));
