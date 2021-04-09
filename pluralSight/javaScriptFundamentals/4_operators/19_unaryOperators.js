2d1409e94ae1117610991885378f9e6a05f52ed9
sagarnikam123 <sagarnikam123@gmail.com>
2021-04-09 14:06:30 +0530

    unary operators

---
+++ pluralSight/javaScriptFundamentals/4_operators/19_unaryOperators.js
@@ -0,0 +1,16 @@
+let year = 1967;
+console.log(year); // 1967
+console.log(++year); // 1968
+
+let month = 11;
+console.log(month++); // 11
+console.log(month); // 12
+
+let quantity = "1992";
+console.log(quantity); // 1992
+console.log(typeof(quantity)); // string
+console.log(typeof(+quantity)); // number
+
+let day = "-1993";
+console.log(+day); // -1993
+console.log(-day); // 1993
