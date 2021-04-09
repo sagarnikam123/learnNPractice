18bc37dd49f24edd90c461af2bf93f7a4337a5a3
sagarnikam123 <sagarnikam123@gmail.com>
2021-04-09 14:06:15 +0530

    equality operators

---
+++ pluralSight/javaScriptFundamentals/4_operators/18_equalityOperators.js
@@ -0,0 +1,10 @@
+console.log(1 == 1); // true
+console.log(1 == true); // true
+console.log(1 === true); // false
+
+let id = 123;
+console.log(id === "123"); // false
+console.log(id == "123"); // true
+
+console.log(id != "123"); // false
+console.log(id !== "123"); // true
