76dfd1a41982adae716a1ae84a3ad3e68d51c6a6
sagarnikam123 <sagarnikam123@gmail.com>
2021-04-09 17:51:15 +0530

    relational operators-done

---
+++ pluralSight/javaScriptFundamentals/4_operators/21_relationalOperators.js
@@ -0,0 +1,20 @@
+let s1 = 'Zoo';
+let s2 = 'alphabet';
+
+if (s1 < s2){
+    console.log(true);
+} 
+else {
+    console.log(false);
+}
+
+// toUpperCase
+let j1 = 'Zoo';
+let j2 = 'alphabet';
+
+if (j1.toUpperCase() < j2.toUpperCase()){
+    console.log(true);
+} 
+else {
+    console.log(false);
+}
