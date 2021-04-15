eae2eb192f81053972e86031bd4bee118fe45183
sagarnikam123 <sagarnikam123@gmail.com>
2021-04-15 21:03:35 +0530

    array iteration

---
+++ pluralSight/javaScriptFundamentals/6_objectsAndArrays/42_arrayIteration.js
@@ -0,0 +1,27 @@
+let carIds = [
+    {carId: 123, style: 'sedan'},
+    {carId: 456, style: 'convertible'},
+    {carId: 789, style: 'sedan'}
+];
+
+carIds.forEach(car => console.log(car));
+
+carIds.forEach( (car, index) => console.log(car, index) );
+
+let result = carIds.every( car => car.carId > 0);
+console.log(result);
+
+
+let convertibles = carIds.filter(
+    car => car.style == 'convertible'
+);
+console.log(convertibles);
+
+
+let vanIds = [
+    {vanId: 123, style: 'sedan'},
+    {vanId: null, style: 'convertible'},
+    {vanId: 789, style: 'sedan'}
+];
+let vanResult = vanIds.every(van => van.vanId > 0);
+console.log(vanResult);
