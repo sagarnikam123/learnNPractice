053ecfc1d0cbe0cb19ebebdb0d548a193c321746
sagarnikam123 <sagarnikam123@gmail.com>
2021-04-17 20:07:35 +0530

    selecting DOM elements

---
+++ pluralSight/javaScriptFundamentals/8_programmingTheBOMAndDOM.js/57_selectingDOMElements.js
@@ -0,0 +1,10 @@
+let el = document.getElementById("first");
+console.log(el);
+
+let els = document.getElementsByClassName('p1');
+console.log(els);
+console.log(els[0]);
+
+let pels = document.getElementsByTagName('p');
+console.log(pels);
+console.log(pels[0]);
