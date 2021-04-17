2476d2a5758c908de5861b7fdd49d05bf490831e
sagarnikam123 <sagarnikam123@gmail.com>
2021-04-17 20:07:56 +0530

    modify DOM element

---
+++ pluralSight/javaScriptFundamentals/8_programmingTheBOMAndDOM.js/58_modifyingDOMElements.js
@@ -0,0 +1,8 @@
+let element = document.getElementById('first');
+
+element.textContent = 'New Content...';
+element.setAttribute('foo', 'fooValue');
+element.classList.add('p2');
+element.style.color = 'blue';
+
+console.log(element);
