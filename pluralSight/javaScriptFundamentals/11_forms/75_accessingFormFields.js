28e60ff066abee74a07337a26687a8feb1ef576d
sagarnikam123 <sagarnikam123@gmail.com>
2021-05-06 17:45:31 +0530

    access form fields

---
+++ pluralSight/javaScriptFundamentals/11_forms/75_accessingFormFields.js
@@ -0,0 +1,10 @@
+let form = document.getElementById('user-form');
+
+form.addEventListener('submit', event => {
+    let user = form.elements['user'];
+    let avatarFile = form.elements['avatar-file'];
+
+    console.log(user.value, avatarFile.value);
+    // prevent the browser from submitting the form 
+    event.preventDefault();
+});
