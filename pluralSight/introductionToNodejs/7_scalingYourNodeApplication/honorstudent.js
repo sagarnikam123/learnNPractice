f0552c303d575a992544a059fdba75e380c0c97a
sagarnikam123 <sagarnikam123@gmail.com>
2021-05-25 16:59:00 +0530

    checking process cmd

---
+++ pluralSight/introductionToNodejs/7_scalingYourNodeApplication/honorstudent.js
@@ -0,0 +1,12 @@
+var fun = require('./mathfun');
+
+process.on('message', function (m) {
+    if (m.cmd === 'double') {
+        console.log('hs: I was asked to double: ' + m.number);
+        fun.evenDoubler(m.number, function (err, result) {
+            process.send({ answer: result });
+        });
+    } else if (m.cmd === 'done') {
+        process.exit();
+    }
+});
