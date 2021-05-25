64a8a495485fc125ea4ea1947585c1ce6afdb157
sagarnikam123 <sagarnikam123@gmail.com>
2021-05-25 16:57:37 +0530

    child exec

---
+++ pluralSight/introductionToNodejs/7_scalingYourNodeApplication/1_childExec.js
@@ -0,0 +1,13 @@
+var exec = require('child_process').exec;
+
+// var child = exec('uptime', function (err, stdout, stderr) {
+// var child = exec('uptime | 23cut -d "," -f 1', function (err, stdout, stderr) {    
+var child = exec('uptime | cut -d "," -f 1', function (err, stdout, stderr) {
+    if (err) {
+        console.log('Error: ' + stderr);
+    } else {
+        console.log('Output is: ' + stdout);
+    }
+});
+
+console.log("PID is " + child.pid);
