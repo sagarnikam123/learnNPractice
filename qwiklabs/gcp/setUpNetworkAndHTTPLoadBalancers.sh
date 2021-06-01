d48ae38bc31f9cc721732563ce765624458ec1f9
sagarnikam123 <sagarnikam123@gmail.com>
2021-06-01 17:05:37 +0530

    not completed

--- qwiklabs/gcp/setUpNetworkAndHTTPLoadBalancers.sh
+++ qwiklabs/gcp/setUpNetworkAndHTTPLoadBalancers.sh
@@ -1,7 +1,12 @@
 # Set Up Network and HTTP Load Balancers-GSP007
 
-# Set the default region and zone for all resources
+# Task 1: Set the default region and zone for all resources
 gcloud config set compute/zone us-central1-a
 gcloud config set conpute/region us-central1
 
-# Create multiple web server instances
+
+# Task 2: Create multiple web server instances
+# Task 3: Configure the load balancing service
+# Task 4: Sending traffic to your instances
+# Task 5: Create an HTTP load balancer
+# Task 6: Testing traffic sent to your instances
