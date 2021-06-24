94af7d6cef65747e0aaf42aedb839af776b8d116
sagarnikam123 <sagarnikam123@gmail.com>
2021-06-24 10:56:20 +0530

    added remaining steps

--- qwiklabs/gcp/computeEngineQwikStartWindows.sh
+++ qwiklabs/gcp/computeEngineQwikStartWindows.sh
@@ -1,6 +1,7 @@
 # Compute Engine: Qwik Start - Windows
 
 # Setup and requirements
+###############################################################################
 # Create a virtual machine instance
     - Compute Engine > VM instances -> click Create Instance (instance-1)
     - Region-> us-central1 (Iowa), Zone-> us-central1-a
@@ -10,7 +11,27 @@
     - Identity & API access -> Service account - > Compute Engine default service account -> 
         Access Scopes - Allow default access
     - Create
+###############################################################################
+# Activate Cloud Shell
 
+# list the active account name 
+gcloud auth list
+
+# list the project ID
+gcloud config list project
+
+# To view your default region and zone
+gcloud config get-value compute/region
+gcloud config get-value compute/zone
+
+# View available regions and zones
+gcloud compute regions list
+gcloud compute zones list
+
+# To set the default region and zone in your local client
+gcloud config set compute/region us-central1
+gcloud config set compute/zone us-central1-a
+###############################################################################
 # Remote Desktop (RDP) into the Windows Server
 # Test the status of Windows Startup
 # To see whether the server instance is ready for an RDP connection
@@ -21,3 +42,7 @@ gcloud compute instances get-serial-port-output instance-1
 gcloud compute reset-windows-password instance-1 --zone us-central1-a --user admin
 
 # Copy and pasting with the RDP client
+ip_address: 104.154.231.111
+password:   
+username:   admin
+###############################################################################
