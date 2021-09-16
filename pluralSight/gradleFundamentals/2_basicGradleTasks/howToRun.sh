1005f2e35fca264147d3fa6b051c10486f756fb8
Sagar Nikam <sagarnikam123@gmail.com>
2021-09-16 15:49:03 +0530

    done

---
+++ pluralSight/gradleFundamentals/2_basicGradleTasks/howToRun.sh
@@ -0,0 +1,23 @@
+# 2_Writing Simple Tasks
+gradle tasks
+gradle tasks --all
+gradle help --task Task4
+###############################################################################
+# 3_Running Tasks
+gradle Task4
+gradle Task3
+gradle Task5
+gradle Task6
+###############################################################################
+# 4_Task Phases
+gradle Task6 # after adding doFirst
+###############################################################################
+# 5_Task Dependencies
+gradle Task6
+gradle Task5
+gradle Task7
+###############################################################################
+# 6_Setting Properties on Tasks
+gradle Task6
+gradle Task7
+###############################################################################
