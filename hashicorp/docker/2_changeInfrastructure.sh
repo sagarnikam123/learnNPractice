d53c864493d6eba2cbb4d0e87e2b977babbfc5b6
Sagar Nikam <sagarnikam123@gmail.com>
2021-11-27 17:27:42 +0530

    modify infra

---
+++ hashicorp/docker/2_changeInfrastructure.sh
@@ -0,0 +1,12 @@
+# Change Infrastructure
+
+# Change the docker_container.nginx resource under the provider block in main.tf 
+# by replacing the ports.external value of 8000 with 8080
+
+# apply execution plan
+terraform apply
+# -/+ destroy and then create replacement
+# # docker_container.nginx must be replaced
+
+# check new resource values
+terraform show
