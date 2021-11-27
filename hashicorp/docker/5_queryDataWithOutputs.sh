9a2a703965221d9cc0322fe6057db083ea16de08
Sagar Nikam <sagarnikam123@gmail.com>
2021-11-27 17:28:27 +0530

    output variables

---
+++ hashicorp/docker/5_queryDataWithOutputs.sh
@@ -0,0 +1,13 @@
+# Query Data with Outputs
+
+# Output Docker container configuration
+# create outputs.tf file & add configurations
+
+# Inspect output values
+terraform apply
+
+# Query the outputs
+terraform output
+
+# Destroy infrastructure
+terraform destroy
