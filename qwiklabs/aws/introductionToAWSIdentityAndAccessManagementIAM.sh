106a3379d3ac1ab8467e85e73aa86b7fa26bfd09
Sagar Nikam <sagarnikam123@gmail.com>
2021-12-20 21:52:26 +0530

    done

---
+++ qwiklabs/aws/introductionToAWSIdentityAndAccessManagementIAM.sh
@@ -0,0 +1,8 @@
+# Introduction to AWS Identity and Access Management (IAM)
+
+###############################################################################
+# Task 1: Explore the Users and Groups
+# Business Scenario
+# Task 2: Add Users to Groups
+# Task 3: Sign-In and Test Users
+###############################################################################
