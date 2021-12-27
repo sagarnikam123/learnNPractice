f9264be233405f34f3ff4c8626c289e6ae7124e0
Sagar Nikam <sagarnikam123@gmail.com>
2021-12-27 13:01:10 +0530

    done

---
+++ qwiklabs/aws/introductionToAWSCloudFormation.sh
@@ -0,0 +1,9 @@
+# Introduction to AWS CloudFormation
+
+###############################################################################
+
+# Task 1: Create a CloudFormation Stack
+# Task 2: Monitor CloudFormation
+# Task 3: Delete the Stack
+
+###############################################################################
