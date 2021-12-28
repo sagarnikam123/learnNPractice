a9d255473567e5ee85c717b597cc5c53945cf8de
Sagar Nikam <sagarnikam123@gmail.com>
2021-12-28 20:06:56 +0530

    done

---
+++ qwiklabs/aws/creatingAnAmazonVirtualPrivateCloudVPCWithAWSCloudFormation.sh
@@ -0,0 +1,15 @@
+# Creating an Amazon Virtual Private Cloud (VPC) with AWS CloudFormation
+
+###############################################################################
+
+# Task 1: Deploy a Stack using AWS CloudFormation
+    # dataFiles/vpc-1.yaml
+    # dataFiles/vpc-1.json
+# Task 2: Examine the VPC
+# Task 3: Updating a Stack
+    # dataFiles/vpc-2.yaml
+    # dataFiles/vpc-2.json    
+# Task 4: Viewing a Stack in CloudFormation Designer
+# Task 5: Delete the Stack
+
+###############################################################################
