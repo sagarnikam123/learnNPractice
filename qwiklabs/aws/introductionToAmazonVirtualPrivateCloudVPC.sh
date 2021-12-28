2676204d0ce85c933852b92920469a5f57bf8d52
Sagar Nikam <sagarnikam123@gmail.com>
2021-12-28 13:20:27 +0530

    done

---
+++ qwiklabs/aws/introductionToAmazonVirtualPrivateCloudVPC.sh
@@ -0,0 +1,9 @@
+# Introduction to Amazon Virtual Private Cloud (VPC)
+
+###############################################################################
+
+# Task 1: Create an Elastic IP address
+# Task 2: Create an Amazon VPC
+# Task 3: Explore your VPC
+
+###############################################################################
