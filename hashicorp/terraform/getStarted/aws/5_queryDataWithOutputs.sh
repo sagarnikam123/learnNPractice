841ca137221d1069b662dfa5ca07ee38be956f25
Sagar Nikam <sagarnikam123@gmail.com>
2021-12-30 15:00:14 +0530

    outputs

---
+++ hashicorp/terraform/getStarted/aws/5_queryDataWithOutputs.sh
@@ -0,0 +1,12 @@
+# Query Data with Outputs
+
+# Output EC2 instance configuration
+# Create a file called outputs.tf -> EC2 instance's ID & IP address
+
+# Inspect output values
+# Changes to Outputs:
+# prints output values to the screen when you apply your configuration
+terraform apply
+
+# Query the outputs
+terraform output
