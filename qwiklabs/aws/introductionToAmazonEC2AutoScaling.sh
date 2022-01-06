46c805238cdec2f430fbee7e2ab7878727c609b3
Sagar Nikam <sagarnikam123@gmail.com>
2022-01-06 19:52:27 +0530

    done

---
+++ qwiklabs/aws/introductionToAmazonEC2AutoScaling.sh
@@ -0,0 +1,10 @@
+# Introduction to Amazon EC2 Auto Scaling
+
+################################################################################
+
+# Task 1: Create a Launch Template
+# Task 2: Create an Auto Scaling Group
+# Task 3: Verify your Auto Scaling group
+# Task 3: Test Auto Scaling
+
+################################################################################
