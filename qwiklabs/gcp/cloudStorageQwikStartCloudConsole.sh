1e3e2b33cf60307c44841f2f7be514d92da8a927
Sagar Nikam <sagarnikam123@gmail.com>
2022-01-07 12:07:30 +0530

    done

---
+++ qwiklabs/gcp/cloudStorageQwikStartCloudConsole.sh
@@ -0,0 +1,11 @@
+# Cloud Storage: Qwik Start - Cloud Console
+
+################################################################################
+
+# Create a Bucket
+# Upload an object into the Bucket
+# Share a Bucket Publicly
+# Create Folders
+# Delete a folder
+
+################################################################################
