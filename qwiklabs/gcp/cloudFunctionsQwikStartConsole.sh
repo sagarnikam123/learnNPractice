45c57c0038537aae41e36db3da0a11bf1172d9c5
Sagar Nikam <sagarnikam123@gmail.com>
2022-01-08 16:28:34 +0530

    done

---
+++ qwiklabs/gcp/cloudFunctionsQwikStartConsole.sh
@@ -0,0 +1,11 @@
+# Cloud Functions: Qwik Start - Console
+
+################################################################################
+
+# Setup
+# Create a function
+# Deploy the function
+# Test the function
+# View logs
+
+################################################################################
