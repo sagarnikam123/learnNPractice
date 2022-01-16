6c05cb7b3c92c907639101407ab1052c2ed3ef10
Sagar Nikam <sagarnikam123@gmail.com>
2022-01-16 12:40:18 +0530

    done

---
+++ qwiklabs/aws/introductionToAWSLambda.sh
@@ -0,0 +1,16 @@
+# Introduction to AWS Lambda
+
+################################################################################
+
+# Task 1: Create the Amazon S3 Buckets
+    # source Bucket name - images-616082402148
+    # output Bucket name - images-616082402148-resized
+
+# Task 2: Create an AWS Lambda Function
+# https://s3-us-west-2.amazonaws.com/us-west-2-aws-training/awsu-spl/spl-88/2.3.18.prod/scripts/CreateThumbnail.zip
+# CreateThumbnail.handler
+
+# Task 3: Test Your Function
+# Task 4: Monitoring and Logging
+
+################################################################################
