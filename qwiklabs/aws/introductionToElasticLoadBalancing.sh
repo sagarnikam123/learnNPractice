718852a89882ffbf59b68856864e93932f037d4f
Sagar Nikam <sagarnikam123@gmail.com>
2022-01-16 11:38:14 +0530

    done

---
+++ qwiklabs/aws/introductionToElasticLoadBalancing.sh
@@ -0,0 +1,13 @@
+# Introduction to Elastic Load Balancing
+
+################################################################################
+
+# Task 1: Test Access to Your Web Servers
+    # WebServer01 - 54.200.150.214
+    # WebServer02 - 52.88.140.251
+# Task 2: Create an Elastic Load Balancer
+    # DNS Name - myELB-c91e5ec7dcb5475e.elb.us-west-2.amazonaws.com
+# Task 3: Test Your Load Balancer
+# Task 4: Test Your Load Balancer During a Failure
+
+################################################################################
