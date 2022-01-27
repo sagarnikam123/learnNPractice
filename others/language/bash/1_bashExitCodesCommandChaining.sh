212b5d2d72791c2fdafea6d2ae8f2de7b34a2306
Sagar Nikam <sagarnikam123@gmail.com>
2022-01-27 22:28:08 +0530

    done

---
+++ others/language/bash/1_bashExitCodesCommandChaining.sh
@@ -0,0 +1,29 @@
+# Bash exit codes & command chaining
+
+# Hello World
+echo "Hello World"
+
+# $? - exit code of last command run
+# 0 - success
+# any non zero - not success
+echo $?
+
+# failed codes
+cat wrong-file.txt
+echo $?
+
+# semicolon(;) - to run command whether failed/success
+echo "foo"; echo "bar"
+cat wrong-file.txt; echo "bar"
+
+# && - not run after failure
+echo "foo" && echo "bar"
+cat wrong-file.txt && echo "bar"
+./test.sh && git push # prevents from pusing failed code
+
+# || - runs success then stops else run till success
+echo "foo" || echo "bar"
+cat wrong-file.txt || echo "bar"
+cat wrong-file.txt || echo $? || echo "bar"
+cat wrong-file.txt || echo $? && echo "bar"
+./test.sh || ./cleanup.sh # if test success then only run cleanup of setup
