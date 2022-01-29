1c481b2628eda85d67faad52074d3b8c21829b9c
Sagar Nikam <sagarnikam123@gmail.com>
2022-01-29 08:20:49 +0530

    change formatting

--- others/language/bash/1_bashExitCodesCommandChaining.sh
+++ others/language/bash/1_bashExitCodesCommandChaining.sh
@@ -2,7 +2,7 @@
 
 # Hello World
 echo "Hello World"
-
+################################################################################
 # $? - exit code of last command run
 # 0 - success
 # any non zero - not success
@@ -11,19 +11,20 @@ echo $?
 # failed codes
 cat wrong-file.txt
 echo $?
-
+################################################################################
 # semicolon(;) - to run command whether failed/success
 echo "foo"; echo "bar"
 cat wrong-file.txt; echo "bar"
-
+################################################################################
 # && - not run after failure
 echo "foo" && echo "bar"
 cat wrong-file.txt && echo "bar"
 ./test.sh && git push # prevents from pusing failed code
-
+################################################################################
 # || - runs success then stops else run till success
 echo "foo" || echo "bar"
 cat wrong-file.txt || echo "bar"
 cat wrong-file.txt || echo $? || echo "bar"
 cat wrong-file.txt || echo $? && echo "bar"
 ./test.sh || ./cleanup.sh # if test success then only run cleanup of setup
+################################################################################
