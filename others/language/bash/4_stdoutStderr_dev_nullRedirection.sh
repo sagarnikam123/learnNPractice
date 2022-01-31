dd1b956ff939c1bfcd6a8de65abeb7e4ea6c56fd
Sagar Nikam <sagarnikam123@gmail.com>
2022-01-31 14:11:34 +0530

    done

---
+++ others/language/bash/4_stdoutStderr_dev_nullRedirection.sh
@@ -0,0 +1,11 @@
+# 4_What is stdout, stderr, _dev_null and redirection 
+
+# version2.sh
+################################################################################
+# 1> - redirect from stream 1
+# &2 - take into stream 2
+# /dev/null - take input from stdin & discards it
+################################################################################
+curl -v -X POST localhost:3000
+curl -v -X POST localhost:3000 2>/dev/null
+################################################################################
