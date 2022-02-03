5fc662e0975bb8e2a2a4d7f1fe561ab6ce416bc2
Sagar Nikam <sagarnikam123@gmail.com>
2022-02-03 13:21:11 +0530

    move subtitles

---
+++ others/tricksTips/moveSubtitlesToRespectiveFolders.py
@@ -0,0 +1,33 @@
+# moves subtitles files (.srt) to respective folders
+
+import glob
+import os
+import shutil
+
+#settings
+courseName ='/home/tetra/Desktop/course1'
+subtitleExt = 'srt'
+subtitleDepositDir='subtitles'
+
+# change directory to given course
+os.chdir(courseName)
+
+# make subtitle folder if not exists
+if not os.path.isdir(subtitleDepositDir):
+    os.makedirs(subtitleDepositDir)
+
+# get directory names
+for dirNames in next(os.walk(os.getcwd())):
+    #print(dirNames) # all directories as list
+    for singleFolder in dirNames:
+        # find list of all files with sp. extension
+        for fileToMove in glob.glob(singleFolder + '/*.' + subtitleExt):
+            #print(fileToMove)
+            #moving subtitle files to subtitleDepositDir
+            # making folder before moving
+            finalDepositFolder=subtitleDepositDir + '/' + singleFolder
+            if not os.path.isdir(finalDepositFolder):
+                os.makedirs(finalDepositFolder)
+                #print(finalDepositFolder)
+            shutil.move(fileToMove, finalDepositFolder)
+   
