4e91266ceb5514812ddf93feb98b9096f3f247bb
Sagar Nikam <sagarnikam123@gmail.com>
2022-02-15 17:29:23 +0530

    port-forward error and helm repo

--- others/tech/argo/argoCD/gettingStarted/gettingStartedArgoCD.sh
+++ others/tech/argo/argoCD/gettingStarted/gettingStartedArgoCD.sh
@@ -15,14 +15,17 @@ sudo chmod +x /usr/local/bin/argocd
 # 3. Access The Argo CD API Server
 # By default, the Argo CD API server is not exposed with an external IP
 
-# Port Forwarding
-kubectl port-forward svc/argocd-server -n argocd 8080:443
+# Port Forwarding (only ipv4)
+# E0215 16:47:15.650227   53917 portforward.go:378] error copying from remote stream to local connection: readfrom tcp6 [::1]:8080->[::1]:55200: write tcp6 [::1]:8080->[::1]:55200: write: broken pipe
+# E0215 16:52:56.625475   93374 portforward.go:378] error copying from remote stream to local connection: readfrom tcp4 127.0.0.1:8080->127.0.0.1:51680: write tcp4 127.0.0.1:8080->127.0.0.1:51680: write: broken pipe
+kubectl port-forward svc/argocd-server --address '127.0.0.1' -n argocd 8080:443
 
 # Service Type Load Balancer - not worked in minikube
 # kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
 # edit service type as ClusterIP -> NodePort
 kubectl -n argocd edit svc argocd-server
+kubectl get nodes -o wide -n argocd # taken any agent's ip & port
 
 # 4. Login Using The CLI
 # initial password for the admin account is auto-generated and stored as clear text in the  field 
@@ -47,3 +50,8 @@ argocd app create guestbook --repo https://github.com/argoproj/argocd-example-ap
 # Syncing via CLI
 argocd app get guestbook
 argocd app sync guestbook
+
+# others
+# helm repo add bitnami https://charts.bitnami.com/bitnami
+# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+# helm repo update
