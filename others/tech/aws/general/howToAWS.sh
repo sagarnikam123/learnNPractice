aa1c7642ed3ece2a82cc535bdcd0ab713fdd92ab
Sagar Nikam <sagarnikam123@gmail.com>
2022-02-15 21:39:30 +0530

    adds ec2-instance-selector

--- others/tech/aws/general/howToAWS.sh
+++ others/tech/aws/general/howToAWS.sh
@@ -1,15 +1,15 @@
 # How to AWS
 
-############################################################
+################################################################################
 #   SSH - EC2
-############################################################
+################################################################################
 
 chmod 0400 /home/quanta/Dropbox/secure/aws/ec2Keypairs/genRootAccess.pem
 #ssh
 ssh -i genRootAccess.pem ubuntu@ec2-13-127-205-225.ap-south-1.compute.amazonaws.com
-############################################################
+################################################################################
 #   Images - AMI
-############################################################
+################################################################################
 
 # getting images
 aws ec2 describe-images --owners 309956199498 --query 'Images[*].[CreationDate,Name,ImageId]' --filters "Name=name,Values=RHEL-7.?*GA*" --region us-east-1 --output table | sort -r
@@ -20,12 +20,21 @@ aws ec2 describe-images \
     --output 'text'
 
 aws ec2 describe-images --owners 679593333241 --region us-east-1 --output table | sort -r
-############################################################
+################################################################################
 #   Availability Zone - Region
-############################################################
+################################################################################
 
 # specific availability zone
 aws ec2 describe-availability-zones --region ap-south-1
 # all availability zones
 aws ec2 describe-availability-zones --all-availability-zones
-############################################################
+################################################################################
+# Amazon EC2 Instance Selector
+
+curl -Lo ec2-instance-selector https://github.com/aws/amazon-ec2-instance-selector/releases/download/v2.0.3/ec2-instance-selector-`uname | tr '[:upper:]' '[:lower:]'`-amd64 && chmod +x ec2-instance-selector
+mkdir -p $HOME/bin && cp ./ec2-instance-selector $HOME/bin/ec2-instance-selector
+
+ec2-instance-selector --memory 2 --vcpus 1 --region ap-south-1 --availability-zones ap-south-1a -o table
+ec2-instance-selector --memory 2 --vcpus 1 --region ap-south-1 --availability-zones ap-south-1b -o table
+ec2-instance-selector --memory 2 --vcpus 1 --region ap-south-1 --availability-zones ap-south-1c -o table
+################################################################################
