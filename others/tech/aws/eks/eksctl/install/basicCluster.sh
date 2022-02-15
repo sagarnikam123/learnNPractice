3081c74f3754127eaa8ecf04fc8084553780c772
Sagar Nikam <sagarnikam123@gmail.com>
2022-02-15 22:43:26 +0530

    with config file

--- others/tech/aws/eks/eksctl/install/basicCluster.sh
+++ others/tech/aws/eks/eksctl/install/basicCluster.sh
@@ -2,14 +2,15 @@
 
 eksctl create cluster --name=firstEKSCluster --nodes=2 \
 --region ap-south-1 \
---instance-types t2.micro \
+--instance-types t2.small \
 --nodegroup-name ng-firsteks \
 --instance-prefix eksfirst \
 --ssh-access \
 --node-volume-type gp2 \
 --node-volume-size=30 \
---zones=ap-south-1a,ap-south-1b
+--zones=ap-south-1a,ap-south-1b \
 --dry-run
+#--kubeconfig=.kube/config.eks.yaml
 
 # check cluster details
 eksctl get cluster firstEKSCluster
@@ -19,6 +20,23 @@ kubectl get nodes -o wide
 
 # destroy cluster
 eksctl delete cluster firstEKSCluster
+################################################################################
+# using cluster config file
+eksctl create cluster --name development --dry-run >> basicCluster.yaml
+eksctl create cluster --config-file=basicCluster.yaml --kubeconfig=.kube/config.eks.yaml
+
+# check
+kubectl --kubeconfig=.kube/config.eks.yaml get nodes
+
+# getting kubeconfig file for given cluster
+eksctl utils write-kubeconfig --cluster firstEKSCluster --kubeconfig=.kube/config.eks.yaml
+################################################################################
+# Amazon EKS recommended maximum Pods for each Amazon EC2 instance type
+# https://github.com/awslabs/amazon-eks-ami/blob/master/files/eni-max-pods.txt
+
+# a script that you can use to calculate the maximum number of Pods for each instance type
+curl -o max-pods-calculator.sh https://raw.githubusercontent.com/awslabs/amazon-eks-ami/master/files/max-pods-calculator.sh
+chmod +x max-pods-calculator.sh
 ################################################################################
 # 2022-02-14 12:52:16 [✖]  AWS::EKS::Nodegroup/ManagedNodeGroup: CREATE_FAILED – 
 # "Resource handler returned message: \"Your requested instance type (t2.micro) 
