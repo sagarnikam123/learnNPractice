9dbc4a96692cbffc19aea69bac9134719f6e84b3
Sagar Nikam <sagarnikam123@gmail.com>
2022-02-19 21:18:30 +0530

    basic of open

---
+++ w3schools/python/fileHandling/1_file_handling.py
@@ -0,0 +1,19 @@
+# File Open
+
+# File Handling
+# open(filename, mode)
+"""
+"r" - Read - Default value. Opens a file for reading,
+            error if the file does not exist
+"a" - Append - Opens a file for appending,
+            creates the file if it does not exist
+"w" - Write - Opens a file for writing, creates the file if it does not exist
+"x" - Create - Creates the specified file, returns an error if the file exists
+"t" - Text - Default value. Text mode
+"b" - Binary - Binary mode (e.g. images)
+"""
+
+# open a file
+f = open("demofile.txt")
+f = open("demofile.txt", "rt")
+###############################################################################
