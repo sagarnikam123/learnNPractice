f1a7dc8e2552076f568c73f434f93d66de9853a6
Sagar Nikam <sagarnikam123@gmail.com>
2022-02-19 21:19:16 +0530

    delete file

---
+++ w3schools/python/fileHandling/4_delete_files.py
@@ -0,0 +1,16 @@
+# Delete a File
+
+# Remove the file "demofile.txt"
+import os
+os.remove("demofile.txt")
+###############################################################################
+# Check if File exist
+if(os.path.exists("demofile.txt")):
+    os.remove("demofile.txt")
+else:
+    print("The file does not exists")
+###############################################################################
+# Delete Folder
+os.makedirs("myfolder")
+os.rmdir("myfolder")  # can only remove empty folders
+###############################################################################
