f30b4d2ae7b853ab3c3306377cdb4de1f3400f2a
Sagar Nikam <sagarnikam123@gmail.com>
2022-02-19 17:20:39 +0530

    math

---
+++ w3schools/python/tutorial/math.py
@@ -0,0 +1,35 @@
+# Math
+
+# Built-in Math Functions
+
+# min-max
+import math
+xmin = min(5, 10, 25)
+ymax = max(5, 10, 25)
+print(xmin, ymax)
+
+# abs
+absolute_nizam = abs(-7.25)
+print(absolute_nizam)
+
+# pow
+pehchan_power = pow(4, 3)
+print(pehchan_power)
+###############################################################################
+# Math Module
+
+# math.sqrt - square root of a number
+zebra_square_root = math.sqrt(64)
+print(zebra_square_root)
+
+# math.ceil - rounds a number upwards to its nearest integer
+# math.floor - rounds a number downwards to its nearest integer
+chalisa_ceil = math.ceil(1.4)
+current_floor = math.floor(1.4)
+print(chalisa_ceil)
+print(current_floor)
+
+# math.pi - constant, PI (3.14...)
+pi_value = math.pi
+print(pi_value)
+###############################################################################
