f5a3979d7a4f9be6adf9b02a61ed5b30b9d7adea
Sagar Nikam <sagarnikam123@gmail.com>
2022-02-20 05:46:28 +0530

    incomplete

---
+++ others/language/bash/9_cURL.sh
@@ -0,0 +1,8 @@
+# cURL - The only HTTP client you'll ever need
+
+################################################################################
+curl google.com
+
+curl localhost:8080/coasters
+curl localhost:8080/coasters -X GET
+################################################################################
