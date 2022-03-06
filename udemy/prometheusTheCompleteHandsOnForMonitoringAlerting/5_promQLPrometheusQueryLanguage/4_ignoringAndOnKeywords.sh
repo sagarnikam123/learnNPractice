88a1d2bbd99828138430b1828a3537ca75db2a19
Sagar Nikam <sagarnikam123@gmail.com>
2022-03-06 16:27:41 +0530

    ignoring on

---
+++ udemy/prometheusTheCompleteHandsOnForMonitoringAlerting/5_promQLPrometheusQueryLanguage/4_ignoringAndOnKeywords.sh
@@ -0,0 +1,5 @@
+# 'ignoring' and 'on' keywords
+
+# not working
+# prometheus_http_requests_total and ignoring(handler) promhttp_metric_handler_errors_total
+# promhttp_metric_handler_errors_total and on(code) prometheus_http_requests_total
