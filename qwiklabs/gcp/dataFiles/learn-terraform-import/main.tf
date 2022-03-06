734b3d56a45fb9865e8cecb0eb9c0ce3da6c3258
Sagar Nikam <sagarnikam123@gmail.com>
2022-03-06 14:00:55 +0530

    terraform import

---
+++ qwiklabs/gcp/dataFiles/learn-terraform-import/main.tf
@@ -0,0 +1,16 @@
+# Configure docker provider
+#
+# https://www.terraform.io/docs/providers/docker/index.html
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.11"
+    }
+  }
+  required_version = ">= 0.14"
+}
+
+provider "docker" {
+  host    = "npipe:////.//pipe//docker_engine"
+}
