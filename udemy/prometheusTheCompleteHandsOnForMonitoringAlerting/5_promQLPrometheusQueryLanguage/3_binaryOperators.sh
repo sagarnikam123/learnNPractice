9182a1b52176bad0a7e05482587087e9d32c4b5f
Sagar Nikam <sagarnikam123@gmail.com>
2022-03-06 16:01:08 +0530

    binary operators

---
+++ udemy/prometheusTheCompleteHandsOnForMonitoringAlerting/5_promQLPrometheusQueryLanguage/3_binaryOperators.sh
@@ -0,0 +1,13 @@
+# Binary Operators
+
+# arithmetic binary operators
+# node_memory_Active_bytes/1024/1024  # (MB)
+# prometheus_http_requests_total
+# prometheus_http_requests_total * 2 # double
+
+# comparison
+# node_cpu_seconds_total > 500
+
+# logical
+# promhttp_metric_handler_errors_total and prometheus_http_requests_total
+# promhttp_metric_handler_errors_total or prometheus_http_requests_total
