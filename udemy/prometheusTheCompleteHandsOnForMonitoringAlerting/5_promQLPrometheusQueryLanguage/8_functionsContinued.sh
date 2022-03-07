4e7b234025160bbcb5d3a4629b6cc7c450ef06e8
Sagar Nikam <sagarnikam123@gmail.com>
2022-03-07 15:49:50 +0530

    other func

---
+++ udemy/prometheusTheCompleteHandsOnForMonitoringAlerting/5_promQLPrometheusQueryLanguage/8_functionsContinued.sh
@@ -0,0 +1,14 @@
+# Functions continued
+
+# _over_time
+# max_over_time(node_cpu_seconds_total[1h])
+# min_over_time(node_cpu_seconds_total[1h])
+# avg_over_time(node_cpu_seconds_total[1h])
+
+# sort(node_cpu_seconds_total)
+# sort_desc(node_cpu_seconds_total)
+
+# time()
+# time() - process_start_time_seconds{job='node_exporter'}
+# day_of_week()
+# day_of_month()
