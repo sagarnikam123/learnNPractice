34fb5e8949f27e30a662aebd4920a0eda9f24830
Sagar Nikam <sagarnikam123@gmail.com>
2022-03-08 17:41:51 +0530

    done

---
+++ qwiklabs/aws/collectingAndAnalyzingLogsWithAmazonCloudWatchLogsInsights.sh
@@ -0,0 +1,11 @@
+# Collecting and Analyzing Logs with Amazon CloudWatch Logs Insights
+
+###############################################################################
+
+# Task 1: Setting up instance logging agents using AWS Systems Manager
+# Task 2: Network logging
+# Task 3: Enable database logging
+# Task 4: Validating and exploring log streams
+# Task 5: Log discovery
+
+###############################################################################
