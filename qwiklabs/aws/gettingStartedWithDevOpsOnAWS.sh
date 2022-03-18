b2101b704b8fb43f4bf90760fc2284fcd1cd0090
Sagar Nikam <sagarnikam123@gmail.com>
2022-03-18 19:42:22 +0530

    incomplete

--- qwiklabs/aws/gettingStartedWithDevOpsOnAWS.sh
+++ qwiklabs/aws/gettingStartedWithDevOpsOnAWS.sh
@@ -2,4 +2,9 @@
 
 ################################################################################
 
+# Task 1: Create the infrastructure code for the environments
+# Task 2: Deploy the application to a staging region using CodePipeline
+# Task 3: Extend continuous delivery to the production region
+# Task 4: Modify the application and test the entire CI/CD process
+
 ################################################################################
