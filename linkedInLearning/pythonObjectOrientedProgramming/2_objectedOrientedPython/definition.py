4fb8aef28d8e8c306c35840280badbaac4a62949
Sagar Nikam <sagarnikam123@gmail.com>
2022-03-20 19:10:31 +0530

    done

---
+++ linkedInLearning/pythonObjectOrientedProgramming/2_objectedOrientedPython/definition.py
@@ -0,0 +1,12 @@
+# TODO: create a basic class
+class Book:
+    def __init__(self, title):
+        self.title = title
+
+# TODO: create instances of the class
+b1 = Book("Brave New World")
+b2 = Book("War and Peace")
+
+# TODO: print the class and property
+print(b1)
+print(b1.title)
