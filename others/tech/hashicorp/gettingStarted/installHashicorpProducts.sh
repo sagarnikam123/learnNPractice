77341e6bf9291cf5ec58c6242ec03c24e5264add
Sagar Nikam <sagarnikam123@gmail.com>
2022-04-09 16:42:52 +0530

    macOS install

---
+++ others/tech/hashicorp/gettingStarted/installHashicorpProducts.sh
@@ -0,0 +1,18 @@
+# install hashicorp products
+
+################################################################################
+# macOS
+brew tap hashicorp/tap
+
+# terraform
+brew install hashicorp/tap/terraform
+
+# packer
+brew install hashicorp/tap/packer
+
+# nomad
+brew install hashicorp/tap/nomad
+
+# vagrant
+brew install vagrant
+################################################################################
