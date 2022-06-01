17213b5ad371499f6c5e0c9d8fa351e38cddfeca
Sagar Nikam <sagarnikam123@gmail.com>
2022-06-01 19:06:41 +0530

    using template

--- others/language/python/flask/projects/flaskMarket/market.py
+++ others/language/python/flask/projects/flaskMarket/market.py
@@ -1,13 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-
-@app.route('/about/<username>')
-def about_page(username):
-    return f'<h1>This is the about page of {username}</h1>'
+def home_page():
+    return render_template('home.html')
