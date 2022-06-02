2f94ef1ac58baf5e81e0bd48bd9b118be8d61ac3
Sagar Nikam <sagarnikam123@gmail.com>
2022-06-02 19:34:42 +0530

    db model and connection

--- others/language/python/flask/projects/flaskMarket/market.py
+++ others/language/python/flask/projects/flaskMarket/market.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'Item {self.name}'
 
 
 @app.route("/")
