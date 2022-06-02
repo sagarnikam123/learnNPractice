0bd0cc2ba99554358419c350eac0661ab5cf4a47
Sagar Nikam <sagarnikam123@gmail.com>
2022-06-02 13:52:49 +0530

    market route

--- others/language/python/flask/projects/flaskMarket/market.py
+++ others/language/python/flask/projects/flaskMarket/market.py
@@ -7,3 +7,8 @@ app = Flask(__name__)
 @app.route('/home')
 def home_page():
     return render_template('home.html')
+
+
+@app.route('/market')
+def market_page():
+    return render_template('market.html', item_name='Phone')
