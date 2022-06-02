66036b0af1844b84e3d002f06359fcafa85e9e14
Sagar Nikam <sagarnikam123@gmail.com>
2022-06-02 16:24:00 +0530

    product table

--- others/language/python/flask/projects/flaskMarket/market.py
+++ others/language/python/flask/projects/flaskMarket/market.py
@@ -11,4 +11,9 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    return render_template('market.html', item_name='Phone')
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    return render_template('market.html', items=items)
