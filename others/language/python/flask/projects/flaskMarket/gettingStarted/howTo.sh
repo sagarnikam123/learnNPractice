c5d145b3d8ce06db13b0c6606c308efcdbd227e3
Sagar Nikam <sagarnikam123@gmail.com>
2022-06-03 13:19:54 +0530

    database data and crud

--- others/language/python/flask/projects/flaskMarket/gettingStarted/howTo.sh
+++ others/language/python/flask/projects/flaskMarket/gettingStarted/howTo.sh
@@ -1,7 +1,8 @@
 # how to flaskMarket
 
 # export
-export FLASK_APP=market.py
+#export FLASK_APP=market.py
+export FLASK_APP=run.py
 export FLASK_DEBUG=1
 
 # run app
@@ -12,20 +13,29 @@ flask run --host=0.0.0.0
 # play with db
 python3
 
-from market import db
+from market.models import db
+# db.drop_all() # delete previous database
 db.create_all()
 
-from market import Item
-item1 = Item(name='IPhone 10', price=500, barcode='893212299897', description='Phone')
-db.session.add(item1)
-db.session.commit()
-Item.query.all()
+from market.models import User, Item
 
+# users
+u1 = User(username='jsc', password_hash='123456', email_address='jsc@jsc.com')
+db.session.add(u1)
+db.session.commit()
+User.query.all()
+
+# items
+item1 = Item(name='IPhone 10', price=500, barcode='893212299897', description='Phone')
 item2 = Item(name='Laptop', price=600, barcode='123985473165', description='Simple Laptop')
 item3 = Item(name='Keyboard', price=600, barcode='231985128446', description='Any Keyboard')
+
+db.session.add(item1)
 db.session.add(item2)
 db.session.add(item3)
 db.session.commit()
+
+# query
 Item.query.all()
 
 # iterate
@@ -37,4 +47,14 @@ for item in Item.query.filter_by(price=500):
     item.name
     item.price
 
+item1 = Item.query.filter_by(name='IPhone 10').first()
+item1.owner = User.query.filter_by(username='jsc').first().id
+# db.session.rollback()
+db.session.add(item1)
+db.session.commit()
+
+item1.owner
+
+i = Item.query.filter_by(name='IPhone 10').first()
+i.owned_user
 exit()
