fd6ba32cd3925a0b547ca2eff7ff61a930c1f8c4
Sagar Nikam <sagarnikam123@gmail.com>
2022-06-03 12:32:24 +0530

    correct spelling

--- others/language/python/flask/projects/flaskMarket/market/models.py
+++ others/language/python/flask/projects/flaskMarket/market/models.py
@@ -4,8 +4,8 @@ from market import db
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
-    email_address = db.Column(db.String(lenth=50), nullable=False, unique=True)
-    password_hash = db.Column(db.String(lenth=60), nullable=False)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=1000)
     items = db.relationship('Item', backref='owned_user', lazy=True)
 
