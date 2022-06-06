47e5faff405278424ae6b9aa3a030b69b292003b
Sagar Nikam <sagarnikam123@gmail.com>
2022-06-06 14:04:38 +0530

    secret key to write to db

--- others/language/python/flask/projects/flaskMarket/market/__init__.py
+++ others/language/python/flask/projects/flaskMarket/market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '4e405016b5847318ef6c710c'
 db = SQLAlchemy(app)
 
 from market import routes
