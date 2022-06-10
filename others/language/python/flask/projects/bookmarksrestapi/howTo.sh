343ffa551427bdaff9f6f4d64ba0b025d38e91c6
Sagar Nikam <sagarnikam123@gmail.com>
2022-06-10 20:13:18 +0530

    exports and create db

--- others/language/python/flask/projects/bookmarksrestapi/howTo.sh
+++ others/language/python/flask/projects/bookmarksrestapi/howTo.sh
@@ -10,3 +10,17 @@ python3 -m virtualenv venv
 source venv/bin/activate
 #deactivate
 python --version
+
+# export
+export FLASK_APP=src
+export FLASK_DEBUG=1
+
+# url
+127.0.0.1:5000/api/v1/auth/me
+
+#  create db
+flask shell
+from src.database import db
+db.create_all()
+db.drop_all()
+db.create_all()
