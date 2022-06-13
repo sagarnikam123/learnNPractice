731feb0e57d6ad50df0e907e845a2d1b5360ab16
Sagar Nikam <sagarnikam123@gmail.com>
2022-06-13 17:41:22 +0530

    login page route

--- others/language/python/flask/projects/flaskMarket/market/routes.py
+++ others/language/python/flask/projects/flaskMarket/market/routes.py
@@ -1,7 +1,7 @@
 from market import app
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
 from market import db
 
 
@@ -30,3 +30,9 @@ def register_page():
         for err_msg in form.errors.values():
             flash(f'There was an error with creating a user: {err_msg}', category='danger')
     return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    return render_template('login.html', form=form)
