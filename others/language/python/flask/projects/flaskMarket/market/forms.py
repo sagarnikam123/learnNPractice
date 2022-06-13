4eeafa12f32f661106a6a0cf50ae2baa3c68b48b
Sagar Nikam <sagarnikam123@gmail.com>
2022-06-13 17:42:01 +0530

    login form

--- others/language/python/flask/projects/flaskMarket/market/forms.py
+++ others/language/python/flask/projects/flaskMarket/market/forms.py
@@ -21,3 +21,9 @@ class RegisterForm(FlaskForm):
     password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
     password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired() ])
     submit = SubmitField(label='Create Account')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(label='User Name:', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Sign in')
