8f19955081b5b31a7f6d21adc661d77542e64cf0
Sagar Nikam <sagarnikam123@gmail.com>
2022-06-13 17:39:24 +0530

    adds db env n config

--- others/language/python/flask/projects/bookmarksrestapi/src/__init__.py
+++ others/language/python/flask/projects/bookmarksrestapi/src/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, jsonify
 from src.auth import auth
 from src.bookmarks import bookmarks
+from src.database import db
 
 
 def create_app(test_config=None):
@@ -9,11 +10,14 @@ def create_app(test_config=None):
 
     if test_config is None:
         app.config.from_mapping(
-            SECRET_KEY=os.environ.get('SECRET_KEY')
+            SECRET_KEY=os.environ.get('SECRET_KEY'),
+            SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DB_URI')
         )
     else:
         app.config.from_mapping(test_config)
 
+    db.app = app
+    db.init_app(app)
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
 
