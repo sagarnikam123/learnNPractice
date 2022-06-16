6b697a65c26a9d069ae898ccc6efee2a38f8f534
Sagar Nikam <sagarnikam123@gmail.com>
2022-06-16 13:00:24 +0530

    jwt secret key

--- others/language/python/flask/projects/bookmarksrestapi/src/__init__.py
+++ others/language/python/flask/projects/bookmarksrestapi/src/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify
 from src.auth import auth
 from src.bookmarks import bookmarks
 from src.database import db
+from flask_jwt_extended import JWTManager
 
 
 def create_app(test_config=None):
@@ -12,13 +13,17 @@ def create_app(test_config=None):
         app.config.from_mapping(
             SECRET_KEY=os.environ.get('SECRET_KEY'),
             SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DB_URI'),
-            SQLALCHEMY_TRACK_MODIFICATIONS=False
+            SQLALCHEMY_TRACK_MODIFICATIONS=False,
+            JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY')
         )
     else:
         app.config.from_mapping(test_config)
 
     db.app = app
     db.init_app(app)
+
+    JWTManager(app)
+
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
 
