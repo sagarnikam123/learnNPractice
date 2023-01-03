99bb4a9746bce6871bbe7d2c329ada3738be7ca9
Sagar Nikam <sagarnikam123@gmail.com>
2023-01-03 21:53:13 +0530

    installs

---
+++ others/tech/ansible/gettingStarted/install.sh
@@ -0,0 +1,13 @@
+# ansible getting started
+
+# control node - an agentless automation tool that you install on a single host for controlling other
+# managed nodes - an entire fleet of machines and other devices managed by Ansible remotely with SSH, Powershell remoting
+# ansible-core - minimalist language and runtime package
+# ansible - much larger “batteries included” package
+
+# install for current user
+python3 -m pip install --user ansible
+# python3 -m pip install --user ansible-core==2.12.3 # specific version
+
+# check version
+ansible --version
