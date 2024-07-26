015204ee667db48b60f56eae9b50fc1458378aa0
Sagar Nikam <sagarnikam123@gmail.com>
2024-07-26 15:34:23 +0530

    feat: install react

---
+++ others/language/react/install/install_react.sh
@@ -0,0 +1,17 @@
+# install react - https://react.dev/
+
+#######################################################################################################################
+# install nodejs(LTS) + npm
+npm --version
+node --version
+
+# install VSCode
+# install react dev tools (chrome extension)
+
+# VSCode extension - thunderclient - alternative to Postman
+# VSCode extension - ES7+ React/Redux/React-Native snippets
+# VSCode extension - Bracket Pair Colorizer 2 (deprecated)
+# VSCode extension - Auto Rename Tag
+# VSCode extension - Live Server
+# VSCode extension - Prettier - Code formatter
+#######################################################################################################################
