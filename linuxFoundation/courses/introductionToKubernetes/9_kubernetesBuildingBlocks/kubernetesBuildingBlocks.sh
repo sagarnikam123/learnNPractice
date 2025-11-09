bab6a4eb04d78d6acda7cc7a4d59c203276aa2e6
Sagar Nikam <sagarnikam123@gmail.com>
2025-11-09 09:54:36 +0530

    feat: daemonset

--- linuxFoundation/courses/introductionToKubernetes/9_kubernetesBuildingBlocks/kubernetesBuildingBlocks.sh
+++ linuxFoundation/courses/introductionToKubernetes/9_kubernetesBuildingBlocks/kubernetesBuildingBlocks.sh
@@ -111,3 +111,22 @@ kubectl get all -l app=nginx-deployment -o wide
 kubectl delete deploy nginx-deployment
 kubectl get deploy,rs,po -l app=nginx-deployment
 ########################################################################################################################
+# DaemonSets
+
+kubectl create -f fluentd-ds.yaml
+kubectl apply -f fluentd-ds.yaml --record
+kubectl get daemonsets
+kubectl get ds -o wide
+kubectl get ds fluentd-agent -o yaml
+kubectl get ds fluentd-agent -o json
+kubectl describe ds fluentd-agent
+kubectl rollout status ds fluentd-agent
+kubectl rollout history ds fluentd-agent
+kubectl rollout history ds fluentd-agent --revision=1
+kubectl set image ds fluentd-agent fluentd=quay.io/fluentd_elasticsearch/fluentd:v4.6.2 --record
+kubectl rollout history ds fluentd-agent --revision=2
+kubectl rollout undo ds fluentd-agent --to-revision=1
+kubectl get all -l k8s-app=fluentd-agent -o wide
+kubectl delete ds fluentd-agent
+kubectl get ds,po -l k8s-app=fluentd-agent
+########################################################################################################################
