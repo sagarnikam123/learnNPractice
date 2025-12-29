b26b18339a290dcfe2580554e105dab658462cdf
Sagar Nikam <sagarnikam123@gmail.com>
2025-12-29 10:40:30 +0530

    feat: opentelemetry

---
+++ linuxFoundation/courses/gettingStartedWithOpenTelemetry/4_handsOnLab_openTelemetryInAction/4_handsOnLab_openTelemetryInAction.sh
@@ -0,0 +1,13 @@
+# Exercise 1: The Demo Environment
+
+# Running the Demo Environment (1)
+git clone https://github.com/lftraining/LFS148-code.git
+cd LFS148-code/
+cd exercises/otel-in-action
+
+docker compose up
+docker ps # 8 container running
+
+# Java-Thymeleaf ToDo List - http://localhost:8090/
+# Python-Flask ToDo List - http://localhost:5001/
+# Jaeger web UI - http://localhost:16686/
