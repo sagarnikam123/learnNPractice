f1023036f0b71656fcedcf74ff5380c83c269111
Sagar Nikam <sagarnikam123@gmail.com>
2026-01-06 12:40:57 +0530

    feat: secrets

--- linuxFoundation/courses/introductionToKubernetes/14_configMapsAndSecrets/14_configMapsAndSecrets.sh
+++ linuxFoundation/courses/introductionToKubernetes/14_configMapsAndSecrets/14_configMapsAndSecrets.sh
@@ -48,7 +48,7 @@ volumes:
     name: vol-config-map
 
 # Using ConfigMaps as Volumes Demo Guide
-kubectl create configmap green-web-cm --from-file=index.html
+kubectl create configmap green-web-cm --from-file=index-green.html
 kubectl apply -f web-green-with-cm.yaml
 kubectl expose deployment green-web --type=NodePort
 minikube service list
@@ -56,3 +56,59 @@ minikube service green-web --url # open in browser
 curl -s http://127.0.0.1:51622 | head
 
 # Secrets
+# Create a Secret from Literal Values
+kubectl create secret generic my-password \
+--from-literal=password=mysqlpassword
+
+kubectl get secret my-password
+kubectl get secret my-password -o yaml
+kubectl describe secret my-password
+
+# Create a Secret from a Definition Manifest
+# encode to base64 value
+echo mysqlpassword | base64
+bXlzcWxwYXNzd29yZAo=
+# decode base64 value
+echo "bXlzcWxwYXNzd29yZAo=" | base64 --decode
+mysqlpassword
+
+kubectl create -f mypass.yaml
+kubectl create -f mypass-stringdata.yaml
+
+kubectl describe secret my-password-2
+kubectl describe secret my-password-3
+
+# Create a Secret from a File
+echo mysqlpassword | base64
+echo mysqlpassword | base64 > password.txt
+kubectl create secret generic my-file-password \
+--from-file=password.txt
+
+kubectl get secret my-file-password -o yaml
+kubectl describe secret my-file-password
+
+# Use Secrets Inside Pods: As Environment Variables
+spec:
+  containers:
+  - image: wordpress:4.7.3-apache
+    name: wordpress
+    env:
+    - name: WORDPRESS_DB_PASSWORD
+      valueFrom:
+        secretKeyRef:
+          name: my-password
+          key: password
+
+# Use Secrets Inside Pods: As Volumes
+spec:
+  containers:
+  - image: wordpress:4.7.3-apache
+    name: wordpress
+    volumeMounts:
+    - name: secret-volume
+      mountPath: /etc/secret-data
+      readOnly: true
+  volumes:
+  - name: secret-volume
+    secret:
+      secretName: my-password
